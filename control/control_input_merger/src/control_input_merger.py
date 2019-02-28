2a4ffacccea56dc2f5ae48f0c5c48d365470919d
erlendlundby <erlend.l.94@gmail.com>
2019-02-28 09:52:16 +0100

    Add wrench messages to subscribing and publishing messages

--- control/control_input_merger/src/control_input_merger.py
+++ control/control_input_merger/src/control_input_merger.py
@@ -8,15 +8,15 @@ class ControlInputMerger(object):
         rospy.init_node('control_input_merger_node')
 
         #Subscriber nodes
-        self.sub_surge = rospy.Subscriber('surge_input', PropulsionCommand, self.surge_callback, queue_size=1)
-        self.sub_sway = rospy.Subscriber('sway_input', PropulsionCommand, self.sway_callback, queue_size=1)
-        self.sub_heave = rospy.Subscriber('heave_input', PropulsionCommand, self.heave_callback, queue_size=1)
-        self.sub_roll = rospy.Subscriber('roll_input',PropulsionCommand, self.yaw_callback, queue_size=1)
-        self.sub_pitch = rospy.Subscriber('pitch_input',PropulsionCommand, self.yaw_callback, queue_size=1)
-        self.sub_yaw = rospy.Subscriber('yaw_input',PropulsionCommand, self.yaw_callback, queue_size=1)
+        self.sub_surge = rospy.Subscriber('surge_input', Wrench, self.surge_callback, queue_size=1)
+        self.sub_sway = rospy.Subscriber('sway_input', Wrench, self.sway_callback, queue_size=1)
+        self.sub_heave = rospy.Subscriber('heave_input', Wrench, self.heave_callback, queue_size=1)
+        self.sub_roll = rospy.Subscriber('roll_input',Wrench, self.yaw_callback, queue_size=1)
+        self.sub_pitch = rospy.Subscriber('pitch_input',Wrench, self.yaw_callback, queue_size=1)
+        self.sub_yaw = rospy.Subscriber('yaw_input',Wrench, self.yaw_callback, queue_size=1)
 
         #Publisher node
-        self.pub_thrust = rospy.Publisher('propulsion_command', PropulsionCommand, queue_size=1)
+        self.pub_thrust = rospy.Publisher('/manta/thruster_manager/input', Wrench, queue_size=1)
 
 
         #Initialize thrust publisher message
@@ -41,28 +41,28 @@ class ControlInputMerger(object):
 
     #Callback
     def surge_callback(self, msg):
-        self.motion_msg.motion[0] = msg.motion[0]
+        self.motion_msg.force[0] = msg.force[0]
         self.pub_thrust.publish(self.motion_msg)
 
 
     def sway_callback(self, msg):
-        self.motion_msg.motion[1] = msg.motion[1]
+        self.motion_msg.force[1] = msg.force[1]
         self.pub_thrust.publish(self.motion_msg)
 
     def heave_callback(self, msg):
-        self.motion_msg.motion[2] = msg.motion[2]
+        self.motion_msg.force[2] = msg.force[2]
         self.pub_thrust.publish(self.motion_msg)
 
     def roll_callback(self, msg):
-        self.motion_msg.motion[3] = msg.motion[3]
+        self.motion_msg.torque[0] = msg.torque[0]
         self.pub_thrust.publish(self.motion_msg)
 
     def pitch_callback(self, msg):
-        self.motion_msg.motion[4] = msg.motion[4]
+        self.motion_msg.torque[1] = msg.torque[1]
         self.pub_thrust.publish(self.motion_msg)
 
     def yaw_callback(self, msg):
-        self.motion_msg.motion[5] = msg.motion[5]
+        self.motion_msg.torque[0] = msg.torque[2]
         self.pub_thrust.publish(self.motion_msg)
 
 
