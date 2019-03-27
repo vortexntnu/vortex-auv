c1de11be390f33cbf6b221bb14223597fda45757
Oysand <oystein.utbjoe.andersland@gmail.com>
2019-03-27 09:45:33 +0100

    Add functionality from depth hold node

--- control/depth_hold_action_server/src/depth_hold_action_server_ros.cpp
+++ control/depth_hold_action_server/src/depth_hold_action_server_ros.cpp
@@ -3,14 +3,29 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include "depth_hold_action_server/DepthHoldAction.h"
-
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Wrench.h>
+#include <iostream>
 
 DepthHoldAction::DepthHoldAction(std::string name) :
     as_(nh_, name, boost::bind(&DepthHoldAction::executeCB, this, _1), false),
     action_name_(name)
     {
         as_.start();
+
+        sub_ = nh_.subscribe("/odometry/filtered", 1, &DepthHoldAction::stateEstimateCallback, this);
+
+        pub_ = nh_.advertise<geometry_msgs::Wrench>("heave_input", 1);
+        double dt = 0.1;
+        double max = 40.0;
+        double min = -40.0;
+        double K_p = 1.5;
+        double K_d = 0.13;
+        double K_i = 0.05;
+
+        height.reset(new DHpid(dt, max, min, K_p, K_d, K_i));
     }
+
 
 DepthHoldAction::~DepthHoldAction()
 {
@@ -18,6 +33,24 @@ DepthHoldAction::~DepthHoldAction()
 
 void DepthHoldAction::executeCB(const depth_hold_action_server::DepthHoldGoalConstPtr &goal)
 {
+    geometry_msgs::Wrench dh_command;
+    ros::Rate rate(10);
+    dh_command.force.x = 0;
+    dh_command.force.y = 0;
+    dh_command.force.z = 0;
+
+    dh_command.torque.x = 0;
+    dh_command.torque.y = 0;
+    dh_command.torque.z = 0;
+
+    while(ros::ok()){
+        dh_command.force.z = -this->height->calculate();
+        std::cout << "Heave command" << dh_command.force.z << std::endl;
+        pub_.publish(dh_command);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
     // helper variables
     ros::Rate r(1);
     bool success = true;
@@ -56,4 +89,10 @@ void DepthHoldAction::executeCB(const depth_hold_action_server::DepthHoldGoalCon
         // set the action state to succeeded
         as_.setSucceeded(result_);
     }
+}
+
+void DepthHoldAction::stateEstimateCallback(const nav_msgs::Odometry &odometry_msgs){
+    double error = static_cast<double>(odometry_msgs.pose.pose.position.z)*(-1) - this->default_height;
+    std::cout <<"Error: "<<  error << std::endl;
+    this->height->updateError(error);
 }
