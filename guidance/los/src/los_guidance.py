6f59265ffe05dccb0aa6c912283ac45c6d35d356
erlendlundby <erlend.l.94@gmail.com>
2019-04-02 10:08:49 +0200

    Creating action server for waypoint coordination

--- guidance/los/src/los_guidance.py
+++ guidance/los/src/los_guidance.py
@@ -2,12 +2,16 @@
 import rospy
 from vortex_msgs.msg import PropulsionCommand
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Wrench
+from geometry_msgs.msg import Wrench, PoseStamped, Pose
 import numpy as np
 import math
+import actionlib
+from waypoint_action_msgs.msg import WaypointAction
 
 class LOS:
     def __init__(self):
+        self.waypoint_one = PoseStamped()
+        self.waypoint_two = PoseStamped()
         #Position
         self.x = 0.0
         self.y = 0.0
@@ -80,14 +84,28 @@ class LOS:
 class LosGuidanceNode(object):
     def __init__(self):
         rospy.init_node('los_guidance_node')
-
-        self.sub = rospy.Subscriber(
-            '/odometry/filtered', Odometry, self.callback, queue_size=1)
-        self.pub_motion = rospy.Publisher('yaw_input',
-                                          Wrench,
-                                          queue_size=1)
+        self.sub = rospy.Subscriber('/odometry/filtered', Odometry, self.callback, queue_size=1)
+        self.pub_motion = rospy.Publisher('yaw_input', Wrench, queue_size=1)
 
         self.los = LOS()
+        self.action_server = actionlib.SimpleActionServer('waypoint_coordination', WaypointAction, self.waypoint_action_server, False)
+        self.action_server.start()
+
+    def waypoint_action_server(self, goal):
+
+        self.los.x_kp1 = goal.pose.pose.position.x
+        self.los.y_kp1 = goal.pose.pose.position.y
+
+        if goal.is_first_wp:
+            odom_msg = rospy.wait_for_message('/odometry/filtered', Odometry)
+            self.los.x_k = odom_msg.pose.pose.position.x
+            self.los.y_k = odom_msg.pose.pose.position.y
+
+        else:
+            self.los.x_k = self.los.x_kp1
+            self.los.y_k = self.los.y_kp1
+        print goal
+        self.action_server.set_succeeded()
 
     def callback(self, msg):
         #print('Message recieved LOS LOS')
@@ -96,7 +114,7 @@ class LosGuidanceNode(object):
         self.los.updatePosition(msg.pose.pose.position.x, msg.pose.pose.position.y, self.heading)
 
         self.heading_d = self.los.LOSG()
-        #print(self.heading_d)
+        print(self.heading_d)
 
         self.error = self.heading-self.heading_d
         self.norm_error = self.error/(abs(self.error)+1)
@@ -106,28 +124,7 @@ class LosGuidanceNode(object):
         print(motion_msg)
         self.pub_motion.publish(motion_msg)
         #motion_msg.force
-        '''
-        motion_msg.motion = [
-            0,     # Surge
-            0,  # Sway
-            0,          # Heave
-            0,      # Roll
-            0,   # Pitch
-            10*self.norm_error  # Yaw
-        ]
-
-        motion_msg.control_mode = [
-            (False),
-            (False),
-            (False),
-            (False),
-            (False),
-            (False)
-        ]
-
-        motion_msg.header.stamp = rospy.get_rostime()
-        self.pub_motion.publish(motion_msg)
-        '''
+  
 
 
 if __name__ == '__main__':
--- pathplanning/waypoint_coordinator/src/waypoint_coordinator.py
+++ pathplanning/waypoint_coordinator/src/waypoint_coordinator.py
@@ -3,8 +3,9 @@ from nav_msgs.msg import Path, Odometry
 from nav_msgs.srv import GetPlan
 import copy
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, Pose
-from waypoint_msgs.msg import Waypoint
+from waypoint_action_msgs.msg import WaypointAction, WaypointGoal
 import rospy
+import actionlib
 
 class PathManager(object):
     def __init__(self):
@@ -12,9 +13,25 @@ class PathManager(object):
         self.start = PoseStamped()
         self.goal = PoseStamped()
         self.waypoint_counter = 0
-        self.is_last_waypoint = False
+    def path_generator_pole_client(self):
+        start = PoseStamped()
+        
+        tolerance = 1.0
+        print "Venter paa path"
+        rospy.wait_for_service('path_generator_pole')
+        
+        try:
+            
+            path_generator_pole = rospy.ServiceProxy('path_generator_pole', GetPlan)
 
+            response = path_generator_pole(self.start,self.goal,tolerance)
+            self.global_path =  response.plan
+            print "path motatt"
 
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+        
 
 class WaypointCoordinator(object):
     def __init__(self):
@@ -23,31 +40,40 @@ class WaypointCoordinator(object):
         self.path_manager = PathManager()
         self.rate = rospy.Rate(10)
         
-        
-    def path_generator_pole_client(self):
-        start = PoseStamped()
-        
-        tolerance = 1.0
-        print "Venter her"
-        rospy.wait_for_service('path_generator_pole')
-        
-        try:
+
+    def waypoint_action_client(self):
+        waypoint_client =  actionlib.SimpleActionClient('waypoint_coordination', WaypointAction)
+        waypoint_client.wait_for_server()
+        goal = WaypointGoal()
+        goal
+        goal.is_last_wp = False
+        goal.is_first_wp = False
+        no_of_wp = self.path_manager.global_path.poses.__len__()
+        print no_of_wp
+
+        while self.path_manager.global_path.poses.__len__() != 0:
+            print self.path_manager.global_path.poses.__len__() != 0
+            print self.path_manager.global_path.poses.__len__()
+            if no_of_wp == self.path_manager.global_path.poses.__len__():
+                goal.is_first_wp = True
+            else:
+                goal.is_first_wp = False
+
+            goal.pose = self.path_manager.global_path.poses.pop(0)
+
+            if self.path_manager.global_path.poses.__len__() == 0:
+                goal.is_last_wp = True
+            print(goal)
+            waypoint_client.send_goal(goal)
+            print "goal sent"
+            waypoint_client.wait_for_result(rospy.Duration.from_sec(5.0))
+        print "ferri"
             
-            path_generator_pole = rospy.ServiceProxy('path_generator_pole', GetPlan)
-
-            response = path_generator_pole(self.path_manager.start,self.path_manager.goal,tolerance)
-            self.path_manager.global_path =  response.plan
-            print "motatt"
-
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
-
-
     def publish(self):
         while not rospy.is_shutdown():
             print WP_coord_node.path_manager.global_path.poses
             self.pub_path.publish(self.path_manager.global_path)
-            self.rate.sleep()
+            self.rate.sleep( )
 
 
 
@@ -56,8 +82,9 @@ class WaypointCoordinator(object):
 if __name__ == '__main__':
     try: 
         WP_coord_node = WaypointCoordinator()
-        WP_coord_node.path_generator_pole_client()
-        WP_coord_node.publish()
+        WP_coord_node.path_manager.path_generator_pole_client()
+        WP_coord_node.waypoint_action_client()
+        #WP_coord_node.publish()
         while not rospy.is_shutdown():
             WP_coord_node.rate.sleep()
 
