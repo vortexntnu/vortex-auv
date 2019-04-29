95aa026ae8279c5590cb317e3f96945488d3741a
EspenWinther <espeneilertsen1@gmail.com>
2019-04-29 13:40:30 +0200

    Add shell for pool test

--- missionplanning/state_machine/src/Fibetest.py
+++ missionplanning/state_machine/src/Fibetest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
+<<<<<<< HEAD
+=======
+#import roslib; roslib.load_manifest('smach_tutorials')
+>>>>>>> 4840e48e1310dfb34d812c54fc44dd4746678021
 import rospy
 import smach
 import smach_ros
@@ -19,6 +23,7 @@ def action_client():
     goal = DepthHoldGoal(depth = 2)
     client.send_goal(goal)
     ready = client.get_state()
+<<<<<<< HEAD
     #client.wait_for_result()
     return client
 
@@ -51,11 +56,27 @@ class Dive(smach.State):
         # turn on diving stuff and do that shit
         checksignal() #check if the ship has recieved a stop signal
         if self.client.get_state[1]:
+=======
+
+    #client.wait_for_result()
+    return client
+
+class Dive(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['submerged','going'])
+        self.client = action_client()
+
+    def execute(self, userdata):
+        print('diving')
+        if self.client.get_state[1]:
+            print('Done')
+>>>>>>> 4840e48e1310dfb34d812c54fc44dd4746678021
             return 'submerged'
         else:
             return 'going'
 
 
+<<<<<<< HEAD
 class Cancel(smach.State)
     def __init__(self):
         smach.State.__init__(self, outcomes=['canceld'])
@@ -66,13 +87,22 @@ class Cancel(smach.State)
 
 
 class Search(smach.State):
+=======
+class Heading(smach.State):
+>>>>>>> 4840e48e1310dfb34d812c54fc44dd4746678021
     def __init__(self):
         smach.State.__init__(self, outcomes=['correct_heading'])
         result = action_client(order = 20)
 
+<<<<<<< HEAD
     def execute(self):
         if gate_found:
             return 'found'
+=======
+    def execute(self, userdata):
+        if heading == ref:
+            return 'correct_heading'
+>>>>>>> 4840e48e1310dfb34d812c54fc44dd4746678021
 
 
 class Center(smach.State):
@@ -85,7 +115,11 @@ class Center(smach.State):
                                           bool,queue_size=1000)
         result = action_client(order = 20)
 
+<<<<<<< HEAD
     def execute(self):
+=======
+    def execute(self, userdata):
+>>>>>>> 4840e48e1310dfb34d812c54fc44dd4746678021
         rospy.loginfo('Centering')
         if sub_gate == 1:
             return 'Centered'
@@ -96,6 +130,7 @@ def main():
     sm = smach.StateMachine(outcomes = ['Done'])
 
     with sm:
+<<<<<<< HEAD
         smach.StateMachine.add('Idle', idle(),
                                 transitions={'doing':'Dive', 'wating':'Idle'})
         smach.StateMachine.add('Dive', Dive(),
@@ -103,6 +138,14 @@ def main():
         smach.StateMachine.add('Cancel', Cancel(),
                                 transitions={'canceld':'Idle'})
 
+=======
+        smach.StateMachine.add('Dive', Dive(),
+                                transitions={'submerged':'Done', 'going':'Dive'})
+        #smach.StateMachine.add('Heading', Heading(),
+        #                        transitions={'correct_heading':'Center'})
+        #smach.StateMachine.add('Center', Center(),
+        #                        transitions={'Centered':'Done'})
+>>>>>>> 4840e48e1310dfb34d812c54fc44dd4746678021
 
     sis = smach_ros.IntrospectionServer('Fibtest_test_server', sm, '/SM_ROOT')
     sis.start()
