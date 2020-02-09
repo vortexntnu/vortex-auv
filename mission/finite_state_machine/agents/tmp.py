a5f605e5e7893d7265e05e0d13bc4bfe334a56bd
Michael Hoyer <michael.hoyer3@gmail.com>
2020-02-09 19:03:38 +0100

    temporary sm for testing

---
+++ mission/finite_state_machine/agents/tmp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import rospy
+from smach import StateMachine, Sequence
+from smach_ros import SimpleActionState
+from geometry_msgs.msg import Pose, Point, Quaternion
+from vortex_msgs.msg import MoveGoal, MoveAction
+from tf.transformations import quaternion_from_euler
+
+
+def dp_move(x, y, z=-1, yaw=0):
+    goal = MoveGoal()
+
+    goal.controller_name = 'DP'
+    goal.target_pose.position = Point(x, y, z)
+    goal.target_pose.orientation = Quaternion(*quaternion_from_euler(0, 0, yaw))
+
+    return SimpleActionState('move', MoveAction, goal=goal)
+
+
+def los_move(x, y, z=-1):
+    goal = MoveGoal()
+
+    goal.controller_name = 'LOS'
+    goal.target_pose.position = Point(x, y, z)
+
+    return SimpleActionState('move', MoveAction, goal=goal)
+
+
+rospy.init_node('the_great_testing_node')
+
+sm = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome='succeeded')
+
+with sm:
+
+    Sequence.add('TWO', los_move(-5, 0))
+    Sequence.add('ONE', dp_move(1, 0))
+    Sequence.add('THREE', dp_move(2, 0))
+
+sm.execute()
