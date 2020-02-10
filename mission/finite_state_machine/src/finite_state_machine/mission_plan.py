1d8170fedb64f45765c3503c36795412a504bb5e
Michael Hoyer <michael.hoyer3@gmail.com>
2020-02-10 15:00:49 +0100

    another small fix

--- mission/finite_state_machine/src/finite_state_machine/mission_plan.py
+++ mission/finite_state_machine/src/finite_state_machine/mission_plan.py
@@ -55,5 +55,5 @@ def make_waypoint(x, y, z=-0.5, yaw_euler=0):
 	yaw_quat = Quaternion(*quaternion_from_euler(0, 0, yaw_euler, 'sxyz'))
 	waypoint = Pose(Point( x, y, z), yaw_quat)
 
-	return (name, waypoint)
+	return waypoint
 
