d0b9c4d3b6010d138a7bdf75571ca92025cfe0f7
Michael Hoyer <michael.hoyer3@gmail.com>
2020-02-12 12:11:55 +0100

    Nice waypoints to drive to

--- mission/finite_state_machine/agents/pooltest.py
+++ mission/finite_state_machine/agents/pooltest.py
@@ -35,8 +35,8 @@ sm = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome=
 with sm:
 
     Sequence.add('ONE', dp_move(1, 0))
-    Sequence.add('TWO', los_move(-7, 0))
-    Sequence.add('THREE', los_move(0, 0))
-    Sequence.add('FOUR', dp_move(2, 0))
+    Sequence.add('TWO', los_move(4, 0))
+    Sequence.add('THREE', los_move(1, 0))
+    Sequence.add('FOUR', dp_move(0, 0))
 
 sm.execute()
