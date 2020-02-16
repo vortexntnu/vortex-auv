87d4a70efbe1e5b1b0d83233d313d295dcfa18ee
Michael Hoyer <michael.hoyer3@gmail.com>
2020-02-16 16:39:01 +0100

    Creates patrol sequence in helper file

--- mission/finite_state_machine/agents/helper.py
+++ mission/finite_state_machine/agents/helper.py
@@ -3,6 +3,7 @@
 
 import rospy
 from smach_ros import SimpleActionState
+from smach import StateMachine, Sequence
 from geometry_msgs.msg import Point, Quaternion
 from vortex_msgs.msg import MoveGoal, MoveAction
 from tf.transformations import quaternion_from_euler
@@ -24,3 +25,18 @@ def los_move(x, y, z=-0.5):
     goal.target_pose.position = Point(x, y, z)
 
     return SimpleActionState('move', MoveAction, goal=goal)
+
+
+def patrol_sequence(states):
+
+    sm = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome='succeeded')
+    counter = 0
+
+    with sm:
+
+        for state in states:
+            counter = counter + 1
+            sm.add("State-%d" %counter, state)
+
+    return sm
+
--- mission/finite_state_machine/agents/pooltest.py
+++ mission/finite_state_machine/agents/pooltest.py
@@ -5,27 +5,23 @@ import rospy
 from smach import StateMachine, Sequence
 from std_msgs.msg import String
 from math import pi
-from helper import dp_move, los_move
+from helper import dp_move, los_move, patrol_sequence
 
 
 rospy.init_node('the_great_testing_node')
 thruster_armer = rospy.Publisher('/mcu_arm', String, queue_size=10)
 
-sm = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome='succeeded')
-
-with sm:
-
-    # Add desired waypoints (including controller choice) here
-    Sequence.add('ONE', dp_move(0, 0, yaw_rad=pi))
-    Sequence.add('TWO', los_move(4, 0))
-    Sequence.add('THREE', los_move(1, 0))
-    Sequence.add('FOUR', dp_move(0, 0, yaw_rad=pi))
-
+patrol = patrol_sequence([
+    dp_move(0, 0, yaw_rad=pi),
+    los_move(4, 0),
+    los_move(1, 0),
+    dp_move(0, 0, yaw_rad=pi)
+])
 
 try:
 
     thruster_armer.publish("data: 'arm'")   # thrusters must be armed before use
-    sm.execute()
+    patrol.execute()
     thruster_armer.publish("data: 'disarm'")   
 
 except:
