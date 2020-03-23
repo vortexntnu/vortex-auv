341de6c0ae81a7a10c2836ff22ecbe7c54130894
Michael Hoyer <michael.hoyer3@gmail.com>
2020-03-23 13:39:06 +0100

    work in progress 20.3

--- mission/finite_state_machine/agents/helper.py
+++ mission/finite_state_machine/agents/helper.py
@@ -2,11 +2,13 @@
 # coding: UTF-8
 
 import rospy
+from smach import StateMachine, Sequence, Concurrence, cb_interface, CBState
 from smach_ros import SimpleActionState
-from smach import StateMachine, Sequence
 from geometry_msgs.msg import Point, Quaternion
 from vortex_msgs.msg import MoveGoal, MoveAction
 from tf.transformations import quaternion_from_euler
+
+
 
 def dp_move(x, y, z=-0.5, yaw_rad=0):
     goal = MoveGoal()
@@ -27,6 +29,12 @@ def los_move(x, y, z=-0.5):
     return SimpleActionState('move', MoveAction, goal=goal)
 
 
+def circle_move(target_point, direction):
+    goal = MoveGoal()
+
+    goal.controller_name = ''
+
+
 def patrol_sequence(action_states):
 
     sm = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome='succeeded')
@@ -40,3 +48,49 @@ def patrol_sequence(action_states):
 
     return sm
 
+
+@cb_interface(
+    outcomes=['alligned', 'wrong_direction'],
+    input_keys=['alligned_pose'],
+    output_keys=['alligned_pose']
+)
+def allignment_checker(userdata):
+    # TODO
+    pass
+
+
+def allign_with_target(target):
+    """
+    Returns a state that alligns with the specified target. 
+
+    target: string on the form 'GATE' or 'BOUY'
+
+    The returned state is responsible for chaning the circeling direction when needed.
+
+    """
+
+    # TODO: get target position (x,y) from landmark server
+
+    # TODO: create a move_goal
+    move_goal = None
+
+    allignment_attempt = Concurrence(
+        outcomes=['succeeded', 'preempted', 'wrong_direction'],
+        outcome_map={
+            'succeeded': {'ALLIGNMENT_CHECKER': 'alligned'},
+            'wrong_direction': {'ALLIGNMENT_CHECKER': 'wrong_direction'}
+        },
+        default_outcome=['preempted'],
+        child_termination_cb=None   # TODO: should allways terminate
+    )
+
+    with allignment_attempt:
+
+        Concurrence.add(
+            'CIRCLE_GATE', 
+            SimpleActionState('move', MoveAction, goal=move_goal) # TODO
+        )
+        Concurrence.add(
+            'ALLIGNMENT_CHECKER', 
+            CBState(allignment_checker)
+        )
