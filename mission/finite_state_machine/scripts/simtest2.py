4699b670801d703363f57ae610c58a5b7b5838d0
Håvard Eiring <havard.eiring@gmail.com>
2020-11-05 20:26:39 +0100

    added MOVE_THROUGH_GATE state

--- mission/finite_state_machine/scripts/simtest2.py
+++ mission/finite_state_machine/scripts/simtest2.py
@@ -3,7 +3,7 @@
 import rospy
 from smach import Sequence
 import smach
-from helper import dp_move
+from helper import dp_move, los_move
 from smach_ros import IntrospectionServer, SimpleActionState
 from geometry_msgs.msg import Point
 from vortex_msgs.msg import MoveAction, MoveGoal
@@ -13,46 +13,46 @@ from sm_classes import GateSearchState, FooState
 def main():
     rospy.init_node('simulator_state_machine')
 
-    simulator_state_machine = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome='succeeded')        
+    simulator_state_machine = Sequence(outcomes=['preempted', 'succeeded', 'aborted'], 
+                                        connector_outcome='succeeded')        
 
     with simulator_state_machine:
 
-        Sequence.add('REACH_DEPTH',dp_move(0,0),transitions={'succeeded':'GATE_SM'})
+        Sequence.add('REACH_DEPTH',
+                        dp_move(0,0),
+                        transitions={'succeeded':'GATE_SM'})
 
-        gate_sm = smach.Sequence(outcomes=['preempted', 'succeeded', 'aborted'], connector_outcome='succeeded')
+        gate_sm = smach.Sequence(outcomes=['preempted', 'succeeded', 'aborted'], 
+                                            connector_outcome='succeeded')
 
         gate_sm.userdata.goal_position = Point(None,None,None)        
 
         with gate_sm:
-            def gate_goal_cb(userdata, goal):
-                gate_goal = MoveGoal()
-                gate_goal.target_pose.position = userdata.goal_position                
-                gate_goal.controller_name = "LOS"
-                return gate_goal
 
             Sequence.add('GATE_SEARCH',
                             GateSearchState(),
                             remapping={'gate_search_output':'goal_position'})        
                         
+            def gate_goal_cb(userdata, goal):
+                gate_goal = MoveGoal()
+                gate_goal.target_pose.position = userdata.goal_position                
+                gate_goal.controller_name = "LOS"
+                return gate_goal
+            
             Sequence.add('LOS_MOVE_TO_GATE',
                             SimpleActionState('move',
                                                 MoveAction,
                                                 goal_cb=gate_goal_cb,
                                                 input_keys=['goal_position']),
                             remapping={'goal_position':'goal_position'})
-
-            Sequence.add('FOO_STATE',
-                            FooState(),
-                            remapping={'foo_state_input':'goal_position'})
-            
-
-        Sequence.add('GATE_SM',gate_sm)
-
+                        
+            Sequence.add('LOS_MOVE_THROUGH_GATE',
+                            los_move(8,1.5))                  
+        
+        Sequence.add('GATE_SM',gate_sm)  
     
-    
-    intro_server = IntrospectionServer(str(rospy.get_name()), simulator_state_machine,'/SM_ROOT')
+    intro_server = IntrospectionServer(str(rospy.get_name()), simulator_state_machine,'/SM_ROOT')    
     intro_server.start()
-
 
     try:
         simulator_state_machine.execute()
@@ -60,7 +60,6 @@ def main():
 
     except Exception as e:
         rospy.loginfo("Pooltest failed: %s" % e)
-
 
 
 
