51738ea4d9570e6ecd5fc19bb1611638a1e31b91
Christopher Strøm <chstr@stud.ntnu.no>
2020-11-07 20:50:03 +0100

    Add Doxygen docs for member variables and vector calculation functions
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- motion/dp_controller/include/dp_controller/quaternion_pd_controller.h
+++ motion/dp_controller/include/dp_controller/quaternion_pd_controller.h
@@ -1,11 +1,18 @@
 /*   Written by Kristoffer Rakstad Solberg, Student
+     Documentation written by Jae Hyeong Hwang and
+     copied to Doxygen format by Christopher Strøm
      Copyright (c) 2019 Manta AUV, Vortex NTNU.
      All rights reserved. */
 
-// An implementation of
-// Fjellstad & Fossen 1994: Quaternion Feedback Regulation of Underwater Vehicles,
-// a nonlinear PD position and orientation controller.
-// All variables are named as they appear in the paper.
+/**
+ * @file
+ * @brief A nonlinear PID controller for position and orientation
+ * 
+ * An implementation of Fjellstad & Fossen 1994: Quaternion
+ * Feedback Regulation of Underwater Vehicles, a nonlinear PD
+ * position and orientation controller. All variables are named
+ * as they appear in the paper.
+*/
 
 #ifndef VORTEX_CONTROLLER_QUATERNION_PD_CONTROLLER_H
 #define VORTEX_CONTROLLER_QUATERNION_PD_CONTROLLER_H
@@ -55,24 +62,54 @@ private:
   Eigen::Matrix6d integralGainMatrix(const Eigen::Matrix3d R);
   void integralWindUp(Eigen::Vector6d &vec, double pose_lim, double att_lim);
 
+  /**
+   * @brief Calculate error vector.
+   * 
+   * The quaternion error is given by the product of desired state q_d and
+   * current state q
+   * 
+   * @param p
+   * @param p_d
+   * 
+   * @param q
+   * @param q_d
+   * 
+   * @return a 6x1 vector consisting of the error in position and attitudes. 
+  */
   Eigen::Vector6d errorVector(const Eigen::Vector3d    &p,
                               const Eigen::Vector3d    &p_d,
                               const Eigen::Quaterniond &q,
                               const Eigen::Quaterniond &q_d);
 
+  /**
+   * @brief calculate the restoring force vector
+   * 
+   * @param R   Rotation matrix from inertial to body frame
+   * 
+   * @return The restoring force vector
+  */
   Eigen::Vector6d restoringForceVector(const Eigen::Matrix3d R);
-  int             sgn(double x);
 
-  double m_c;             // Orientation gain
-  double m_c_i;           // Orientation integral gain
-  Eigen::Matrix6d m_K_d;  // Derivative gain matrix
-  Eigen::Matrix3d m_K_x;  // Position error gain matrix
-  Eigen::Matrix3d m_K_i;  // Integral gain matrix
+  /**
+   * @brief The mathematical sign-operator
+   * 
+   * @param x   The value whos sign is to be determined
+   * 
+   * @return -1 if @p x is negative, and 1 if @p x is zero or greater
+   * 
+  */
+  int sgn(double x);
 
-  Eigen::Vector3d m_r_G;  // Center of gravity, expressed in body frame
-  Eigen::Vector3d m_r_B;  // Center of buoyancy, expressed in body frame
-  double m_W;             // [N] Weight of ROV
-  double m_B;             // [N] Buoyancy of ROV
+  double m_c;             /** Orientation gain            */
+  double m_c_i;           /** Orientation integral gain   */
+  Eigen::Matrix6d m_K_d;  /** Derivative gain matrix      */
+  Eigen::Matrix3d m_K_x;  /** Position error gain matrix  */
+  Eigen::Matrix3d m_K_i;  /** Integral gain matrix        */
+
+  Eigen::Vector3d m_r_G;  /** Center of gravity, expressed in body frame  */
+  Eigen::Vector3d m_r_B;  /** Center of buoyancy, expressed in body frame */
+  double m_W;             /** [N] Weight of drone                         */
+  double m_B;             /** [N] Buoyancy of drone                       */
 
 };
 
