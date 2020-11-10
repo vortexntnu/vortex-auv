109f8c29d645e0ae54e6e7d40696fb5622f86164
Christopher Strøm <chstr@stud.ntnu.no>
2020-11-10 03:02:45 +0100

    Add documentation to eigen helper header
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- motion/dp_controller/include/dp_controller/eigen_helper.h
+++ motion/dp_controller/include/dp_controller/eigen_helper.h
@@ -8,7 +8,13 @@
 #include <string>
 #include <vector>
 
-// Return false if X has any nan or inf elements.
+/**
+ * @brief check if a matrix is valid (no nan or inf entries)
+ * 
+ * @param X   The matrix to be checked
+ * 
+ * @return true if X has any nan or inf elements, false otherwise
+*/
 template<typename Derived>
 inline bool isFucked(const Eigen::MatrixBase<Derived>& X)
 {
@@ -17,8 +23,16 @@ inline bool isFucked(const Eigen::MatrixBase<Derived>& X)
   return has_nan || has_inf;
 }
 
-// Read a matrix from the ROS parameter server.
-// Return false if unsuccessful.
+
+/**
+ * @brief read a matrix from the ROS parameter server
+ * 
+ * @param nh    ROS NodeHandle
+ * @param name  Parameter name of the matrix in the parameter server
+ * @param X     The matrix that will be filled with the values read from the parameter server
+ * 
+ * @return false if unsuccessful, true if successful
+*/
 inline bool getMatrixParam(ros::NodeHandle nh, std::string name, Eigen::MatrixXd *X)
 {
   XmlRpc::XmlRpcValue matrix;
@@ -40,6 +54,12 @@ inline bool getMatrixParam(ros::NodeHandle nh, std::string name, Eigen::MatrixXd
   return true;
 }
 
+/**
+ * @brief print Eigen matrix
+ * 
+ * @param name    Name of matrix
+ * @param X       Matrix
+*/
 inline void printEigen(std::string name, const Eigen::MatrixXd &X)
 {
   std::stringstream ss;
@@ -47,8 +67,15 @@ inline void printEigen(std::string name, const Eigen::MatrixXd &X)
   ROS_INFO_STREAM(ss.str());
 }
 
-// Calculate the pseudoinverse matrix of the matrix X.
-// Return false if the calculations fails.
+
+/**
+ * @brief calculate pseudoinverse matrix
+ * 
+ * @param X         The matrix to be inverted
+ * @param X_pinv    The inverted version of the @p X matrix
+ * 
+ * @return false if the calculation fails, true otherwise
+*/
 inline bool pseudoinverse(const Eigen::MatrixXd &X, Eigen::MatrixXd *X_pinv)
 {
   Eigen::MatrixXd copy = X.transpose() * (X * X.transpose()).inverse();
@@ -61,7 +88,14 @@ inline bool pseudoinverse(const Eigen::MatrixXd &X, Eigen::MatrixXd *X_pinv)
   return true;
 }
 
-// Return the 3-by-3 skew-symmetric matrix of the vector v.
+
+/**
+ * @brief calculate a 3x3 skew-symmetric matrix
+ * 
+ * @param v   The vector from which the matrix is to be calculated
+ * 
+ * @return the 3x3 skew-symmetric matrix of @p v
+*/
 inline Eigen::Matrix3d skew(const Eigen::Vector3d &v)
 {
   Eigen::Matrix3d S;
@@ -71,6 +105,14 @@ inline Eigen::Matrix3d skew(const Eigen::Vector3d &v)
   return S;
 }
 
+
+/**
+ * @brief Convert a vector to a ThrusterForces vortex message
+ * 
+ * @param u     The Eigen::vector to be converted
+ * @param msg   The vortex_msgs output message  
+ * 
+*/
 inline void arrayEigenToMsg(const Eigen::VectorXd &u, vortex_msgs::ThrusterForces *msg)
 {
   int r = u.size();
@@ -80,8 +122,18 @@ inline void arrayEigenToMsg(const Eigen::VectorXd &u, vortex_msgs::ThrusterForce
   msg->thrust = u_vec;
 }
 
-// Saturate all elements of vector v to within [min, max].
-// Return true if all elements already are within the range.
+
+/**
+ * @brief saturate a vector to a given range
+ * 
+ * @param v     The vector to be saturated
+ * @param min   Lower saturation limit
+ * @param max   Upper saturation limit
+ * 
+ * @return true if all elements are in the [ @p min @p max ] range already
+ * false otherwise
+ * 
+*/
 inline bool saturateVector(Eigen::VectorXd *v, double min, double max)
 {
   bool vector_in_range = true;
