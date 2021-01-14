7dd3377383a6a436530a9c2a01236ec10ad58932
JAE HYEONG HWANG <71170784+jae-hyeong-hwang@users.noreply.github.com>
2021-01-14 13:41:59 +0100

    Update reference_model.h
    deleted class, changed it into double type function

--- motion/dp_controller/include/dp_controller/reference_model.h
+++ motion/dp_controller/include/dp_controller/reference_model.h
@@ -8,12 +8,6 @@
 #include <math.h>
 using namespace Eigen;
 
-class QuaternionPdController
-{
-public:
-	ReferenceModel(const Eigen::Vector3d &r_G, const Eigen::Vector3d &r_B)
-	Eigen::Vector3d ReferenceModel(const Eigen::Vector3d   &x,
-                                   const Eigen::Vector3d   &x_ref);
-};
+double ReferenceModel(const Egien::Vector3d &x, const Eigen::Vector3d &x_ref);
 
 #endif  // VORTEX_CONTROLLER_REFERENCE_MODEL_H
