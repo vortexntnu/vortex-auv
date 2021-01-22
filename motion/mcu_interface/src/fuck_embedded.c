d793a3958ec47f45d84bd836c5989fca228f6168
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-01-22 15:04:49 +0100

    Some duplicated and some outcommented code, but it compiles on the arduino and downloads onto the mcu
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- motion/mcu_interface/src/fuck_embedded.c
+++ motion/mcu_interface/src/fuck_embedded.c
@@ -1,5 +1,5 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/delay.h>
 #include <avr/interrupt.h>
 
 /* Defines to quicly change between the thrusters. Could use something like this in the future */
@@ -12,11 +12,17 @@
 #define THR6 0x128
 #define THR7 0x12A
 
+
+#ifndef F_CPU
+  #define F_CPU 16000000UL
+#endif /* F_CPU */
+
+
 /* Setting clock */
 /* 16MHz */
-CKSEL = 1111;
-SUT = 11;
-__no_operation();
+// CKSEL = 1111;
+// SUT = 11;
+// __no_operation();
 
 
 /* See tables 22-10 to 22-12 for baudrate */
@@ -83,7 +89,7 @@ TCNT5 = 0;
 
 
 /* Inserting nop for system-sync */
-__no_operation();
+// __no_operation();
 
 /* Function to map the wanted Duty-cycle to counting-variable */
 /* Variables used: 
@@ -114,4 +120,85 @@ int main(void){
     }
   }
   return 0;
+}
+
+
+/******************************************************************************
+ * 
+ * The following code is tried and tested on the MCU
+ * 
+ * It compiles, and both of the leds flashes
+ * 
+ * 
+ ******************************************************************************/
+
+#include<avr/io.h>
+#include<avr/delay.h>
+void setup()
+{
+  /* Clear ports */
+  DDRE = 0;
+  DDRH = 0;
+  DDRL = 0;
+  
+  
+  /* Setting ports as output */
+  DDRE |= (1 << PE3) | (1 << PE4) | (1 << PE5); 
+  DDRH |= (1 << PH1) | (1 << PH3) | (1 << PH4) | (1 << PH5);
+  DDRL |= (1 << PL3) | (1 << PL4);
+  
+  
+  /* Initializing ports as pull-down */
+  PORTE &= !((1 << PE3) | (1 << PE4) | (1 << PE5));  
+  PORTH &= !((1 << PH2) | (1 << PH1) | (1 << PH0));
+  PORTL &= !((1 << PL3) | (1 << PL4));
+  
+  
+  /* Init duty-cycles to 0 */
+  OCR3A = 0; /* Duty-cycle THR0 */
+  OCR3B = 0; /* Duty-cycle THR1 */ 
+  OCR3C = 0; /* Duty-cycle THR2 */ 
+  
+  OCR4A = 0; /* Duty-cycle THR3 */ 
+  OCR4B = 0; /* Duty-cycle THR4 */ 
+  OCR4C = 0; /* Duty-cycle THR5 */ 
+  
+  OCR5A = 0; /* Duty-cycle THR6 */ 
+  OCR5B = 0; /* Duty-cycle THR7 */
+  
+  
+  /* Setting A to Fast PWM, clearing OCnA, OCnB and OCnC on compare match */
+  TCCR3A = (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM32) | (1 << WGM31) | (1 << WGM30);
+  TCCR4A = (1 << COM4A1) | (1 << COM4B1) | (1 << COM4C1) | (1 << WGM42) | (1 << WGM41) | (1 << WGM40);
+  TCCR5A = (1 << COM5A1) | (1 << COM5B1) | (1 << COM5C1) | (1 << WGM52) | (1 << WGM51) | (1 << WGM50);
+  
+  
+  /* Setting clock-source to 1/1024 * clk. See table 17-6 */
+  TCCR3B = (1 << CS32) | (1 << CS30);
+  TCCR4B = (1 << CS42) | (1 << CS40);
+  TCCR4B = (1 << CS52) | (1 << CS50);
+  
+  
+  /* Enabling timers */
+  PRR1 &= !((1 << PRTIM3) | (1 << PRTIM4) | (1 << PRTIM5));
+  
+  
+  /* Enabling global interrupt */
+  SREG |= 0x7;
+  
+  
+  /* Init timers */
+  TCNT3 = 0;
+  TCNT4 = 0;
+  TCNT5 = 0;
+
+  /* Test with LED */
+  DDRF = 0xFF;
+}
+void loop()
+{
+    PORTF = 0xAA;
+    _delay_ms(1000);
+    PORTF = 0x55;
+    _delay_ms(1000);
 }
