b14526f267bca2688e0abd9495a37342f6ffe408
Håvard Eiring <havard.eiring@gmail.com>
2021-01-23 12:30:19 +0100

    defined landmarks methods

--- mission/landmarks/src/landmarks.cpp
+++ mission/landmarks/src/landmarks.cpp
@@ -1,42 +1,29 @@
-#include "ros/ros.h"
-#include "geometry_msgs/Point.h"
-#include <vortex_msgs/ObjectPosition.h>
-#include <map>
-#include <string>
+#include "landmarks.h"
 
-class Landmarks{
-public:
-    Landmarks ():loop_rate(10) {
-        op_sub = n.subscribe("object_positions_in",10, &Landmarks::callback, this);
-        op_pub = n.advertise<vortex_msgs::ObjectPosition>("object_positions_out",10);
-    }
+Landmarks::Landmarks ():loop_rate(10) {
+    op_sub = n.subscribe("object_positions_in",10, &Landmarks::callback, this);
+    op_pub = n.advertise<vortex_msgs::ObjectPosition>("object_positions_out",10);
+}
 
-    void callback(vortex_msgs::ObjectPosition objPos){
-        objectPositions[objPos.objectID] = objPos.position;
-        op_pub.publish(objPos);        
-    }
+void Landmarks::callback(vortex_msgs::ObjectPosition objPos){
+    objectPositions[objPos.objectID] = objPos.position;
+    op_pub.publish(objPos);            
+}
 
-    void execute(){
-        while (ros::ok()){
-            ros::spinOnce();
-            loop_rate.sleep();
-        }
+void Landmarks::execute(){
+    while (ros::ok()){
+        ros::spinOnce();
+        loop_rate.sleep();
     }
-    void printMap(std::map<std::string,geometry_msgs::Point> objectsMap){
-        for(auto elem : objectsMap){
-            ROS_INFO("ID: %s", elem.first.c_str());
-            ROS_INFO("position: %f,%f,%f",elem.second.x,elem.second.y,elem.second.z);
+}
+
+void Landmarks::printMap(std::map<std::string,geometry_msgs::Point> objectsMap){
+    for(auto elem : objectsMap){
+        ROS_INFO("ID: %s", elem.first.c_str());
+        ROS_INFO("position: %f,%f,%f",elem.second.x,elem.second.y,elem.second.z);
             
-        }
     }
-
-protected:
-    ros::NodeHandle n;
-    ros::Subscriber op_sub;
-    ros::Publisher op_pub;
-    ros::Rate loop_rate;
-    std::map<std::string,geometry_msgs::Point> objectPositions;
-};
+}
 
 int main(int argc, char **argv){
     ros::init(argc,argv,"landmarks");
