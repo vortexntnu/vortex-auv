e3539a607565fc4334138a546115772a9f6a4bc9
Håvard Eiring <havard.eiring@gmail.com>
2021-01-23 12:37:28 +0100

    added class documentation

--- mission/landmarks/include/landmarks.h
+++ mission/landmarks/include/landmarks.h
@@ -5,6 +5,12 @@
 #include <string>
 
 class Landmarks{
+    /*
+    To serve as an interface between the perception system and the control system,
+    an instance of this class receives object positions ("landmarks") published by 
+    the perception system on a ROS-topic. The object positions are stored in a map
+    before they are published on a ROS-topic which the control system is subscribed to. 
+    */
 public:
     Landmarks();
     void callback(vortex_msgs::ObjectPosition objPos);
