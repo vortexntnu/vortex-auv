83b3679afc8b51c5b3fb83bbf099197c4e797a43
Håvard Eiring <havard.eiring@gmail.com>
2021-02-13 16:48:00 +0100

    working on reach depth test

--- mission/finite_state_machine/tests/src/sm_tests.py
+++ mission/finite_state_machine/tests/src/sm_tests.py
@@ -4,19 +4,30 @@ import rospy
 import rostest
 from nav_msgs.msg import Odometry
 from geometry_msgs import PoseWithCovariance, Pose, Point
+from std_msgs.msg import String
 from time import sleep
 
 class StateMachineTesting(unittest.TestCase):
+    def __init__(self):
+        rospy.init_node('sm_tester')
+        rospy.Subscriber('state_transition', String, self.transition_cb)
+        self.active_state = None
+    
+    def transition_cb(self,data):
+        print(data.data)
+        self.active_state = data.data
+
+
 
     def test_reach_depth(self):
-        rospy.init_node('sm_tester')
         odom_pub = rospy.Publisher('odometry/filtered', Odometry, queue_size=10)
         sleep(5)
-        odom_pub.Publish(Odometry(None,None,PoseWithCovariance(Pose(Point(0.0,0.0,-0.5),None),None),None))
+        odom_pub.publish(Odometry(None,None,PoseWithCovariance(Pose(Point(0.0,0.0,-0.5),None),None),None))
         sleep(1)
-        #self.assert
+        self.assertEqual(self.active_state,'REACH_DEPTH','The state machine is not in state: "REACH_DEPTH"')
 
 
 
 
 if __name__ == '__main__':
+    rostest.rosrun('finite_state_machine','sm_tester',StateMachineTesting)
