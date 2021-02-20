1002fd7810771e0a186f1670b9c07ce91975cdf2
Vortex-Hvit <vortex_hvit@gmail.com>
2021-02-20 22:22:58 +0100

    Added I2C functions.

--- motion/mcu_interface_new/include/mcu_interface_new/mcu_interface.h
+++ motion/mcu_interface_new/include/mcu_interface_new/mcu_interface.h
@@ -8,6 +8,8 @@
 #include <cmath>
 #include <vector>
 
+#include "mcu_interface_new/i2c.h"
+
 class MCU_Interface{
     public:
         MCU_Interface();
@@ -16,6 +18,8 @@ class MCU_Interface{
 
         void execute();
     private:
+        I2CDevice device;
+
         ros::NodeHandle nh;
         ros::Subscriber thruster_forces_sub;
         ros::Subscriber thruster_arm_sub;
@@ -31,7 +35,7 @@ class MCU_Interface{
         const int thrust_range_limit = 100;
 
         /** Callbacks */
-        void thruster_forces_cb(const vortex_msgs::ThrusterForces &msg) ;
+        void thruster_forces_cb(const vortex_msgs::ThrusterForces &msg);
         void thruster_arm_cb(const std_msgs::String &msg);
 
         /** Utility */
@@ -40,6 +44,8 @@ class MCU_Interface{
         double thrust_to_microseconds(const double thrust);
         void output_to_zero();
 
+        void i2c_init(int MCU_addr);
         void transfer_to_mcu(const std::vector<double> pwm);
+        void transfer_to_mcu(u_int8_t a_byte);
 
 };
--- motion/mcu_interface_new/src/mcu_interface.cpp
+++ motion/mcu_interface_new/src/mcu_interface.cpp
@@ -1,5 +1,13 @@
 #include "mcu_interface_new/mcu_interface.h"
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "mcu_interface_new/i2c.h"
+
+#include <time.h>
+#include <stdlib.h>
+
 MCU_Interface::MCU_Interface ():loop_rate(10){
     thruster_forces_sub = nh.subscribe("/thrust/thruster_forces", 10, &MCU_Interface::thruster_forces_cb, this);
     thruster_arm_sub = nh.subscribe("/thrust/arm", 10, &MCU_Interface::thruster_arm_cb, this);
@@ -52,7 +60,7 @@ void MCU_Interface::thruster_forces_cb(const vortex_msgs::ThrusterForces &msg) {
         microseconds.push_back(thrust_to_microseconds(thrust[i] + thruster_offset[i]));
     }
 
-    transfer_to_mcu(microseconds);
+    MCU_Interface::transfer_to_mcu(microseconds);
 }
 
 void MCU_Interface::thruster_arm_cb(const std_msgs::String &msg) {
@@ -102,11 +110,95 @@ double MCU_Interface::thrust_to_microseconds(const double thrust) {
     return 0;
 }
 
-void MCU_Interface::transfer_to_mcu(const std::vector<double> pwm) {
+void MCU_Interface::i2c_init(int MCU_addr) {
+    int bus;
+    //const char *data = "01110";
 
-    // Use I2C connection to transfer the values in the pwm vector
+    /* First open i2c bus */
+    if ((bus = i2c_open("/dev/i2c-8")) == -1) {
+
+        perror("Open i2c bus error");
+    }
+
+    /* Fill i2c device struct */
+    MCU_Interface::device.bus = bus;
+    MCU_Interface::device.addr = MCU_addr;
+    MCU_Interface::device.tenbit = 0;
+    MCU_Interface::device.delay = 10;
+    MCU_Interface::device.flags = 0;
+    MCU_Interface::device.page_bytes = 10;
+    MCU_Interface::device.iaddr_bytes = 0; /* Set this to zero, and using i2c_ioctl_xxxx API will ignore chip internal address */
 }
 
+void MCU_Interface::transfer_to_mcu(const std::vector<double> pwm) {
+// To compile and run all files:
+// cc -c ./i2c_test.c && cc ./i2c.o ./i2c_test.o -o main && ./main
+
+    std::vector<int> pwm_int_vec(pwm.begin(), pwm.end());
+    
+    int pwm_arr[num_thrusters];
+    std::copy(pwm_int_vec.begin(), pwm_int_vec.end(), pwm_arr);
+
+    const I2CDevice dev_1 = MCU_Interface::device;
+
+    char num_str[num_thrusters];
+    for (int i = 0; i < num_thrusters; i++) {
+        uint8_t tmp_int8 = 0.128 * pwm_arr[i] - 1;
+        num_str[i] = tmp_int8;
+    }
+
+    if (i2c_ioctl_write(&dev_1, 0x0, num_str, strlen(num_str)) != strlen(num_str))
+    {
+        /* Error process */
+    }
+    // for (u_int16_t i = 9000; i < 100000; i++) {
+    //     u_int16_t num = rand() % 65535;
+    //     char num_str[5];
+    //     sprintf(num_str, "%d", i);
+
+    // 	printf("%s\n", num_str);
+
+    //     /* Write data to i2c */
+    //     if (i2c_ioctl_write(&MCU_Interface::device, 0x0, num_str, strlen(num_str)) != strlen(num_str)) {
+    //         /* Error process */
+    //     }
+    // }
+    //i2c_close(MCU_Interface::device.bus);
+}
+
+void MCU_Interface::transfer_to_mcu(u_int8_t a_byte) {
+// To compile and run all files:
+// cc -c ./i2c_test.c && cc ./i2c.o ./i2c_test.o -o main && ./main
+    const I2CDevice dev_1 = MCU_Interface::device;
+
+    for (int outer = 0; outer < 1000; outer++) {
+        char num_str[num_thrusters];
+        for (int i = 0; i < num_thrusters; i++)
+        {
+            num_str[i] = a_byte;
+        }
+
+        if (i2c_ioctl_write(&dev_1, 0x0, num_str, strlen(num_str)) != strlen(num_str))
+        {
+            /* Error process */
+        }
+    }
+
+    
+    // for (u_int16_t i = 9000; i < 100000; i++) {
+    //     u_int16_t num = rand() % 65535;
+    //     char num_str[5];
+    //     sprintf(num_str, "%d", i);
+
+    // 	printf("%s\n", num_str);
+
+    //     /* Write data to i2c */
+    //     if (i2c_ioctl_write(&MCU_Interface::device, 0x0, num_str, strlen(num_str)) != strlen(num_str)) {
+    //         /* Error process */
+    //     }
+    // }
+    //i2c_close(MCU_Interface::device.bus);
+}
 
 
 void MCU_Interface::execute(){
