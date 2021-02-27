ec2b8caa6ce884fe0fc471b6083a394e3105ca96
Michael Hoyer <michaelhoyer@outlook.com>
2021-02-27 16:00:21 +0100

    remove thrust direction

--- motion/thruster_allocator/include/vortex_allocator/allocator_ros.h
+++ motion/thruster_allocator/include/vortex_allocator/allocator_ros.h
@@ -22,7 +22,6 @@ private:
 
   int m_num_degrees_of_freedom;
   int m_num_thrusters;
-  std::vector<int> m_direction;
   std::map<std::string, bool> m_active_degrees_of_freedom;
   double m_min_thrust;
   double m_max_thrust;
