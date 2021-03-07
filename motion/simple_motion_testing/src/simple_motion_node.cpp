42faf02351c3e11d8a0c2749fc7a1756d7d5763d
TarekEl-Agroudi <tarek.elagroudi@gmail.com>
2021-03-07 12:55:56 +0100

    implimented class for subscribing and publishing

--- motion/simple_motion_testing/src/simple_motion_node.cpp
+++ motion/simple_motion_testing/src/simple_motion_node.cpp
@@ -1,7 +1,7 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
+#include <iostream>
 
 
 #include "nav_msgs/Odometry.h"
@@ -9,37 +9,64 @@
 #include "geometry_msgs/Wrench.h"
 #include "geometry_msgs/Vector3.h"
 
-int main(int argc, char **argv)
+class subscribe_and_publish{
+public:
+    subscribe_and_publish(){
+        wrench_pub = nh.advertise<geometry_msgs::Wrench>("/auv/thruster_manager/input", 100);
+        odom_sub = nh.subscribe("/odometry/filtered", 100, &subscribe_and_publish::odometry_callback,this);
+    }
 
-{
-    ros::init(argc,argv,"simple_motion_node");
+    void odometry_callback(const nav_msgs::Odometry & position_msg){
+        std::cout << position_msg.pose.pose.position.x << "\n";
+        //geometry_msgs::Wrench msg;
+        //msg.force.x = 10.0;//K_p*(reference-position_msg.pose.pose.position.x);
+       // wrench_pub.publish(msg);
+        sleep(1);
+    }
+private:
     ros::NodeHandle nh;
+    ros::Publisher wrench_pub;
+    ros::Subscriber odom_sub;
+    float reference = 1.0;
+    float K_p = 3;      
+};
 
-    ros::Publisher wrench_pub = nh.advertise<geometry_msgs::Wrench>("/auv/thruster_manager/input", 100);
 
-    ros::Rate loop_rate(10);
 
-    //generate hard-coded wrench message
-  
-    geometry_msgs::Vector3 force;
-    force.x = 10.0;
-    force.y = 0.0;
-    force.z = 0.0;
-    geometry_msgs::Vector3 torque;
-    torque.x = 0.0;
-    torque.y = 0.0;
-    torque.z = 0.0;
+// void odometry_callback(const nav_msgs::Odometry & position_msg){
+//     std::cout << position_msg.pose.pose.position.x << "\n";
+//     //msg.force.x = 10.0;//K_p*(reference-position_msg.pose.pose.position.x);
+//     //sleep(1);
+    
+// }
+
+int main(int argc, char **argv)
+{
     geometry_msgs::Wrench msg;
-    msg.force = force;
-    msg.torque = torque;
+
+    ros::init(argc,argv,"simple_motion_node");
+    
+    // ros::NodeHandle nh;
+    // ros::Publisher wrench_pub = nh.advertise<geometry_msgs::Wrench>("/auv/thruster_manager/input", 100);
+    // ros::Subscriber odom_sub = nh.subscribe("/odometry/filtered", 100, &odometry_callback);
+
+    //ros::Rate loop_rate(10);
+
+    subscribe_and_publish P_controller;
 
     //publish wrench message to topic /auv/thruster_manager/input
 
+    // msg.force.x = 10;
+    // sleep(5);
+    // wrench_pub.publish(msg);
+    // sleep(5);
+    // msg.force.x = -10;
+    // wrench_pub.publish(msg);
+    // sleep(5);
+    // msg.force.x = 0;
+    // wrench_pub.publish(msg);
 
-    for (int i = 0; i<10; i++){
-        wrench_pub.publish(msg);
-        sleep(1);
-    }
+
 
     ros::spin();
 
