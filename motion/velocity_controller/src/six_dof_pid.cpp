c6487acf59d521f10476864d2b1f30b1c2b87b6c
Vortex-Noctua <jontg@stud.ntnu.no>
2021-03-25 17:07:28 +0100

    progress save

--- motion/velocity_controller/src/six_dof_pid.cpp
+++ motion/velocity_controller/src/six_dof_pid.cpp
@@ -1,3 +1,5 @@
+
+
 #include "ros/ros.h"
 #include "ros/console.h"
 #include "nav_msgs/Odometry.h"
@@ -9,33 +11,34 @@ class VelocityController
 public:
   VelocityController(ros::NodeHandle ros_node);
   void odometryCallback(nav_msgs::Odometry& odom_msg);
-  void desiredVelocityCallback(geometry_msgs::Twist& twist_msg);
+  void controlLawCallback(geometry_msgs::Twist& twist_msg);
 
 private:
+  std::string odometry_topic;
+  std::string thrust_topic;
+  std::string desired_velocity_topic;
+  ros::Publisher thrust_pub;
+  ros::Subscriber odom_sub;
+  ros::Subscriber vel_sub;
   nav_msgs::Odometry odometry;
 };
 
 VelocityController::VelocityController(ros::NodeHandle ros_node)
 {
   // get params
-  std::string odometry_topic;
   if (!ros_node.getParam("/velocity_controller/odometry_topic", odometry_topic))
     odometry_topic = "/odometry/filtered";
 
-  std::string thrust_topic;
   if (!ros_node.getParam("/velocity_controller/thrust_topic", thrust_topic))
     thrust_topic = "/thrust/desired";
 
-  std::string desired_velocity_topic;
   if (!ros_node.getParam("velocity_controller/desired_velocity_topic", desired_velocity_topic))
     desired_velocity_topic = "/controller/desired_velocity";
 
   // create subscribers and publsihers
-  ros::Publisher thrust_pub = ros_node.advertise<geometry_msgs::Wrench>(thrust_topic, 1);
-  ros::Subscriber odom_sub =
-      ros_node.subscribe<nav_msgs::Odometry>(odometry_topic, 1, VelocityController::odometryCallback);
-  ros::Subscriber vel_sub =
-      ros_node.subscribe<geometry_msgs::Twist>(desired_velocity_topic, 1, VelocityController::desiredVelocityCallback);
+  thrust_pub = ros_node.advertise<geometry_msgs::Wrench>(thrust_topic, 1);
+  odom_sub = ros_node.subscribe<nav_msgs::Odometry>(odometry_topic, 1, VelocityController::odometryCallback);
+  vel_sub = ros_node.subscribe<geometry_msgs::Twist>(desired_velocity_topic, 1, VelocityController::controlLawCallback);
 
   // wait for first odometry message
   if (!ros::topic::waitForMessage<nav_msgs::Odometry>(odometry_topic, ros_node, ros::Duration(30)))
@@ -47,7 +50,24 @@ VelocityController::VelocityController(ros::NodeHandle ros_node)
 
 void VelocityController::odometryCallback(nav_msgs::Odometry& odom_msg)
 {
-  odometry = odom_msg; // TODO: might have to make this thread safe
+  odometry = odom_msg;  // TODO: might have to make this thread safe
+}
+
+void VelocityController::controlLawCallback(geometry_msgs::Twist& twist_msg)
+{
+  // copy odometry to remove chance of odom beeing updated during function call
+  nav_msgs::Odometry current_odom = odometry;
+
+  // convert to Eigen? Has to be set up, but would simplify everything else
+
+  // calculate restoring forces
+
+  // calculate tau using MiniPID and restoring forces 
+  geometry_msgs::Wrench tau;
+  
+
+  // publish tau as thrust
+  thrust_pub.publish(tau);
 }
 
 int main(int argc, char** argv)
