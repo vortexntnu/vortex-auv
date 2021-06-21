38e1b137021e49271e725340a120e9d3af37db48
Christopher Strøm <detoxte@gmail.com>
2021-06-21 21:50:58 +0200

    Add barebones light interface node
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- manipulators/lights_interface/scripts/lights_interface_node.py
+++ manipulators/lights_interface/scripts/lights_interface_node.py
@@ -4,17 +4,58 @@ import rospy
 from sensor_msgs.msg import Joy
 from vortex_msgs.msg import Pwm
 
-# TODO: get data from controller, implement btn bounceback, have multiple light intensity lvls using pwm
-# TODO: sub/pub node --> sub to joystick, pub to PCA using pwm
-# TODO: pub using pwm msg type - pin 8
+# TODO:  have multiple light intensity lvls using pwm
 # TODO: subscribe to lights pwm topic from PCA interface node
 
-class LightsInterface:
-    def __init__(self):
-        self.btn_status = 0
-        self.btn_status = 0
+on = 1
+off = -1
+active = 1
+inactive = 0
 
-        self.lights_pwm = 0
+PWM_LOW = 1100
+PWM_HIGH = 1900
+
+class LightsInterfaceNode:
+    def __init__(self):
+        rospy.init_node('/manipulators/lights_interface')
 
         self.js_sub = rospy.Subscriber('/mission/joystick_data', Joy, self.callback, queue_size=1)
         self.pwm_pub = rospy.Publisher('/manipulators/lights', Pwm, queue_size=1)
+
+        self.pwm_pin = 8
+        self.light_state = off
+
+        self.cooldown_period = 2 # Seconds
+        self.last_press = datetime.now()
+
+    def callback(self, joy_msg):
+        
+        Dpad = joy_msg.axes[6]
+
+        time_since_press = self.last_press - datetime.now()
+        if time_since_press > self.cooldown_period:
+
+            if Dpad == on and self.light_state != active:
+                self.pwm_pub.publish(PWM_HIGH)
+                self.last_press = datetime.now()
+                self.light_state = active
+
+            elif Dpad == off and self.light_state == active:
+                self.pwm_pub.publish(PWM_LOW)
+                self.last_press = datetime.now()
+                self.light_state = inactive
+
+    def publish_pwm_msg(self, us):
+            msg = Pwm()
+            msg.pins = self.pwm_pin
+            msg.positive_width_us = us
+
+            self.pwm_pub.publish(msg)
+
+    # If PWM set is not sticky, have a spin function that writes
+
+if __name__ == '__main__':
+    node = LightsInterfaceNode()
+    
+    while not rospy.is_shutdown():
+        rospy.spin()
