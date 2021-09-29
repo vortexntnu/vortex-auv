62889fb301db611c59469e3b90e094be2a5eb643
Erik Rowe <ekrowe@stud.ntnu.no>
2021-09-29 22:45:11 +0200

    updated size_estimator

--- object_detection/size_estimator/src/size_estimator.py
+++ object_detection/size_estimator/src/size_estimator.py
@@ -1,9 +1,9 @@
+#!/usr/bin/env python
 
 
+import rospy
 
-from darknet_ros_msgs.msg import (
-    BoundingBoxmsg
-)
+from darknet_ros_msgs.msg import BoundingBoxes, BoundingBox
 
 def length_of_bounding_box(bbox):
     return xmax - xmin
@@ -11,6 +11,26 @@ def length_of_bounding_box(bbox):
 def hight_of_bounding_box(bbox):
     return ymax - ymin
 
+
+class SizeEstimatorNode():
+    def __init__(self):
+        rospy.init_node('size_estimator')
+        self.estimatorSub = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.callback)
+        self.estimatorPub = rospy.Publisher('/object_detection/size_estimator', BoundingBox, queue_size= 1)
+
+
+    def callback(self, data):
+        self.min_x = data.bounding_boxes[0].xmin + 123
+        self.var = data
+        self.var.xmin = self.min_x
+        estimatorPub.publish(self.var)
+    
+
+if __name__ == '__main__':
+    node = SizeEstimatorNode()
+    
+    while not rospy.is_shutdown():
+        rospy.spin()
 
 
 
