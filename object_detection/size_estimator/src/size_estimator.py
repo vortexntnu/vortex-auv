e593b4d74478990e91f1355b0be84b6bf1548012
JanaLan <j.langholz@gmx.de>
2021-09-29 20:22:33 +0200

    change in size_estimator.py

---
+++ object_detection/size_estimator/src/size_estimator.py
@@ -0,0 +1,16 @@
+
+
+
+from darknet_ros_msgs.msg import (
+    BoundingBoxmsg
+)
+
+def length_of_bounding_box(bbox):
+    return xmax - xmin
+
+def hight_of_bounding_box(bbox):
+    return ymax - ymin
+
+
+
+
