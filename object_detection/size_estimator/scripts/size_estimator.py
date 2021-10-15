228b87ece1cb175ad46ccf888b6e1711756049f3
Stormtrooper <stormtrooper@vortexntnu.no>
2021-10-15 22:15:23 +0200

    Fix logic in the size and angle estimation. Split neg and pos halves.

--- object_detection/size_estimator/scripts/size_estimator.py
+++ object_detection/size_estimator/scripts/size_estimator.py
@@ -7,16 +7,16 @@ from sensor_msgs.msg import Image
 from vortex_msgs.msg import BBox, BBoxes
 
 import numpy as np
-from math import sin
+from math import sin, radians
 from std_msgs.msg import Int64, Float64
 
 
 class SizeEstimatorNode():
     fov_horizontal = 110.0                          # Degrees
-    fov_vertical = 70                               # Degrees
+    fov_vertical = 70.0                               # Degrees
     focal_length = 2.12                             # mm
-    max_width = 1344.0                              # pxl
-    max_height = 376.0                              # pxl
+    max_width = 672                                 # pxl
+    max_height = 376                                # pxl
     angles_pr_pxl_hor = fov_horizontal/max_width
     angles_pr_pxl_ver = fov_vertical/max_height
 
@@ -53,10 +53,13 @@ class SizeEstimatorNode():
             delta_angle_x, angle_centre_object_x = self.calc_angles(self.angles_pr_pxl_hor, min_x_pxl,max_x_pxl)
             delta_angle_y, angle_centre_object_y = self.calc_angles(self.angles_pr_pxl_ver, min_y_pxl,max_y_pxl)
 
+            # Redefine angles to coord frame
+            redefined_angle_x = self.redefine_angles(angle_centre_object_x, self.fov_horizontal)
+            redefined_angle_y = self.redefine_angles(angle_centre_object_y, self.fov_vertical)
+
             # Calculate the sizes in metres of a boundingbox
             length_x_mtr = self.calc_size(delta_angle_x, depth_mtr)
             length_y_mtr = self.calc_size(delta_angle_y, depth_mtr)
-            
 
             # Build the new bounding box message
             CurrentBoundingBox = BBox()
@@ -65,13 +68,41 @@ class SizeEstimatorNode():
             CurrentBoundingBox.width = length_x_mtr
             CurrentBoundingBox.height = length_y_mtr
             CurrentBoundingBox.z = depth_mtr
-            CurrentBoundingBox.centre_angle_x = angle_centre_object_x
-            CurrentBoundingBox.centre_angle_y = angle_centre_object_y
+            CurrentBoundingBox.centre_angle_x = redefined_angle_x
+            CurrentBoundingBox.centre_angle_y = redefined_angle_y
 
             # Append the new message to bounding boxes array
             ArrayBoundingBoxes.bounding_boxes.append(CurrentBoundingBox)
 
         self.estimatorPub.publish(ArrayBoundingBoxes)
+    
+    def redefine_angles(self, angle_centre_object, fov_type):
+        """
+        Creates a coordinate system for the view. Angles in the right half of the view are positive, and the left half are negative.
+        Does the same for top and bottom half of screen. Sets Origo in the center of the screen.
+
+        Args:
+            angle_centre_object         angle_centre_object_x/angle_centre_object_y
+            fov_type                    fov_horizontal/fov_vertical
+
+        Returns:
+            redefined_angle             angle in new coordinate system, pos angle in right/upper half os the screen and vice versa
+        """
+
+        # Split the screen in two halves
+        new_fov = fov_type * 0.5
+        
+        # For some reason the horizontal angle is calculated from left to right,
+        # however the vertical angle is quite unintuitively calculated from top to bottom,
+        # and therefore needs to be inverted to have neg degrees in the bottom half  
+        if new_fov == 35:
+            redefined_angle = -(angle_centre_object - new_fov)
+            # rospy.loginfo("\n%f \n%f",angle_centre_object, new_fov)
+        else:
+            redefined_angle = angle_centre_object - new_fov
+
+        return redefined_angle
+        
 
     def calc_size(self, delta_angle, dist_to_object):
         """
@@ -84,7 +115,8 @@ class SizeEstimatorNode():
             length_in_mtr           The width or height of the detected object box
         """
 
-        length_in_mtr = dist_to_object * sin(delta_angle)
+        length_in_mtr = dist_to_object * sin(radians(delta_angle))
+
         return length_in_mtr
 
     def calc_angles(self, angles_pr_pxl, min_pos, max_pos):
@@ -104,11 +136,9 @@ class SizeEstimatorNode():
         angle_max = max_pos * angles_pr_pxl
         angle_min = min_pos * angles_pr_pxl
         delta_angle = angle_max - angle_min
-        angle_centre_object = angle_min + delta_angle*0.5
+        angle_centre_object = angle_min + delta_angle * 0.5
         
         return delta_angle, angle_centre_object
-
-
 
 if __name__ == '__main__':
     node = SizeEstimatorNode()
