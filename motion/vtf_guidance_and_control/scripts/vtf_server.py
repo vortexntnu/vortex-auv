1b3337146b7729e556f28eb922d03366787d2eb7
finnfi <32849595+finnfi@users.noreply.github.com>
2022-01-13 21:15:40 +0100

    Update motion/vtf_guidance_and_control/scripts/vtf_server.py
    Co-authored-by: Christopher Strøm <48532094+chrstrom@users.noreply.github.com>

--- motion/vtf_guidance_and_control/scripts/vtf_server.py
+++ motion/vtf_guidance_and_control/scripts/vtf_server.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-# Written by Kristoffer Rakstad Solberg, Student
-# Documented by Christopher Strom and Jae Hyeong Hwang
-# Copyright (c) 2020 Manta AUV, Vortex NTNU.
-# All rights reserved.
 
 import rospy
 
