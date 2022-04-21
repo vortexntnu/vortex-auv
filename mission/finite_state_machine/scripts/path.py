252094b3e9e941b092cc5123eba3b44ce25f91d5
Finn Maurer <finn.gross.maurer@gmail.com>
2022-04-21 17:57:50 +0200

    created path state script

--- mission/finite_state_machine/scripts/path.py
+++ mission/finite_state_machine/scripts/path.py
@@ -7,7 +7,7 @@ from landmarks.srv import request_position
 import actionlib
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseActionGoal, MoveBaseGoal
-from vortex_msgs.msg import VtfPathFollowingAction, VtfPathFollowingGoal, SetVelocityGoal, SetVelocityAction
+from vortex_msgs.msg import VtfPathFollowingAction, VtfPathFollowingGoal, DpSetpoint,SetVelocityGoal, SetVelocityAction
 
 from tf.transformations import quaternion_from_euler
 from fsm_helper import dp_move, get_pose_in_front, rotate_certain_angle
@@ -44,8 +44,7 @@ class PathSearch(smach.State):
             print(self.object.objectPose.pose.position)
             rospy.wait_for_service('send_positions')   
             self.object = self.landmarks_client("path").object
-            rate.sleep()
-        
+            rate.sleep()       
         self.vtf_client.cancel_all_goals()
 
         print("PATH POSITION DETECTED: "+ 
@@ -61,19 +60,20 @@ class PathConverge(smach.State):
         
         self.landmarks_client = rospy.ServiceProxy('send_positions',request_position)
         rospy.wait_for_service('send_positions') 
-        self.object = self.landmarks_client("path").object
+        self.object = self.landmarks_client("buoy").object
 
         vtf_action_server = "/controllers/vtf_action_server"
         self.vtf_client = actionlib.SimpleActionClient(vtf_action_server, VtfPathFollowingAction)
 
+        self.dp_pub = rospy.Publisher("controllers/dp_data", DpSetpoint)
+
     def execute(self, userdata):
 
         goal = VtfPathFollowingGoal()
-        self.object = self.landmarks_client("path").object
         goal_pose = get_pose_in_front(self.object.objectPose.pose, 0.5) 
         print("get_pose_in_front returned:")
         print(goal_pose)
-        goal.waypoints =[goal_pose.position]
+        goal.waypoints = [goal_pose.position]
         goal.forward_speed = 0.1
         goal.heading = "path_dependent_heading"
 
@@ -81,20 +81,41 @@ class PathConverge(smach.State):
         self.vtf_client.send_goal(goal)
         rate = rospy.Rate(1)
         rate.sleep()
-        while not rospy.is_shutdown():
-            if self.vtf_client.simple_state == actionlib.simple_action_client.SimpleGoalState.DONE and self.object.estimateConverged:
-                break
+        while not rospy.is_shutdown() \
+            and not self.vtf_client.simple_state == actionlib.simple_action_client.SimpleGoalState.DONE:
             self.object = self.landmarks_client("path").object
             goal.waypoints = [self.object.objectPose.pose.position]
-            print("PATH POSITION DETECTED: "+ str(goal.waypoints[0].x) + ", "+ str(goal.waypoints[0].y)+ ", "+ str(goal.waypoints[0].z))
+            print("PATH POSITION DETECTED: "+ \
+                str(goal.waypoints[0].x) + ", "+ str(goal.waypoints[0].y)+ ", "+ str(goal.waypoints[0].z))
             goal.waypoints[0] = get_pose_in_front(self.object.objectPose.pose, 0.5).position
             self.vtf_client.send_goal(goal)
-            userdata.gate_converge_output=self.object
             rate.sleep()
-            
+            if self.object.estimateFucked:
+                self.vtf_client.cancel_all_goals()
+                return 'preempted'
+        self.vtf_client.cancel_all_goals()
 
-        return 'succeeded'
-
+        dp_goal = DpSetpoint()
+        dp_goal.control_mode = 7 # POSE_HOLD
+        dp_goal.setpoint = get_pose_in_front(self.object.objectPose.pose, 0.5)
+        self.dp_pub.publish(dp_goal)
+        while not rospy.is_shutdown()\
+            and not self.object.estimateConverged:
+            self.object = self.landmarks_client("path").object
+            if self.object.estimateFucked:
+                dp_goal.control_mode = 0 # OPEN_LOOP
+                self.dp_pub.publish(dp_goal)
+                return 'preempted'
+            rate.sleep()
+        dp_goal.control_mode = 0 # OPEN_LOOP
+        self.dp_pub.publish(dp_goal)
+        self.object = self.landmarks_client("path").object
+        userdata.buoy_converge_output=self.object
+        print("PATH POSITION ESTIMATE CONVERGED AT: " + str(self.object.objectPose.position.x) + "; " \
+        + str(self.object.objectPose.position.y) + "; " \
+        + str(self.object.objectPose.position.z))
+        return 'succeded'
+        
 class PathExecute(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['preempted', 'succeeded', 'aborted'],input_keys=['path'])   
