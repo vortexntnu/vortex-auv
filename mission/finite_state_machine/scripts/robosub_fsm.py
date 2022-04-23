30f37f1102bcb748ad24107f6cbd1fed9e338b3f
ronja kræmer <rkhansen01@gmail.com>
2022-04-23 12:50:04 +0200

    add buoy to fsm
    Signed-off-by: ronja kræmer <rkhansen01@gmail.com>

--- mission/finite_state_machine/scripts/robosub_fsm.py
+++ mission/finite_state_machine/scripts/robosub_fsm.py
@@ -7,6 +7,7 @@ from smach_ros import IntrospectionServer
 from gate_choose_side import GateSearch, GateConverge, GateExecute
 from pole import PoleSearch, PoleConverge, PoleExecute
 from path import PathSearch, PathConverge, PathExecute
+from buoy import BuoySearch, BuoyConverge, BuoyExecute
 from reach_depth import ReachDepth
 
 def main():
@@ -57,9 +58,26 @@ def main():
                         transitions={'aborted' : 'PATH_SEARCH'}
                         )
         
-        StateMachine.add('PATH_SM',path_sm )
+        StateMachine.add('PATH_SM',path_sm, transitions={'succeeded':'BUOY_SM'})
 
         ##BUOY
+        buoy_sm = StateMachine(outcomes=['preempted', 'succeeded', 'aborted'])
+
+        with buoy_sm:
+            StateMachine.add('BUOY_SEARCH',
+                        BuoySearch(), 
+                        transitions={'succeeded':'BUOY_CONVERGE'})
+            
+            
+            StateMachine.add('BUOY_CONVERGE',
+                        BuoyConverge(),
+                        transitions={'succeeded' : 'BUOY_EXECUTE','aborted' : 'BUOY_SEARCH'}, 
+                        remapping={'buoy_converge_output':'buoy'})
+            
+            StateMachine.add('BUOY_EXECUTE',
+                        BuoyExecute())
+        
+        StateMachine.add('BUOY_SM',buoy_sm)
 
         ##TORPEDO
 
@@ -68,12 +86,12 @@ def main():
         ##Resurface
 
 
-    # intro_server = IntrospectionServer(str(rospy.get_name()), robosub_state_machine,'/SM_ROOT')    
-    # intro_server.start()
+    intro_server = IntrospectionServer(str(rospy.get_name()), robosub_state_machine,'/SM_ROOT')    
+    intro_server.start()
 
     try:
         robosub_state_machine.execute()
-        # intro_server.stop()
+        intro_server.stop()
 
     except Exception as e:
         rospy.loginfo("State machine failed: %s" % e)
