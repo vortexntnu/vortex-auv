c5c9b96ba68f98c1b1c2faaaf1190b8361e11929
TarekEl-Agroudi <tarek.elagroudi@gmail.com>
2022-04-24 12:00:55 +0200

    implemented and tested perception simulator for prequals

--- mission/finite_state_machine/scripts/perception_simulator.py
+++ mission/finite_state_machine/scripts/perception_simulator.py
@@ -1,20 +1,55 @@
+#!/usr/bin/env python
 
 import rospy
 from std_msgs.msg import String
+from vortex_msgs.msg import ObjectPosition
+from geometry_msgs.msg import Point
+import time
 
 class Perciever:
     def __init__(self):
-        self.state = ""
+        self.state = "da"
         rospy.Subscriber("/fsm/state",String,self.state_cb)
 
-    def state_cb(self, msg):
-        self.state = msg
+        self.landmarks_pub = rospy.Publisher('/fsm/object_positions_in',ObjectPosition,queue_size=1)
+        
+        self.gate = ObjectPosition()
+        self.gate.objectID = "gate"
+        self.gate.objectPose.pose.position = Point(5,0,-1.4)
+        self.gate.objectPose.pose.orientation.x = 0
+        self.gate.objectPose.pose.orientation.y = 0
+        self.gate.objectPose.pose.orientation.z = 1
+        self.gate.objectPose.pose.orientation.w = 0
+        self.gate.isDetected = True
+        self.gate.estimateConverged = True
 
-    def execute():
+        self.pole = ObjectPosition()
+        self.pole.objectID = "pole"
+        self.pole.objectPose.pose.position = Point(17.2,0,-1.4)
+        self.pole.objectPose.pose.orientation.x = 0
+        self.pole.objectPose.pose.orientation.y = 0
+        self.pole.objectPose.pose.orientation.z = 1
+        self.pole.objectPose.pose.orientation.w = 0
+        self.pole.isDetected = True
+        self.pole.estimateConverged = True
+
+    def state_cb(self, msg):
+        self.state = msg.data
+
+    def execute(self):
         rate = rospy.Rate(1)
-        while not rospy.is_shutdown:
-            
+        while self.state != "gate_search":
             rate.sleep()
+        time.sleep(5)
+        self.landmarks_pub.publish(self.gate)
+        while self.state != "pole_search":
+            rate.sleep()
+        self.landmarks_pub.publish(self.pole)
+        while self.state != "gate_search":
+            rate.sleep()
+        self.gate.objectPose.pose.orientation.z = 0
+        self.gate.objectPose.pose.orientation.w = 1
+        self.landmarks_pub.publish(self.gate)
 
 
 if __name__ == '__main__':
