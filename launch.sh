17ded980f7ff65431ddbcdc5cc7c1b0e43b5efde
TarekEl-Agroudi <tarek.elagroudi@gmail.com>
2022-07-17 14:33:28 +0200

    added fsm and vortex auv to launch script

--- launch.sh
+++ launch.sh
@@ -3,3 +3,5 @@
 # The purpose of this file is to automate launching of 
 # all nodes found in this repository FOR ROBOSUB 2022
 
+roslaunch auv_setup beluga.launch
+roslaunch finite_state_machine robosub.launch
