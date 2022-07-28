7e6aa90303fe9f812c0dc92b9f106ceecde658d7
Christopher Strøm <detoxte@gmail.com>
2022-07-28 11:13:07 -0400

    Hotfixes

--- activate_thrusters.sh
+++ activate_thrusters.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-echo "yaw";
-timeout 2s rostopic pub /thrust/desired_forces geometry_msgs/Wrench "force:
-  x: 0.0  
-  y: 0.0  
-  z: 0.0  
-torque:   
-  x: 0.0  
-  y: 0.0     
-  z: 0.01";
-
-echo "##First##"
+echo "##ARMING##";
 timeout 2s rostopic pub /thrust/desired_forces geometry_msgs/Wrench "force:
   x: 0.0  
   y: 0.0  
@@ -19,6 +9,5 @@ torque:
   x: 0.0  
   y: 0.0 
   z: 0.0";
+echo "ARMING DONE" ;
 
-  echo "done" ;
-  echo "##First##"  
--- mission/finite_state_machine/scripts/gate.py
+++ mission/finite_state_machine/scripts/gate.py
@@ -241,7 +241,7 @@ class GateConverge(smach.State):
 
         goal = VtfPathFollowingGoal()
         self.object = self.landmarks_client("gate").object
-        goal_pose = get_pose_in_front(self.object.objectPose.pose, 0.5, 0)
+        goal_pose = get_pose_in_front(self.object.objectPose.pose, -0.5, 0)
         print("get_pose_in_front returned:")
         print(goal_pose)
         goal.waypoints = [goal_pose.position]
@@ -320,6 +320,14 @@ class GateExecute(smach.State):
             vtf_action_server, VtfPathFollowingAction
         )
 
+        desired_velocity_topic = rospy.get_param(
+            "/controllers/velocity_controller/desired_velocity_topic"
+        )
+        self.velocity_ctrl_client = rospy.ServiceProxy(
+            desired_velocity_topic, SetVelocity
+        )
+        rospy.wait_for_service(desired_velocity_topic)
+
         self.state_pub = rospy.Publisher("/fsm/state", String, queue_size=1)
 
         rospy.Subscriber("/odometry/filtered", Odometry, self.odom_cb)
@@ -331,7 +339,7 @@ class GateExecute(smach.State):
     def execute(self, userdata):
         self.state_pub.publish("gate_execute")
         goal = VtfPathFollowingGoal()
-        goal_pose = get_pose_in_front(userdata.gate.objectPose.pose, -0.5, 0)
+        goal_pose = get_pose_in_front(userdata.gate.objectPose.pose, 1.5, 0)
         goal.waypoints = [goal_pose.position]
         goal.forward_speed = rospy.get_param("/fsm/medium_speed")
         goal.heading = "path_dependent_heading"
@@ -342,6 +350,7 @@ class GateExecute(smach.State):
         rate.sleep()
 
         # Move through gate
+        rospy.loginfo("MOVING THROUGH GATE")
         while not rospy.is_shutdown():
             if (
                 self.vtf_client.simple_state
@@ -351,11 +360,16 @@ class GateExecute(smach.State):
             rate.sleep()
 
         # Yaw 720 degrees
-        for i in range(4):
+
+        # NOT WORKING RN!
+        rate = rospy.Rate(10)
+        rospy.loginfo("PERFORMING ACROBATICS")
+        for i in range(8):
+            rospy.loginfo(f"{i} out of 4 turns!")
             goal = Pose()
             goal.position = self.odom.pose.pose.position
             goal.orientation = self.odom.pose.pose.orientation
-            goal = rotate_certain_angle(goal, 180)
+            goal = rotate_certain_angle(goal, 45)
             vel_goal = Twist()
             vel_goal.angular.z = rospy.get_param("/fsm/turn_speed")
             vel_goal.linear.z = (
@@ -363,8 +377,13 @@ class GateExecute(smach.State):
             )  # should be ommited if drone is balanced and level underwater
             vel_goal.linear.x = 0.01  # should be ommited if drone is balanced and level underwater. Same other places.
             self.velocity_ctrl_client(vel_goal, True)
-            while not within_acceptance_margins(goal, self.odom, check_yaw_only=True):
+            while (
+                not within_acceptance_margins(goal, self.odom, True)
+            ):
+                rospy.loginfo("SPINNING!")
                 rate.sleep()
+            self.velocity_ctrl_client(vel_goal, False)
+
 
 
         return "succeeded"
