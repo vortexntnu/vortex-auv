67fd53630732dccdba031594edfe71a7623de80e
Vortex NTNU <post@vortexntnu.no>
2022-07-28 19:15:36 +0000

    Update launch scripts
    Signed-off-by: Vortex NTNU <post@vortexntnu.no>

--- launch.sh
+++ launch.sh
@@ -4,11 +4,13 @@
 # all nodes found in this repository FOR ROBOSUB 2022
 
 echo "##AUV Setup##"
-sleep 10s
-roslaunch auv_setup beluga.launch & 
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/vortex_ws/devel/setup.bash
+
+roslaunch auv_setup beluga.launch> /dev/null 2>&1 & 
 sleep 10s
 echo "##Activate thruster##"
-./activate_thrusters.sh & 
-sleep 10s
+/home/ubuntu/vortex_ws/src/Vortex-AUV/activate_thrusters.sh & 
+sleep 5s
 echo "##Starting FSM##"
 roslaunch finite_state_machine robosub_fsm.launch
---
+++ roscore.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "##ROSCORE##"
+source /opt/ros/noetic/setup.bash
+
+roscore
