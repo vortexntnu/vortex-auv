beb056287c8721cab31fddd611abbbf41b7c2ca1
Christopher Strøm <detoxte@gmail.com>
2022-07-30 03:16:31 -0400

    Add more data to perception sim

--- perception_sim.sh
+++ perception_sim.sh
@@ -7,7 +7,7 @@ objectPose:
     stamp: {secs: 0, nsecs: 0}
     frame_id: ''
   pose:
-    position: {x: 2.0, y: 2.0, z: -2.0}
+    position: {x: 3.0, y: 1.0, z: -2.0}
     orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0} 
 isDetected: true
 estimateConverged: true
@@ -23,7 +23,7 @@ objectPose:
     stamp: {secs: 0, nsecs: 0}
     frame_id: ''
   pose:
-    position: {x: 5.0, y: 2.0, z: -2.0}
+    position: {x: 7.5, y: -2.6, z: -2.0}
     orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0} 
 isDetected: true
 estimateConverged: true
@@ -31,14 +31,14 @@ estimateFucked: false" &
 
 sleep 10
 
-rostopic pub /fsm/object_positions_in vortex_msgs/ObjectPosition "objectID: 'torpedo'
+rostopic pub /fsm/object_positions_in vortex_msgs/ObjectPosition "objectID: 'torpedo_poster'
 objectPose:
   header:
     seq: 0
     stamp: {secs: 0, nsecs: 0}
     frame_id: ''
   pose:
-    position: {x: 7.0, y: 1.0, z: -2.0}
+    position: {x: 12.0, y: 4.65, z: -2.0}
     orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0} 
 isDetected: true
 estimateConverged: true
@@ -46,6 +46,23 @@ estimateFucked: false" &
 
 
 sleep 10
+
+rostopic pub /fsm/object_positions_in vortex_msgs/ObjectPosition "objectID: 'torpedo_target'
+objectPose:
+  header:
+    seq: 0
+    stamp: {secs: 0, nsecs: 0}
+    frame_id: ''
+  pose:
+    position: {x: 12.0, y: 4.65, z: -1.8}
+    orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0} 
+isDetected: true
+estimateConverged: true
+estimateFucked: false" &
+
+
+sleep 10
+
 
 rostopic pub /fsm/object_positions_in vortex_msgs/ObjectPosition "objectID: 'octagon'
 objectPose:
@@ -54,7 +71,7 @@ objectPose:
     stamp: {secs: 0, nsecs: 0}
     frame_id: ''
   pose:
-    position: {x: 10.0, y: 0.0, z: -2.0}
+    position: {x: 17.0, y: 2.0, z: -2.0}
     orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0} 
 isDetected: true
 estimateConverged: true
