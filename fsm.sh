c9ba12f498d7b2be053a458a61c77ed4c3302ae7
Vortex NTNU <post@vortexntnu.no>
2022-07-31 14:00:47 +0000

    Fix fsm sh
    Signed-off-by: Vortex NTNU <post@vortexntnu.no>

--- fsm.sh
+++ fsm.sh
@@ -2,9 +2,11 @@
 
 # The purpose of this file is to automate launching of 
 # all nodes found in this repository FOR ROBOSUB 2022
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/vortex_ws/devel/setup.bash
 
 echo "##Activate thruster##"
-./activate_thrusters.sh & 
+/home/ubuntu/vortex_ws/src/Vortex-AUV/activate_thrusters.sh & 
 sleep 5s
 echo "##Starting FSM##"
 roslaunch finite_state_machine robosub_fsm.launch
