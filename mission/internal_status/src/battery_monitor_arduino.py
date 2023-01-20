1e172270087bd8db4610bd826acff97dd82a70e5
PizzaAllTheWay <karate.martyans1@gmail.com>
2023-01-20 21:22:53 +0100

    Calibrated Values

--- mission/internal_status/src/battery_monitor_arduino.py
+++ mission/internal_status/src/battery_monitor_arduino.py
@@ -28,7 +28,7 @@ class BatteryMonitor:
 
         # Calibration values for converting from raw digital binary form to decimal form
         # Calibration values were manualy calibrated to +- 0.1V acuracy!
-        self.calVoltageA = 11
+        self.calVoltageA = 11*4
         self.calVoltageB = 0.0
         # needs to be changed
         self.calCurrent = 37.8788
