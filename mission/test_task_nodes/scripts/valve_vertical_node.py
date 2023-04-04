d5b0286fd1eafdaae46702ab8453040cf5dc1705
ronja kræmer <rkhansen01@gmail.com>
2023-04-04 15:56:08 +0200

    add node for testing task manager
    Signed-off-by: ronja kræmer <rkhansen01@gmail.com>

--- mission/test_task_nodes/scripts/valve_vertical_node.py
+++ mission/test_task_nodes/scripts/valve_vertical_node.py
@@ -9,30 +9,24 @@ class ValveVertical:
         rospy.init_node("valve_vertical")
 
         self.isEnabled = False
-        self.task_manager_client = dynamic_reconfigure.client.Client(
-        "/task_manager_server", timeout=3, config_callback=self.callback)
 
 
-    def callback(config, self):
+    def callback(self, config):
+        rospy.loginfo("""Client: state change request: {Tac_states}""".format(**config))
         activated_task_id = config["Tac_states"]
-        for task in defines.Tasks.tasks_list:
 
-        # param = rospy.get_param(f"/tasks/{task.name}")
-        # rospy.loginfo(f"Inactive task name: {task.name}, true/false: {param}")
-            if task.id == activated_task_id:
-                self.isEnabled = True
-
-            # param = rospy.get_param(f"/tasks/{task.name}")
-            # rospy.loginfo(f"Active task name: {task.name}, true/false: {param}")
+        if defines.Tasks.valve_vertical.id == activated_task_id:
+            self.isEnabled = True
+        else:
+            self.isEnabled = False
+        print(f"isEnabled: {self.isEnabled} ")
 
         return config
-    
 
-    def spin(self):
-        while not rospy.is_shutdown:
-            rospy.spin()
 
 
 if __name__ == "__main__":
-    valve_vertical_task = ValveVertical()
-    valve_vertical_task.spin()
+    valve_vertical = ValveVertical()
+    task_manager_client = dynamic_reconfigure.client.Client(
+    "task_manager/task_manager_server", timeout=5, config_callback=valve_vertical.callback)
+    rospy.spin()
