c97d7356837765900d04a2a2f2bdc2a092e780f3
ronja kræmer <rkhansen01@gmail.com>
2023-04-04 15:55:52 +0200

    remove list that is not going to be used and add docking as a task
    Signed-off-by: ronja kræmer <rkhansen01@gmail.com>

--- mission/task_manager/task_manager_defines/defines.py
+++ mission/task_manager/task_manager_defines/defines.py
@@ -13,5 +13,4 @@ class Tasks:
     valve_vertical = Task(id=1, name="valve_vertical")
     valve_horisontal = Task(id=2, name="valve_horisontal")
     pipeline_inspection = Task(id=3, name="pipeline_inspection")
-
-    tasks_list = [joystick, valve_vertical, valve_horisontal, pipeline_inspection]
+    docking = Task(id=4, name="docking")
