c8073d6e801909f69227a82c4ce924499a5e7b07
alekskl01 <aleksander.klu@hotmail.com>
2023-04-26 16:01:18 +0200

    cleanup

--- mission/TAC_tasks/scripts/docking/docking.py
+++ mission/TAC_tasks/scripts/docking/docking.py
@@ -15,37 +15,6 @@ from task_manager_defines import defines
 from vortex_msgs.msg import (dpAction, dpGoal, dpResult, ObjectPosition)
 
 
-def quaternion_rotation_matrix(Q):
-
-    # Convert a quaternion into a full three-dimensional rotation matrix.
-
-    # Extract the values from Q
-    q0 = Q[0]
-    q1 = Q[1]
-    q2 = Q[2]
-    q3 = Q[3]
-
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02], [r10, r11, r12], [r20, r21, r22]])
-
-    return rot_matrix
-
-
 def distance_between_points(p1, p2):
     return math.dist([p1.x, p1.y, p1.z], [p2.x, p2.y, p2.z])
 
@@ -128,7 +97,7 @@ class DockingExecute(smach.State):
             self.object.objectPose.pose.orientation.w
         ]
 
-        rotationMatrix = quaternion_rotation_matrix(tf_quaternion)
+        rotationMatrix = quaternion_matrix(tf_quaternion)
         offsetArray = rotationMatrix.dot(offsetFromMC)
 
         offsetPoint = Point()
