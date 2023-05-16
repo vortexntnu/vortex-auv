1b27622d34c21d45660d4a34d75aed53515488c0
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2023-05-16 16:55:42 +0200

    Restructure and improve joystick interface

---
+++ mission/HMI/joystick_interface/scripts/joystick_interface.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python3
+# type: ignore
+
+from enum import IntEnum
+
+import rospy
+import actionlib
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
+from vortex_msgs.msg import ControlModeAction, ControlModeGoal
+from geometry_msgs.msg import Wrench
+
+from libjoystick.JoystickControlModes import *
+from libjoystick.ControlModeHandling import ControlModeHandling
+
+class JoystickInterface:
+    def __init__(self):
+        """
+        Define constants used in the joystick mapping, and any ros
+        specifics
+        """
+        rospy.init_node("joystick_interface")
+        self.ros_rate = rospy.Rate(50.0)
+        
+        # Attributes
+        self.buttons = {}
+        self.axes = {}
+
+        self.surge  = 0.0
+        self.sway   = 0.0
+        self.heave  = 0.0
+        self.roll   = 0.0
+        self.pitch  = 0.0
+        self.yaw    = 0.0
+
+        self.control_mode_handler = ControlModeHandling()
+
+        # ROS params
+        self.joystick_surge_scaling = rospy.get_param(
+            "/joystick/scaling/surge", 60)
+        self.joystick_sway_scaling = rospy.get_param("/joystick/scaling/sway", 60)
+        self.joystick_heave_scaling = rospy.get_param(
+            "/joystick/scaling/heave", 60)
+        self.joystick_roll_scaling = rospy.get_param("/joystick/scaling/roll", 35)
+        self.joystick_pitch_scaling = rospy.get_param(
+            "/joystick/scaling/pitch", -30)
+        self.joystick_yaw_scaling = rospy.get_param("/joystick/scaling/yaw", 20)
+
+        self.joystick_buttons_map = [
+            "A",
+            "B",
+            "X",
+            "Y",
+            "LB",
+            "RB",
+            "back",
+            "start",
+            "power",
+            "stick_button_left",
+            "stick_button_right",
+        ]
+
+        self.joystick_axes_map = [
+            "horizontal_axis_left_stick",
+            "vertical_axis_left_stick",
+            "LT",
+            "horizontal_axis_right_stick",
+            "vertical_axis_right_stick",
+            "RT",
+            "dpad_horizontal",
+            "dpad_vertical",
+        ]
+
+        # Subscribers and publishers
+        self.joystick_sub = rospy.Subscriber("/joy",
+                                             Joy,
+                                             self.joystick_cb,
+                                             queue_size=1)
+        self.joystick_pub = rospy.Publisher("/mission/joystick_data",
+                                            Joy,
+                                            queue_size=1)
+
+        self.wrench_pub = rospy.Publisher("/thrust/desired_forces",
+                                          Wrench,
+                                          queue_size=1)
+
+        # Services and actions
+        self.guidance_interface_client = actionlib.SimpleActionClient(
+            "/guidance_interface/joystick_server", ControlModeAction)
+
+        rospy.loginfo("Waiting for joystick input...")
+        rospy.wait_for_message("/joy", Joy)
+        rospy.loginfo("Joystick interface is up and running")
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            if self.buttons != {}:
+                self.control_mode_handler.control_mode_change(self.buttons, self.wrench_pub)
+            
+            self.surge   = self.axes["vertical_axis_left_stick"] * self.joystick_surge_scaling
+            self.sway    = self.axes["horizontal_axis_left_stick"] * self.joystick_sway_scaling
+            self.heave   = -(self.axes["RT"] - self.axes["LT"]) / 2 * self.joystick_heave_scaling
+            self.roll    = (self.buttons["RB"] - self.buttons["LB"]) * self.joystick_roll_scaling
+            self.pitch   = self.axes["vertical_axis_right_stick"] * self.joystick_pitch_scaling * (-1)
+            self.yaw     = self.axes["horizontal_axis_right_stick"] * self.joystick_yaw_scaling
+
+            self.dpad_hor = self.axes["dpad_horizontal"]
+            self.dpad_ver = self.axes["dpad_vertical"]
+
+            self.publish_joystick_data()
+
+            self.ros_rate.sleep()
+
+    def joystick_cb(self, msg):
+        try:
+            self.buttons = {self.joystick_buttons_map[i]: msg.buttons[i] for i in range(len(msg.buttons))}
+            self.axes = {self.joystick_axes_map[i]: msg.axes[i] for i in range(len(msg.axes))}
+        except Exception as e:
+            rospy.logerr(f"Error in joystick_cb: {e}")   
+
+    def publish_joystick_data(self):
+        # Publish joystick and wrench data
+        joystick_msg = Joy()
+        joystick_msg.axes = [
+            self.surge,
+            self.sway,
+            self.heave,
+            self.roll,
+            self.pitch,
+            self.yaw,
+            self.dpad_hor,
+            self.dpad_ver,
+        ]
+        self.joystick_pub.publish(joystick_msg)
+
+        if self.control_mode_handler.control_mode == JoystickControlModes.OPEN_LOOP.value:
+            wrench_msg = Wrench()
+            wrench_msg.force.x  = self.surge
+            wrench_msg.force.y  = self.sway
+            wrench_msg.force.z  = self.heave
+            wrench_msg.torque.x = self.roll
+            wrench_msg.torque.y = self.pitch
+            wrench_msg.torque.z = self.yaw
+            self.wrench_pub.publish(wrench_msg)
+
+if __name__ == "__main__":
+    try:
+        joystick_interface = JoystickInterface()
+        joystick_interface.spin()
+
+    except rospy.ROSInterruptException:
+        pass
---
+++ mission/HMI/joystick_interface/src/libjoystick/ControlModeHandling.py
@@ -0,0 +1,87 @@
+import rospy
+import rosnode
+import subprocess
+
+from geometry_msgs.msg import Wrench
+
+from libjoystick.JoystickControlModes import *
+
+class ControlModeHandling:
+    def __init__(self):
+        self.control_mode = JoystickControlModes(0)
+
+    def control_mode_change(self, buttons, wrench_publisher_handle, dp_client_handle):
+        pressed = -1
+
+        if buttons["stick_button_left"] and buttons["stick_button_right"] and buttons["RB"] and buttons["LB"]:
+            pressed = JoystickControlModes.KILLSWITCH.value
+            self.control_mode = pressed
+            ControlModeHandling.killswitch(buttons, wrench_publisher_handle)
+        
+        if buttons["start"]:
+            pressed = JoystickControlModes.EMERGENCY_STOP.value
+            self.control_mode = pressed
+            ControlModeHandling.emergency_stop(wrench_publisher_handle)
+
+        elif buttons["A"]:
+            pressed = JoystickControlModes.OPEN_LOOP.value
+            self.control_mode = pressed
+
+        elif buttons["B"]:
+            pressed = JoystickControlModes.POSE3_HOLD.value
+            self.control_mode = pressed
+            ControlModeHandling.pose3_hold(dp_client_handle)
+
+        elif buttons["X"]:
+            pressed = JoystickControlModes.POSE4_HOLD.value
+            self.control_mode = pressed
+            ControlModeHandling.pose4_hold(dp_client_handle)
+
+        elif buttons["Y"]:
+            pressed = JoystickControlModes.DP_CMD.value
+            self.control_mode = pressed
+
+        if pressed != -1:
+            rospy.loginfo(f"Control mode changed by joystick: {get_joystick_control_mode_name(pressed)}")
+            rospy.sleep(rospy.Duration(0.25))  # Sleep to avoid aggressive switching
+    
+    @staticmethod
+    def killswitch(buttons, wrench_publisher_handle):
+        rospy.logwarn("KILLSWITCH ENABLED!")
+        
+        wrench_publisher_handle.publish(Wrench())
+        rospy.sleep(rospy.Duration(1.0))
+
+        nodes = rosnode.get_node_names()
+        nodes_to_kill = ["/dp_controller_node", "thruster_interface", "pca9685_ros_driver"]
+        for node in nodes_to_kill:
+            if node in nodes:
+                ControlModeHandling.kill_node(node)
+            else:
+                rospy.loginfo(node + " is not running...")
+
+    @staticmethod
+    def emergency_stop(wrench_publisher_handle):
+        rospy.logwarn("EMERGENCY STOP ENABLED!")
+        nodes = rosnode.get_node_names()
+        node = "/dp_controller_node"
+        if node in nodes:
+            ControlModeHandling.kill_node(node)
+        else:
+            rospy.loginfo(node + " is not running...")
+        wrench_publisher_handle.publish(Wrench())
+        rospy.sleep(rospy.Duration(1.0))
+
+    @staticmethod
+    def pose3_hold():
+        rospy.logwarn("pose3_hold ENABLED")
+
+    @staticmethod
+    def pose4_hold():
+        rospy.logwarn("pose4_hold ENABLED")
+
+    # Function to kill a ROS node
+    @staticmethod
+    def kill_node(node_name):
+        command = ['rosnode', 'kill', node_name]
+        subprocess.call(command)
