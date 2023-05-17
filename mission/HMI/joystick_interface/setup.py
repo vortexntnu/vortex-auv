1c0b0a8d9f1c80613a2cd56e2584e40e23e5a399
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2023-05-18 01:04:44 +0200

    Update setup.py
    Remove commented out code.

--- mission/HMI/joystick_interface/setup.py
+++ mission/HMI/joystick_interface/setup.py
@@ -3,15 +3,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# setup(
-#     version="1.0.0",
-#     description="Library for the joystick interface",
-#     author="Benjaminas Visockis",
-#     author_email="benjamin.visockis@gmail.com",
-#     packages=['libjoystick'],
-#     package_dir={'': 'src'}
-# )
-
 d = generate_distutils_setup(packages=['libjoystick'], package_dir={'': 'src'})
 
 setup(**d)
