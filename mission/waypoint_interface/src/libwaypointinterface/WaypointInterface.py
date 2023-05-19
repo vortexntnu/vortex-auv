6edcc399f7347451de1c17bb72f7744a2732b537
Benjaminas Visockis <benjaminas.visockis@dnv.com>
2023-05-19 21:55:11 +0200

    Add comments to code.
    Compliments to the chef, ChatGPT-4.

--- mission/waypoint_interface/src/libwaypointinterface/WaypointInterface.py
+++ mission/waypoint_interface/src/libwaypointinterface/WaypointInterface.py
@@ -9,12 +9,15 @@ from geometry_msgs.msg import PoseStamped
 from libwaypointinterface.DPClient import DPClient
 
 class WaypointInterface:
-
+    """
+    The WaypointInterface class processes incoming waypoints from operator interfaces,
+    such as RViz and FoxGlove-Studio, and sends navigational goals to a guidance module (DP currently). 
+    """
     def __init__(self):
         rospy.init_node('waypoint_interface', anonymous=False)
 
         # Attributes
-        self.init_z = False
+        self.init_z = False # If Z-DOF is initiated, keep it so
 
         self.nav_goal = PoseStamped()
         self.nav_goal_z = 0.0
@@ -37,6 +40,15 @@ class WaypointInterface:
         rospy.loginfo("Waypoint Interface Initialized!")
 
     def nav_goal_to_dp_goal(self, enable_z=False):
+        """
+        Converts the current navigation goal to a DP goal and sends it to the DPClient.
+
+        Args:
+            enable_z (bool, default=False): Indicates whether to adjust the altitude of the DP goal.
+        
+        Returns:
+            bool: Stops and returns 'False' is 'odom' TF is not available.
+        """
         if not self.dp_client.get_enabled_status:
             self.dp_client.enable()
         
@@ -63,11 +75,27 @@ class WaypointInterface:
         return True
 
     def nav_goal_cb(self, posestamped_msg):
+        """
+        Callback function for "/move_base_simple/goal" topic.
+        Convert provided navigation goal message to a DP goal.
+        Sends the goal to DP server.
+
+        Args:
+            posestamped_msg (PoseStamped): The received PoseStamped message.
+        """
         self.nav_goal = posestamped_msg
         self.nav_goal_to_dp_goal()
         self.dp_client.send_goal()
 
     def nav_goal_z_cb(self, float32_msg):
+        """
+        Callback function for "/move_base_simple/goal/z" topic. 
+        Adds provided Z setpoint to the current Pose goal.
+        Sends the modified goal to DP server.
+
+        Args:
+            float32_msg (Float32): The received Float32 message.
+        """
         self.nav_goal_z = float32_msg.data
         self.nav_goal_to_dp_goal(enable_z=True)
         self.dp_client.send_goal()
