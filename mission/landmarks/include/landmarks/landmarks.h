a015244b1a77d0094f203b7e3e9b8684977c640f
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2023-05-22 18:29:46 +0200

    Small changes.

--- mission/landmarks/include/landmarks/landmarks.h
+++ mission/landmarks/include/landmarks/landmarks.h
@@ -20,9 +20,9 @@ public:
   /**
    * The callback function for the op_sub-subscriber.
    * @param objPose is the message received on the ROS-topic, containing an
-   * object ID and a pose of the object. The object ID and pose is
-   * stored in the objectPositions-map The message received is further published
-   * on the object_positions_out-topic.
+   * @param objPose is the message received on the ROS-topic, containing the
+   * object ID and the pose. They are stored in the objectPositions map.
+   * The message received is further published on the object_positions_out topic.
    */
   void callback(vortex_msgs::ObjectPosition objPose);
   /**
