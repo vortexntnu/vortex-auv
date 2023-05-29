d72d5e78fcfc0f3ed2583768f181e4dcbf3dd661
Benjaminas Visockis <benjaminas.visockis@vortexntnu.no>
2023-05-29 20:14:45 +0200

    Move dp_client_py to src as lib is ignored.

--- motion/control/dp_controller/setup.py
+++ motion/control/dp_controller/setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(packages=['dp_client_py'],
-                             package_dir={'': 'lib'})
+                             package_dir={'': 'src'})
 
 setup(**d)
