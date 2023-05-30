045f358b311a1c41d5e17081afcb6f0c200a2a02
Benjaminas Visockis <benjaminas.visockis@vortexntnu.no>
2023-05-30 02:42:01 +0200

    Add functionailty to the DP python client.

--- motion/control/dp_controller/src/dp_client_py/DPClient.py
+++ motion/control/dp_controller/src/dp_client_py/DPClient.py
@@ -6,6 +6,8 @@ import actionlib
 from vortex_msgs.msg import (dpAction, dpGoal, dpResult, dpFeedback)
 from geometry_msgs.msg import Pose
 
+import dynamic_reconfigure
+
 
 class DPClient:
     """
@@ -22,6 +24,9 @@ class DPClient:
         # Attributes
         self.is_enabled = False
 
+        self.acceptance_margins = [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0]
+        self.current_goal_id = ""
+        
         self.goal = dpGoal()
 
         # DP client handle
@@ -30,6 +35,8 @@ class DPClient:
         rospy.loginfo(f"{rospy.get_name()}: Waiting for DP Server...")
         self.client_handle.wait_for_server()
         rospy.loginfo(f"{rospy.get_name()}: Connected to DP Server!")
+
+        self.rcfg_client = dynamic_reconfigure.client.Client("/dp_controller", timeout=1)
 
         rospy.Subscriber("/DpAction/result", dpResult, self.result_cb)
         self.result = False
@@ -59,6 +66,7 @@ class DPClient:
         """
         rospy.set_param("/DP/Enable", True)
         self.is_enabled = True
+        self.result = False
 
     def disable(self):
         """
@@ -67,6 +75,38 @@ class DPClient:
         rospy.set_param("/DP/Enable", False)
         self.is_enabled = False
 
+    def get_acceptance_margins(self):
+        config_map = self.rcfg_client.get_configuration()
+        self.acceptance_margins = [config_map["Margin_x"], config_map["Margin_y"], config_map["Margin_z"],
+                                   config_map["Margin_roll"], config_map["Margin_pitch"], config_map["Margin_yaw"]
+                                  ]
+        return self.acceptance_margins
+
+    def set_acceptance_margins(self, DOF_array):
+        new_acceptance_margins = self.get_acceptance_margins()
+        for i, DOF in enumerate(DOF_array):
+            if DOF == 0.0 or DOF == -1.0:
+                pass
+            new_acceptance_margins[i] = DOF
+        self.rcfg_client.update_configuration({"Margin_x":new_acceptance_margins[0], 
+                                               "Margin_y":new_acceptance_margins[1], 
+                                               "Margin_z":new_acceptance_margins[2], 
+                                               "Margin_roll":new_acceptance_margins[3], 
+                                               "Margin_pitch":new_acceptance_margins[4], 
+                                               "Margin_yaw":new_acceptance_margins[5], 
+                                               })
+        self.acceptance_margins = new_acceptance_margins
+        rospy.loginfo(f"{rospy.get_name()}: Updated DP acceptance margins!")
+
+        return True
+    
+    def has_reached_goal(self):
+        margins = self.get_acceptance_margins()
+        for i, margin in enumerate(margins):
+            if abs(self.feedback[i]) > margin:
+                return False
+        return True
+
     def send_goal(self):
         """
         Sends the goal to the DP server.
@@ -74,6 +114,7 @@ class DPClient:
         Returns:
             bool: True if the goal was sent successfully, False otherwise.
         """
+        self.result = False
 
         is_server = self.client_handle.wait_for_server(
             timeout=rospy.Duration(3))
@@ -101,9 +142,9 @@ class DPClient:
         Args:
             msg_bool (Bool): The received Bool message.
         """
-        self.result = msg_bool
+        self.result = msg_bool.result.finished
 
-    def feedback_cb(self, float32_arr_msg):
+    def feedback_cb(self, fb_msg):
         """
         Callback function for "/DpAction/feedback" topic.
         Provides current error-to-goal for each degree-of-freedom (DOF).
@@ -111,4 +152,5 @@ class DPClient:
         Args:
             float32_arr_msg (Float32[6]): The received Float32[6] message.
         """
-        self.feedback = float32_arr_msg
+        self.feedback = list(fb_msg.feedback.error)
+        self.current_goal_id = fb_msg.status.goal_id.id
