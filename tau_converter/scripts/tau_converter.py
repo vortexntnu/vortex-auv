d606b590acf6579fc0256963498c7962b4f3b808
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2023-06-17 21:25:22 +0200

    Streamline tau republisher

--- tau_converter/scripts/tau_converter.py
+++ tau_converter/scripts/tau_converter.py
@@ -5,13 +5,12 @@ from geometry_msgs.msg import Wrench
 
 
 class TauRepublisher():
-    """Republishes tau from DP controller
+    """Republishes tau with a flipped z axis
     """
 
     def __init__(self, topic):
         rospy.init_node('tau_republisher_node')
 
-        self.tau_cmd_msg = Wrench()
         self.dpTauSub = rospy.Subscriber(topic, Wrench,
                                          self.tau_repub_callback)
         self.tauPub = rospy.Publisher('/thrust/desired_forces/flipped',
@@ -19,24 +18,20 @@ class TauRepublisher():
                                       queue_size=1)
 
     def tau_repub_callback(self, msg):
+        tau_cmd_msg = Wrench()
 
         #Setting the message equal to the dp force
-        self.tau_cmd_msg.force = msg.force
-        self.tau_cmd_msg.torque = msg.torque
+        tau_cmd_msg.force = msg.force
+        tau_cmd_msg.torque = msg.torque
 
         #Flipping sign in z
-        self.tau_cmd_msg.force.z = self.tau_cmd_msg.force.z * (-1)
-
-    def spin(self):
-
-        while not rospy.is_shutdown():
-            self.tauPub.publish(self.tau_cmd_msg)
-
+        tau_cmd_msg.force.z = tau_cmd_msg.force.z * (-1)
+        self.tauPub.publish(tau_cmd_msg)
 
 if __name__ == '__main__':
     try:
         tau_repub_node = TauRepublisher(topic='/thrust/desired_forces')
-        tau_repub_node.spin()
+        rospy.spin()
 
     except rospy.ROSInterruptException:
         pass
