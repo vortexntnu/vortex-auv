0d67561849aeba217e008dbf5eca8a1e20e913e4
Clang Robot <robot@example.com>
2023-11-08 10:11:23 +0000

    Committing clang-format changes

--- motion/control/gripper_tb/src/main.cpp
+++ motion/control/gripper_tb/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-
 // define pins used
 #define servo_pwm_pin 7
 #define dc_motor_pwm_pin 6
@@ -11,7 +10,7 @@
 #define pot_pin A1
 
 // Setup for serial to pc
-SoftwareSerial testPort(1,0);
+SoftwareSerial testPort(1, 0);
 
 // defining constants f
 unsigned long pwm_full_duty = 1860;
@@ -21,8 +20,8 @@ unsigned long pwm_zero_duty = 1040;
 uint16_t control_val = 0;
 unsigned long dc_motor_pwm_count = 0;
 uint16_t duty_cycle = 0;
-const double adc_to_duty = 820.0/1023.0; // (pwm_full_duty - pwm_zero_duty) / adc_resolution
-
+const double adc_to_duty =
+    820.0 / 1023.0; // (pwm_full_duty - pwm_zero_duty) / adc_resolution
 
 // function declarations
 uint16_t adc_to_duty_percent(int adc_val);
@@ -37,20 +36,21 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
+
   // Start each software serial port
   testPort.begin(9600);
   testPort.listen();
   */
-  //Serial.println(adc_to_duty);
-  // Setup pins for inn and output
+  // Serial.println(adc_to_duty);
+  //  Setup pins for inn and output
   pinMode(green_led, OUTPUT);
   pinMode(red_led, OUTPUT);
   pinMode(dc_motor_pwm_pin, OUTPUT);
   // pinMode(servo_pwm_pin, OUTPUT);
   pinMode(pot_pin, INPUT);
   pinMode(ks_pin, INPUT);
-  /* dont start program unless dc control is at 0 and kill switch is not activated */ 
+  /* dont start program unless dc control is at 0 and kill switch is not
+   * activated */
   check_duty_low();
   arm_esc();
 }
@@ -59,16 +59,16 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(ks_pin)) { /* Kill switch not activated */
     control_val = analogRead(pot_pin);
-    
+
     dc_motor_pwm_control(adc_to_duty_percent(control_val));
-  }
-  else { /* Kill switch activated */
-    /* dont continue program again unless dc control is at 0 and kill switch is not activated */ 
+  } else { /* Kill switch activated */
+    /* dont continue program again unless dc control is at 0 and kill switch is
+     * not activated */
     check_duty_low();
   }
 }
 
-/* dont continue unless dc control is at 0 and kill switch is not activated */ 
+/* dont continue unless dc control is at 0 and kill switch is not activated */
 void check_duty_low() {
   control_val = analogRead(pot_pin);
   while ((control_val > 20) || (digitalRead(ks_pin) == 0)) {
@@ -81,8 +81,8 @@ void check_duty_low() {
   digitalWrite(red_led, LOW);
 }
 /*
-* 
-*/
+ *
+ */
 uint16_t adc_to_duty_percent(int adc_val) {
   uint16_t duty_percent = 0;
   if (adc_val > 20) {
@@ -106,5 +106,5 @@ void dc_motor_pwm_control(uint16_t duty_cycle) {
 
 void arm_esc() {
   digitalWrite(dc_motor_pwm_pin, LOW);
-  delayMicroseconds(1860*3);
+  delayMicroseconds(1860 * 3);
 }
