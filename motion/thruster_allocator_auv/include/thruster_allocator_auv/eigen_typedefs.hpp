fd49a378da993c6bc294661bbf715160d6c91b21
Clang Robot <robot@example.com>
2024-02-11 17:16:10 +0000

    Committing clang-format changes

--- motion/thruster_allocator_auv/include/thruster_allocator_auv/eigen_typedefs.hpp
+++ motion/thruster_allocator_auv/include/thruster_allocator_auv/eigen_typedefs.hpp
@@ -4,7 +4,7 @@
 #include <Eigden/Dense>
 
 namespace Eigen {
-    typedef Eigen::Matrix<double, 6, 1> Vector6d;
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
 }
 
-#endif //VORTEX_EIGEN_TYPEDEFS_H
+#endif // VORTEX_EIGEN_TYPEDEFS_H
