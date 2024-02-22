9620db8a0aa45555a96eb3cd6f60a9ac2f74b9b4
Clang Robot <robot@example.com>
2024-02-22 18:02:17 +0000

    Committing clang-format changes

--- motion/thruster_interface/src/thruster_interface_node.cpp
+++ motion/thruster_interface/src/thruster_interface_node.cpp
@@ -1,10 +1,9 @@
-#include <cstdio>
 #include "thruster_interface_lib.hpp"
+#include <cstdio>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
 
   printf("hello world thruster_interface package\n");
   return 0;
