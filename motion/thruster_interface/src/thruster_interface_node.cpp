a8a7521e59ed28665d002cdac90989b40d772c83
vortexuser <vortex.git@vortexntnu.no>
2024-02-22 19:01:35 +0100

    Added the thruster_interface package for ros2 enviroment, cheked that it builds and sources properly

---
+++ motion/thruster_interface/src/thruster_interface_node.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "thruster_interface_lib.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world thruster_interface package\n");
+  return 0;
+}
