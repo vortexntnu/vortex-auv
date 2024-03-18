40e7154672f8e93df8564be2526695334700a8c7
Alekskl <Aleksander.klu@hotmail.com>
2024-03-18 19:13:33 +0100

    added launch file and renamed new_auv to orca

---
+++ auv_setup/launch/orca.launch.py
@@ -0,0 +1,34 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Set environment variable
+    set_env_var = SetEnvironmentVariable(
+        name='ROSCONSOLE_FORMAT',
+        value='[${severity}] [${time}] [${node}]: ${message}'
+    )
+
+    # Thruster Allocator launch
+    thruster_allocator_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('thruster_allocator_auv'),'launch','thruster_allocator_auv.launch.py')
+        )
+    )
+
+    #Thruster Interface launch
+    thruster_interface_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('thruster_interface_auv'),'launch','thruster_interface_auv.launch.py')
+        )
+    )
+
+    # Return launch description
+    return LaunchDescription([
+        set_env_var,
+        thruster_allocator_launch,
+        thruster_interface_launch
+    ])
