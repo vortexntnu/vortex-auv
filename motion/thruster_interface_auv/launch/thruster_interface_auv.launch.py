13bb8075b2f0dee9fe575098dba74df477a2579f
Alekskl <Aleksander.klu@hotmail.com>
2024-03-21 19:18:07 +0100

    added mapping, direction and offset to thruster interface.

--- motion/thruster_interface_auv/launch/thruster_interface_auv.launch.py
+++ motion/thruster_interface_auv/launch/thruster_interface_auv.launch.py
@@ -1,8 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from os import path
 
 
 def generate_launch_description():
+
+    yaml_file_path = path.join(
+        get_package_share_directory("thruster_interface_auv"),
+        "../../../../", # Go back to the workspace
+        "src/vortex-auv/auv_setup/config/robots/",
+        "orca.yaml"
+    )
+        
     return LaunchDescription([
         Node(
             package='thruster_interface_auv',
@@ -10,5 +20,6 @@ def generate_launch_description():
             name='thruster_interface_auv_node',
             output='screen',
             emulate_tty=True,
+            parameters=[yaml_file_path]
         )
     ])
