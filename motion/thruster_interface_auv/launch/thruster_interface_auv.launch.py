dc6efcb92bc91a485353cf0bc21d2bd43ffa2abc
Alekskl <Aleksander.klu@hotmail.com>
2024-03-21 19:57:11 +0100

    does not read from file anymore, must fix

--- motion/thruster_interface_auv/launch/thruster_interface_auv.launch.py
+++ motion/thruster_interface_auv/launch/thruster_interface_auv.launch.py
@@ -5,18 +5,11 @@ from os import path
 
 
 def generate_launch_description():
-
-    yaml_file_path = path.join(
-        get_package_share_directory("thruster_interface_auv"),
-        "../../../../",  # Go back to the workspace
-        "src/vortex-auv/auv_setup/config/robots/",
-        "orca.yaml")
-
+    # TODO: read parameters from yaml file
     return LaunchDescription([
         Node(package='thruster_interface_auv',
              executable='thruster_interface_auv_node.py',
              name='thruster_interface_auv_node',
              output='screen',
-             emulate_tty=True,
-             parameters=[yaml_file_path])
+             emulate_tty=True)
     ])
