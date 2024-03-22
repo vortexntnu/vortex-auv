7451c3013c7c94598a9bc4c01ece6e0afd12eb51
Alekskl <Aleksander.klu@hotmail.com>
2024-03-22 12:36:36 +0100

    fixed parameters for thruster interface

--- motion/thruster_interface_auv/launch/thruster_interface_auv.launch.py
+++ motion/thruster_interface_auv/launch/thruster_interface_auv.launch.py
@@ -3,13 +3,14 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from os import path
 
-
 def generate_launch_description():
-    # TODO: read parameters from yaml file
-    return LaunchDescription([
-        Node(package='thruster_interface_auv',
-             executable='thruster_interface_auv_node.py',
-             name='thruster_interface_auv_node',
-             output='screen',
-             emulate_tty=True)
-    ])
+    thruster_interface_auv_node = Node(
+        package='thruster_interface_auv',
+        executable='thruster_interface_auv_node.py',
+        name='thruster_interface_auv_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[path.join(get_package_share_directory('auv_setup'), 'config',
+                    'robots', 'orca.yaml')]
+    )
+    return LaunchDescription([thruster_interface_auv_node])
