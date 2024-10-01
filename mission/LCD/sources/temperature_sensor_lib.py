218f22c7928a53cd8d66479fd0278086cf939235
Andreas Kluge Svendsrud <89779148+kluge7@users.noreply.github.com>
2024-10-01 18:37:49 +0200

    refactor: apply code formatting fixes via pre-commit hooks

--- mission/LCD/sources/temperature_sensor_lib.py
+++ mission/LCD/sources/temperature_sensor_lib.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-! NOTE: 
+! NOTE:
 ! For now we don't have a external sensor to measure internal temperature
 ! Instead we just use Internal Computer temperature sensor to gaugue temperature of the environment approximately
 ! In the future someone should implement a external temperature sensor for measuting a more accurate state of the temperatuer on the inside of the AUV
