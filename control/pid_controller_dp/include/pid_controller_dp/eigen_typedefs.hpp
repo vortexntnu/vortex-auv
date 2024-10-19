f21369f3a30e77fd364120d84dad0494ce7876e2
Anders Høgden <andeshog@stud.ntnu.no>
2024-10-19 09:52:00 +0200

    remove unnecessary typedef

--- control/pid_controller_dp/include/pid_controller_dp/eigen_typedefs.hpp
+++ control/pid_controller_dp/include/pid_controller_dp/eigen_typedefs.hpp
@@ -12,7 +12,6 @@ namespace Eigen {
 typedef Eigen::Matrix<double, 6, 6> Matrix6d;
 typedef Eigen::Matrix<double, 3, 3> Matrix3d;
 typedef Eigen::Matrix<double, 6, 1> Vector6d;
-typedef Eigen::Matrix<double, 3, 1> Vector3d;
 }
 
 #endif
