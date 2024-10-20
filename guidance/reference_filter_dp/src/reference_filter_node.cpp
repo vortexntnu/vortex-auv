79fc2aa7c584f15391d5a259ae0a20d449684ef8
Anders Høgden <andeshog@stud.ntnu.no>
2024-10-20 18:48:07 +0200

    feat: add multithreaded executor

--- guidance/reference_filter_dp/src/reference_filter_node.cpp
+++ guidance/reference_filter_dp/src/reference_filter_node.cpp
@@ -3,7 +3,13 @@
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Started reference filter node");
-    rclcpp::spin(std::make_shared<ReferenceFilterNode>());
+    
+    auto node = std::make_shared<ReferenceFilterNode>();
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    
+    executor.spin();
+
     rclcpp::shutdown();
     return 0;
 }
