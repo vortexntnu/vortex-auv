21cd628b5cf4784c5b34ddc321d3dc5dd7305ef5
christopherbjorge <chrisbjoe@hotmail.com>
2024-10-20 16:33:55 +0200

    ekf filtering

--- navigation/ekf_filtering/include/ekf_filtering/ekf_filtering_ros.hpp
+++ navigation/ekf_filtering/include/ekf_filtering/ekf_filtering_ros.hpp
@@ -1,23 +1,32 @@
-#ifndef EKF_FILTER_ROS_HPP
-#define EKF_FILTER_ROS_HPP
+#ifndef EKF_FILTERING_ROS_HPP
+#define EKF_FILTERING_ROS_HPP
 
 #include <chrono>
 #include <memory>
 #include <string>
-#include "libraries.hpp"
+#include <rclcpp/rclcpp.hpp>
 
 #include "geometry_msgs/msg/point_stamped.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "message_filters/subscriber.h"
-#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/parameter_event_handler.hpp"
+#include <rclcpp/qos.hpp>
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/create_timer_ros.h"
 #include "tf2_ros/message_filter.h"
 #include "tf2_ros/transform_listener.h"
-#include "src/vortex-aruco-detection/aruco-detector/include/aruco_detector/aruco_detector_ros.hpp"
+#include <tuple>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <vortex_filtering/vortex_filtering.hpp>
 
 
-#endif
+
+#include <vector>
+//#include <opencv2/opencv.hpp>
+
+
+
 /* Requirements
 - A node that subscribes to a node for the kalman_callback which gives the local coordinates as 6DOF
 - A node that publishes coordinates in global frame
@@ -51,33 +60,24 @@ For KallmanFilterCallback function:
 */
 
 
-using std::chrono_literals;
+using std::placeholders::_1;
+
 
 namespace vortex
 {
+
 namespace ekf_filtering
 
 {
-class EkfMapperNode : public rclcpp::Node {
+
+class EkfFilteringNode : public rclcpp::Node{
+
     public:
-    void ekf_filtering::EkfFilteringNode::EkfFilterCallback()
-    {
-        /*
-        * @brief Constructs an ArucoDetectorNode object.
-        */
-        EkfMapperNode(const rclcpp::NodeOptions & options);
 
-        /**
-         * @brief Destroys the ArucoDetector object.
-         */
-        ~EkfMapperNode(){};
+        EkfFilteringNode();
 
+        ~EkfFilteringNode(){};
 
-
-
-
-
-    }
     private:
     
         std::string target_frame_; //Frame to map the local frame data
@@ -92,34 +92,37 @@ class EkfMapperNode : public rclcpp::Node {
         using SensMod = vortex::models::IdentitySensorModel<6,6>;
         using EKF = vortex::filter::EKF<DynMod, SensMod>;
 
-        enum class BoardDetectionStatus{
-            BOARD_NEVER_DETECTED,
-            MEASUREMENT_AVAILABLE,
-        };
+        static rclcpp::Time previous_time;
+        rclcpp::Time current_time;
+        rclcpp::Duration time_since_previous_callback;
+        bool callback_flag;
+        
 
         void kalmanFilterCallback();
+        void poseCallback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr pose_msg);
 
 
         std::shared_ptr<DynMod> dynamic_model_;
         std::shared_ptr<SensMod> sensor_model_;
         vortex::prob::Gauss<6> board_pose_est_;
-        
+
+
+        std::string target_frame_;
+        std::shared_ptr<tf2_ros::Buffer> tf2_buffer;
+        std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;
+        std::chrono::duration<int> buffer_timeout;
+
+        // Subscriber and message filter for the input PoseStamped messages
+        message_filters::Subscriber<geometry_msgs::msg::PoseStamped> pose_sub_;
+        std::shared_ptr<tf2_ros::MessageFilter<geometry_msgs::msg::PoseStamped>> tf2_filter_;
+
+        // Publisher for the transformed poses
+        rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr transformed_pose_pub
 
 
 
+} 
+} //namespace ekf_filtering
+} //namespace vortex
 
-
-
-
-
-
-}
-
-
-
-
-
-
-
-} //std::chrono_literals
-} //ekf_filtering
+#endif
--- navigation/ekf_filtering/src/ekf_filtering_ros.cpp
+++ navigation/ekf_filtering/src/ekf_filtering_ros.cpp
@@ -1,4 +1,4 @@
-#include "ekf_filtering_ros.hpp"
+#include <ekf_filtering/ekf_filtering_ros.hpp>
 #include <sstream>
 #include <filesystem>
 
@@ -25,28 +25,77 @@ The output should be in the global frame, not local frame
 */
 
 /// tie
-template<typename... _Elements>
-constexpr tuple<_Elements&...>
-tie(_Elements&... __args) noexcept
-{ 
-    return tuple<_Elements&...>(__args...); 
+
+using std::placeholders::_1;
+
+namespace vortex {
+namespace ekf_filtering {
+
+EkfFilteringNode::EkfFilteringNode() : Node("pose_transformer_node")
+    {
+        // Declare and acquire `target_frame` parameter
+        target_frame_ = this->declare_parameter<std::string>("target_frame", "odom");
+
+        std::chrono::duration<int> buffer_timeout(1);
+
+        tf2_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+        // Create the timer interface before call to waitForTransform,
+        // to avoid a tf2_ros::CreateTimerInterfaceException exception
+        auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
+        this->get_node_base_interface(),
+        this->get_node_timers_interface());
+
+        tf2_buffer_->setCreateTimerInterface(timer_interface);
+        tf2_listener_ =
+        std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
+
+        // Adjust the QoS to match the publisher's QoS
+        rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(10)).reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+
+        // Subscribe to the input PoseStamped topic
+        pose_sub_.subscribe(this, "/aruco_board_pose_camera", qos.get_rmw_qos_profile());
+
+        // Set up the MessageFilter to transform poses into the "odom" frame
+        tf2_filter_ = std::make_shared<tf2_ros::MessageFilter<geometry_msgs::msg::PoseStamped>>(
+            pose_sub_, *tf2_buffer_, "odom", 100, this->get_node_logging_interface(), this->get_node_clock_interface());
+
+        tf2_filter_->registerCallback(std::bind(&EkfFilteringNode::poseCallback, this, _1));
+
+        // Publisher for the transformed PoseStamped message
+        transformed_pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/aruco_board_pose_odom", 10);
+
+        RCLCPP_INFO(this->get_logger(), "EkfFilteringNode has been started.");
     }
 
-void object_filter::EkfFilteringNode::kalmanFilterCallback()
+
+void EkfFilteringNode::kalmanFilterCallback()
 {
     static rclcpp::Time previous_time = this->now();
     rclcpp::Time current_time = this->now();
-    rclcpp::Duration time_since_previous_callback = current_time - previous_time;
+    //clcpp::Duration time_since_previous_callback = current_time - previous_time;
     previous_time = current_time;
+    callback_flag = false;
+
 
     auto [status, board_pose_meas, stamp] = board_measurement_.getBoardPoseStamp();
-    switch(status) 
+    
+    //if no previous measurement, previous - current = 
+
+    //else we have previous measurement
+
+    if(callback_flag == false)
     {
-    case BoardDetectionStatus::BOARD_NEVER_DETECTED:
-        return;
-    case BoardDetectionStatus::MEASUREMENT_AVAILABLE:
-        std::tie(board_pose_est_, std::ignore, std::ignore) = EKF::step(*dynamic_model_, *sensor_model_, time_since_previous_callback.seconds(),board_pose_est_, board_pose_meas);
+        //run first step
+        callback_flag = true;
+    }
+    else
+    {
+        //run ekf step with previous steps
+        callback_flag = false;
+        std::tie(board_pose_est_, std::ignore, std::ignore) = EKF::step(*dynamic_model_, *sensor_model_, previous_time, board_pose_est_, board_pose_meas);
         break;
+
+    }
    
     cv::Vec3d rvec,tvec;
     tvec[0] = board_pose_est_.mean()(0);
@@ -61,6 +110,22 @@ void object_filter::EkfFilteringNode::kalmanFilterCallback()
     geometry_msgs::msg::PoseStamped pose_msg = cv_pose_to_ros_pose_stamped(tvec, quat, frame_, stamp);
     board_pose_pub_->publish(pose_msg);
     }
-}
+
+void poseCallback(pose_msg)
+{
+    //Attempt to transform the PosedStamped into the "odom" frame
+    geometry_msgs::msg::PoseStamped transformed_pose;
+    try {
+        tf2_buffer_ -> transform(*pose_msg, transformed_pose, "odom", tf2::Duration(std::chrono::milliseconds(100)));
+        transformed_pose_pub_ ->publish(transformed_pose)
+        RCLCPP_INFO(this->get_logger(), "Pose transformed and published.");
+    } catch (tf2::TransformException &ex) {
+        RCLCPP_WARN(this->get_logger(), "Transform failed: %s", ex.what());
+    }
+    }
+
+
+} //namespace ekf filtering
+} //namespace vortex
 
 
