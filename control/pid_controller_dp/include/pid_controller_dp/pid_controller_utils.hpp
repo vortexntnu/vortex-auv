3a2076f1c41d33c7c1aa4ea146142e14066c3ec4
Talha Nauman Choudhry <talhanc@stud.ntnu.no>
2024-10-23 15:47:56 +0200

    refactor: Update include order in pid_controller_utils.hpp
    Reorder the include statements in pid_controller_utils.hpp to follow the established convention of including standard library headers before external library headers. This improves code readability and maintainability.
    
    Refactor the include order in pid_controller_utils.hpp to adhere to the established convention.

--- control/pid_controller_dp/include/pid_controller_dp/pid_controller_utils.hpp
+++ control/pid_controller_dp/include/pid_controller_dp/pid_controller_utils.hpp
@@ -1,12 +1,12 @@
 #ifndef PID_UTILS_HPP
 #define PID_UTILS_HPP
 
-#include <std_msgs/msg/float64_multi_array.hpp>
 #include "pid_controller_dp/eigen_typedefs.hpp"
-#include <eigen3/Eigen/Geometry>
 #include <cmath>
-#include <tf2/LinearMath/Quaternion.h>
+#include <eigen3/Eigen/Geometry>
+#include <std_msgs/msg/float64_multi_array.hpp>
 #include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 Eigen::Matrix6d float64multiarray_to_diagonal_matrix6d(const std_msgs::msg::Float64MultiArray &msg);
 
--- control/pid_controller_dp/src/pid_controller.cpp
+++ control/pid_controller_dp/src/pid_controller.cpp
@@ -3,46 +3,48 @@
 #include <iostream>
 
 PIDController::PIDController()
-    : Kp_(Eigen::Matrix6d::Identity()),   
-      Ki_(Eigen::Matrix6d::Zero()),        
-      Kd_(Eigen::Matrix6d::Zero()),             
-      integral_(Eigen::Vector6d::Zero()), 
-      dt(0.01)                     
-{}
+    : Kp_(Eigen::Matrix6d::Identity()),
+      Ki_(Eigen::Matrix6d::Zero()),
+      Kd_(Eigen::Matrix6d::Zero()),
+      integral_(Eigen::Vector6d::Zero()),
+      dt(0.01) {}
 
 Eigen::Vector6d PIDController::calculate_tau(const Eigen::Vector6d &eta, const Eigen::Vector6d &eta_d, const Eigen::Vector6d &nu, const Eigen::Vector6d &eta_dot_d) {
-    Eigen::Vector6d error = apply_ssa(eta - eta_d);
+  Eigen::Vector6d error = apply_ssa(eta - eta_d);
 
-    Eigen::Matrix6d J = calculate_J(eta);
+  Eigen::Matrix6d J = calculate_J(eta);
 
-    Eigen::Matrix6d J_inv = J.inverse();
+  Eigen::Matrix6d J_inv = J.inverse();
 
-    Eigen::Vector6d nu_d = J_inv * eta_dot_d;
+  Eigen::Vector6d nu_d = J_inv * eta_dot_d;
 
-    Eigen::Vector6d error_nu = nu - nu_d;
+  Eigen::Vector6d error_nu = nu - nu_d;
 
-    Eigen::Vector6d tau = -(Kp_ * J_inv * error + Ki_ * integral_ + Kd_ * error_nu);
+  Eigen::Vector6d tau = -(Kp_ * J_inv * error + Ki_ * integral_ + Kd_ * error_nu);
 
-    integral_ = anti_windup(dt, error, integral_);
+  integral_ = anti_windup(dt, error, integral_);
 
-    return tau;
+  return tau;
 }
 
 void PIDController::setKp(const Eigen::Matrix6d &Kp) {
-    this->Kp_ = Kp;
-    std::cout << "Kp: " << std::endl << Kp_ << std::endl;
+  this->Kp_ = Kp;
+  std::cout << "Kp: " << std::endl
+            << Kp_ << std::endl;
 }
 
 void PIDController::setKi(const Eigen::Matrix6d &Ki) {
-    this->Ki_ = Ki;
-    std::cout << "Ki: " << std::endl << Ki_ << std::endl;
+  this->Ki_ = Ki;
+  std::cout << "Ki: " << std::endl
+            << Ki_ << std::endl;
 }
 
 void PIDController::setKd(const Eigen::Matrix6d &Kd) {
-    this->Kd_ = Kd;
-    std::cout << "Kd: " << std::endl << Kd_ << std::endl;
+  this->Kd_ = Kd;
+  std::cout << "Kd: " << std::endl
+            << Kd_ << std::endl;
 }
 
 void PIDController::setTimeStep(double dt) {
-    this->dt = dt;
+  this->dt = dt;
 }
