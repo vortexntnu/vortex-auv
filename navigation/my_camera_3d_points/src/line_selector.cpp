02221f4b8e70aad65a6a1f40b3e4115b7d0dd1c6
christopherbjorge <chrisbjoe@hotmail.com>
2024-11-10 14:03:20 +0100

    added file for development

--- navigation/my_camera_3d_points/src/line_selector.cpp
+++ navigation/my_camera_3d_points/src/line_selector.cpp
@@ -3,7 +3,11 @@ Todo
 select which line is the active line to follow
 filter the lines into active and next line. Lines recieved as a pose stamped message
 recieving lines as 3d endpoints 
+donn| know het whhere start and end, use orientation of camera to decide with x/y coord
 
+
+kham has transformed points to 3d
+function that uses the 2d ponints, and sorts/groups the points to a line in camera frame
 
 */
 
