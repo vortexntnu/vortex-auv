1737bfb41176dbf16b0f4354256d0307a2483b99
christopherbjorge <chrisbjoe@hotmail.com>
2024-11-10 14:02:48 +0100

    new files for development

---
+++ navigation/my_camera_3d_points/src/line_selector.cpp
@@ -0,0 +1,11 @@
+/*
+Todo
+select which line is the active line to follow
+filter the lines into active and next line. Lines recieved as a pose stamped message
+recieving lines as 3d endpoints 
+
+
+*/
+
+
+
