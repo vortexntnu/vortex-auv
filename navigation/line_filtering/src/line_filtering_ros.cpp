13e573e4aa9d957cb5906a49ec85e1f6a249a858
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2024-11-10 19:57:41 +0100

    sim interface setup

--- navigation/line_filtering/src/line_filtering_ros.cpp
+++ navigation/line_filtering/src/line_filtering_ros.cpp
@@ -2,29 +2,33 @@
 
 Camera3DPointsNode::Camera3DPointsNode() : Node("camera_3d_points_node")
 {
+    rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
+    auto qos_sensor_data = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 1), qos_profile);
+
     image_sub_ = image_transport::create_subscription(
-        this, "/zed/zed_node/left/image_rect_color", std::bind(&Camera3DPointsNode::imageCallback, this, std::placeholders::_1), "raw");
+        this, "/cam_down/image_color", std::bind(&Camera3DPointsNode::imageCallback, this, std::placeholders::_1), "raw");
 
     camera_info_sub_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
-        "/zed/zed_node/left/camera_info", 10, std::bind(&Camera3DPointsNode::cameraInfoCallback, this, std::placeholders::_1));
+        "/cam_down/camera_info", qos_sensor_data, std::bind(&Camera3DPointsNode::cameraInfoCallback, this, std::placeholders::_1));
     
    depth_sub_ = this->create_subscription<std_msgs::msg::Float64>(
-    "dvl/depth", 10, std::bind(&Camera3DPointsNode::depthCallback, this, std::placeholders::_1));
+    "/dvl/depth", qos_sensor_data, std::bind(&Camera3DPointsNode::depthCallback, this, std::placeholders::_1));
 
     depth_.data = -1.0;
 
-    point_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("camera_3d_points", 10);
-    upper_left_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("upper_left", 10);
-    upper_right_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("upper_right", 10);
-    lower_left_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("lower_left", 10);
-    lower_right_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("lower_right", 10);
-    pointmiddle_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("middle", 10);
+    point_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("camera_3d_points", qos_sensor_data);
+    upper_left_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("upper_left", qos_sensor_data);
+    upper_right_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("upper_right", qos_sensor_data);
+    lower_left_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("lower_left", qos_sensor_data);
+    lower_right_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("lower_right", qos_sensor_data);
+    pointmiddle_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("middle", qos_sensor_data);
 }
 
 void Camera3DPointsNode::cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg)
 {
     K_ = cv::Mat(3, 3, CV_64F, const_cast<double*>(msg->k.data())).clone();
     RCLCPP_INFO(this->get_logger(), "Camera Intrinsic Matrix initialized.");
+    camera_info_sub_.reset();
 }
 
 void Camera3DPointsNode::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg)
@@ -33,6 +37,8 @@ void Camera3DPointsNode::imageCallback(const sensor_msgs::msg::Image::ConstShare
         RCLCPP_WARN(this->get_logger(), "Camera intrinsic matrix not initialized yet.");
         return;
     }
+
+    RCLCPP_INFO(this->get_logger(), "Received image with size %dx%d", msg->width, msg->height);
 
     cv_bridge::CvImagePtr cv_ptr;
     try {
@@ -50,10 +56,12 @@ void Camera3DPointsNode::imageCallback(const sensor_msgs::msg::Image::ConstShare
         cv::Point2f(cv_ptr->image.cols / 2.0, cv_ptr->image.rows / 2.0)
     };
 
+    
+
     if (depth_.data == -1.0){
         return;
     }
-    float64_t depth = depth_.data;
+    double depth = depth_.data;
 
     std::vector<cv::Point3f> points_3d;
     for (size_t i = 0; i < image_points.size(); ++i) {
@@ -68,6 +76,7 @@ void Camera3DPointsNode::imageCallback(const sensor_msgs::msg::Image::ConstShare
         pose_msg.pose.position.z = depth;
         pose_msg.pose.orientation.w = 1.0;
 
+    
         switch (i) {
             case 0: upper_left_->publish(pose_msg); break;
             case 1: upper_right_->publish(pose_msg); break;
@@ -75,6 +84,7 @@ void Camera3DPointsNode::imageCallback(const sensor_msgs::msg::Image::ConstShare
             case 3: lower_right_->publish(pose_msg); break;
             case 4: pointmiddle_->publish(pose_msg); break;
         }
+    
 
         RCLCPP_INFO(this->get_logger(), "Point %zu: 2D (%f, %f) -> 3D (%f, %f, %f)", 
                     i + 1, image_points[i].x, image_points[i].y, X, Y, depth);
@@ -96,6 +106,6 @@ void Camera3DPointsNode::imageCallback(const sensor_msgs::msg::Image::ConstShare
 
     void Camera3DPointsNode::depthCallback(const std_msgs::msg::Float64::SharedPtr msg)
 {
-    double depth = msg->data;
-    RCLCPP_INFO(this->get_logger(), "Received depth: %f", depth);
+    depth_.data = msg->data;
+    RCLCPP_INFO(this->get_logger(), "Received depth: %f", msg->data);
 }
