0ce92115b490b160e6fd79dfda76ea7dd51e9540
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-02-02 16:28:04 +0100

    refactor(docker): updated to use export for environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,19 @@
 # This script builds and runs a Docker image
 
 # Define environment variables for the Docker build process and execute the build script
-IMAGE="asv-image:latest" \
-BASE_IMAGE="rwthika/ros2:humble" \
-TARGET="dev" \
-PLATFORM="linux/arm64" \
+export IMAGE="asv-image:latest"
+export BASE_IMAGE="rwthika/ros2:humble"
+export TARGET="dev"
+# export PLATFORM="linux/arm64"  # Uncomment this line for ARM64 builds
+# export PLATFORM="amd64"  # Uncomment this line for AMD64 builds
+
+# Run the build script
 ./docker/docker-ros/scripts/build.sh
 
 # Run the built Docker image with appropriate flags
 docker run -it --rm \
     --privileged \
-    --network=host \
+    --network host \
     --ipc=host \
     -v "$(pwd):/docker-ros/ws/src/target:rshared" \
     "asv-image:latest" /bin/bash
