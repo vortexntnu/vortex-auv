73c788fd432ca8f7df65f9e8e386fdaa0cc24010
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-02-26 10:41:01 +0100

    docs(scripts): updated documentation for python packages in ci_install_dependencies.sh

--- scripts/ci_install_dependencies.sh
+++ scripts/ci_install_dependencies.sh
@@ -9,7 +9,7 @@ echo "Installing additional Python dependencies..."
 # Upgrade pip first
 python3 -m pip install --upgrade pip
 
-# Example Python packages that rosdep doesn't handle:
+# Note: These are required by velocity_controller_lqr
 python3 -m pip install control==0.10.1
 python3 -m pip install "numpy<1.25.0"
 
