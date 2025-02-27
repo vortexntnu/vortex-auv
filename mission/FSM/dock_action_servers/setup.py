241d49d27a88947ac80d7ac6836b8faa1efc2317
Vetlets05 <vetlets@icloud.com>
2025-02-27 19:23:19 +0100

    refactor: remove unused test folder

--- mission/FSM/dock_action_servers/setup.py
+++ mission/FSM/dock_action_servers/setup.py
@@ -5,7 +5,7 @@ package_name = 'dock_action_servers'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
