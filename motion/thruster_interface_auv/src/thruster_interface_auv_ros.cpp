4c72331c7b07a1d367f1356c4345ae86207d8ee5
Anders Høgden <andeshog@stud.ntnu.no>
2025-03-01 20:26:54 +0100

    refactor: replace raw loop with algorithm and remove unncecessary comments

--- motion/thruster_interface_auv/src/thruster_interface_auv_ros.cpp
+++ motion/thruster_interface_auv/src/thruster_interface_auv_ros.cpp
@@ -2,10 +2,9 @@
 
 ThrusterInterfaceAUVNode::ThrusterInterfaceAUVNode()
     : Node("thruster_interface_auv_node") {
-    // extract all .yaml parameters
+   
     this->extract_all_parameters();
 
-    // Set up subscriber and publisher
     rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
     auto qos_sensor_data = rclcpp::QoS(
         rclcpp::QoSInitialization(qos_profile.history, 1), qos_profile);
@@ -18,14 +17,11 @@ ThrusterInterfaceAUVNode::ThrusterInterfaceAUVNode()
 
     thruster_pwm_publisher_ =
         this->create_publisher<std_msgs::msg::Int16MultiArray>(
-            publisher_topic_name_, qos_sensor_data);
+            publisher_topic_name_, 1);
 
-    // call constructor for thruster_driver_
     thruster_driver_ = std::make_unique<ThrusterInterfaceAUVDriver>(
         i2c_bus_, i2c_address_, thruster_parameters_, poly_coeffs_);
 
-    // Declare thruster_forces_array_ in case no topic comes at the
-    // very beginning
     thruster_forces_array_ = std::vector<double>(8, 0.00);
 
     this->initialize_parameter_handler();
@@ -41,11 +37,9 @@ void ThrusterInterfaceAUVNode::thruster_forces_callback(
 }
 
 void ThrusterInterfaceAUVNode::pwm_callback() {
-    // drive thrusters...
     std::vector<uint16_t> thruster_pwm_array =
         thruster_driver_->drive_thrusters(this->thruster_forces_array_);
 
-    //..and publish PWM values for debugging purposes
     if (debug_flag_) {
         std_msgs::msg::Int16MultiArray pwm_message;
         pwm_message.data = std::vector<int16_t>(thruster_pwm_array.begin(),
@@ -57,7 +51,6 @@ void ThrusterInterfaceAUVNode::pwm_callback() {
 void ThrusterInterfaceAUVNode::initialize_parameter_handler() {
     param_handler_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
 
-    // Register the parameter event callback directly in the lambda expression
     debug_flag_parameter_cb = param_handler_->add_parameter_callback(
         "debug.flag", std::bind(&ThrusterInterfaceAUVNode::update_debug_flag,
                                 this, std::placeholders::_1));
@@ -71,7 +64,6 @@ void ThrusterInterfaceAUVNode::update_debug_flag(const rclcpp::Parameter& p) {
 }
 
 void ThrusterInterfaceAUVNode::extract_all_parameters() {
-    // thruster parameters from orca.yaml
     this->declare_parameter<std::vector<int>>(
         "propulsion.thrusters.thruster_to_pin_mapping");
     this->declare_parameter<std::vector<int>>(
@@ -85,18 +77,16 @@ void ThrusterInterfaceAUVNode::extract_all_parameters() {
     this->declare_parameter<std::vector<double>>("coeffs.16V.LEFT");
     this->declare_parameter<std::vector<double>>("coeffs.16V.RIGHT");
 
-    // i2c parameters
     this->declare_parameter<int>("i2c.bus");
     this->declare_parameter<int>("i2c.address");
 
-    // topics
     this->declare_parameter<std::string>("topics.thruster_forces");
     this->declare_parameter<std::string>("topics.pwm_output");
 
     this->declare_parameter<bool>("debug.flag");
 
     //-----------------------------------------------------------------------
-    // get them
+
     auto thruster_mapping =
         this->get_parameter("propulsion.thrusters.thruster_to_pin_mapping")
             .as_integer_array();
@@ -124,17 +114,21 @@ void ThrusterInterfaceAUVNode::extract_all_parameters() {
         this->get_parameter("topics.pwm_output").as_string();
 
     this->debug_flag_ = this->get_parameter("debug.flag").as_bool();
-
-    // create <ThrusterParameters> and <PolyCoeffs> vectors
-    ThrusterParameters temp;
-    for (size_t i = 0; i < thruster_mapping.size(); ++i) {
-        temp.mapping = static_cast<uint8_t>(thruster_mapping[i]);
-        temp.direction = static_cast<int8_t>(thruster_direction[i]);
-        temp.pwm_min = static_cast<uint16_t>(thruster_PWM_min[i]);
-        temp.pwm_max = static_cast<uint16_t>(thruster_PWM_max[i]);
-
-        this->thruster_parameters_.push_back(temp);
-    }
+    
+    std::transform(
+        thruster_mapping.begin(), thruster_mapping.end(),
+        thruster_direction.begin(),
+        std::back_inserter(this->thruster_parameters_),
+        [&](const int64_t& mapping, const int64_t& direction) {
+            size_t index = &mapping - &thruster_mapping[0];
+            return ThrusterParameters{
+                static_cast<uint8_t>(mapping),
+                static_cast<int8_t>(direction),
+                static_cast<uint16_t>(thruster_PWM_min[index]),
+                static_cast<uint16_t>(thruster_PWM_max[index])
+            };
+        }
+    );
 
     this->poly_coeffs_.push_back(left_coeffs);
     this->poly_coeffs_.push_back(right_coeffs);
