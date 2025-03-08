f4290fc7af689d972d8a773c5b9054b190b2195a
Anders Høgden <157955984+Andeshog@users.noreply.github.com>
2025-03-08 16:55:14 +0100

    Make container multithreaded

--- auv_setup/launch/orca.launch.py
+++ auv_setup/launch/orca.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description() -> LaunchDescription:
         name="thruster_container",
         namespace="orca",
         package="rclcpp_components",
-        executable="component_container",
+        executable="component_container_mt",
         composable_node_descriptions=[
             ComposableNode(
                 package="thrust_allocator_auv",
