e56f4d8ccd768a61ab7b2fbee36f12ab91e229dd
Thomas <thomapau@stud.ntnu.no>
2025-03-09 17:52:22 +0100

    minor changes

--- acoustics/acoustics_interface/launch/acoustics_interface_launch.py
+++ acoustics/acoustics_interface/launch/acoustics_interface_launch.py
@@ -9,9 +9,9 @@ from launch import LaunchDescription
 def generate_launch_description():
     # Path to the YAML file
     yaml_file_path = os.path.join(
-        get_package_share_directory("blackbox"),
-        "../../../../",  # Go to the workspace
-        "src/vortex-auv/auv_setup/config/robots/",  # Go inside where yamal files are located at
+        get_package_share_directory("auv_setup"),
+        "config",
+        "robots",
         'orca.yaml',
     )
 
