dfa24c0be9892b959744182764eb4ed175e01e91
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2025-03-14 23:31:19 +0100

    install gcc13 in ci_install_dependencies.sh

--- scripts/ci_install_dependencies.sh
+++ scripts/ci_install_dependencies.sh
@@ -13,4 +13,9 @@ python3 -m pip install --upgrade pip
 python3 -m pip install control==0.10.1
 python3 -m pip install "numpy<1.25.0"
 
+apt update
+apt install -y gcc-13 g++-13
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
+
 echo "Done installing additional dependencies."
