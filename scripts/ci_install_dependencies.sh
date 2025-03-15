89271809cee6100df670beda1de01c8fb876cc44
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-03-15 14:26:59 +0100

    test2

--- scripts/ci_install_dependencies.sh
+++ scripts/ci_install_dependencies.sh
@@ -13,9 +13,16 @@ python3 -m pip install --upgrade pip
 python3 -m pip install control==0.10.1
 python3 -m pip install "numpy<1.25.0"
 
-apt update
-apt install -y gcc-13 g++-13
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
+# Note: These are required by vortex-vkf
+# Add Ubuntu Toolchain PPA to get gcc-13/g++-13
+sudo apt-get update -qq
+sudo apt-get install -y --no-install-recommends software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+
+# Install and switch to GCC 13
+sudo apt-get install -y --no-install-recommends gcc-13 g++-13
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
 
 echo "Done installing additional dependencies."
