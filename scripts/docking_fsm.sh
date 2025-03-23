9695174a5fe8868402fb65e7bd174126f68c8a1f
Hako2807 <hakonts@icloud.com>
2025-03-23 15:36:30 +0100

    feat: add ekf_pose_filtering to docking_fsm script

--- scripts/docking_fsm.sh
+++ scripts/docking_fsm.sh
@@ -5,10 +5,12 @@ tmux new-session -d -s "docking-fsm"
 tmux split-window -v -t "docking-fsm:0"
 tmux split-window -h -t "docking-fsm:0.0"
 tmux split-window -h -t "docking-fsm:0.2"
+tmux split-window -h -t "docking-fsm:0.3"
 
 tmux send-keys -t "docking-fsm:0.0" "ros2 launch dock_action_servers docking_action_servers.launch.py" C-m
 tmux send-keys -t "docking-fsm:0.1" "ros2 launch auv_setup dp.launch.py " C-m
 tmux send-keys -t "docking-fsm:0.2" "ros2 launch pose_action_server pose_action_server.launch.py" C-m
-tmux send-keys -t "docking-fsm:0.3" "ros2 launch stonefish_sim simulation_nogpu.launch.py" C-m
+tmux send-keys -t "docking-fsm:0.3" "ros2 launch ekf_pose_filtering ekf_pose_filtering.launch.py" C-m
+tmux send-keys -t "docking-fsm:0.4" "ros2 launch stonefish_sim simulation_nogpu.launch.py" C-m
 
 tmux attach-session -t "docking-fsm"
