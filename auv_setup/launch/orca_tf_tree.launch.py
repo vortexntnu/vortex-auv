88d964de2269f30cf0a4570fd7cb64eae498a685
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2025-03-30 21:51:46 +0200

    urdf and state publisher

---
+++ auv_setup/launch/orca_tf_tree.launch.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Path to your xacro file
+    xacro_file = os.path.join(
+        get_package_share_directory('auv_setup'), 'config', 'urdf', 'orca.urdf.xacro'
+    )
+
+    # Run xacro to convert to URDF
+    robot_description = subprocess.check_output(['xacro', xacro_file]).decode()
+
+    return LaunchDescription(
+        [
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[{'robot_description': robot_description}],
+            )
+        ]
+    )
