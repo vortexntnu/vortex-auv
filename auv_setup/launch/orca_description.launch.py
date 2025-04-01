4f70711067fbd118f879ff485da2d289037f15ff
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2025-04-01 16:48:12 +0200

    added flir cond launch arg

--- auv_setup/launch/orca_description.launch.py
+++ auv_setup/launch/orca_description.launch.py
@@ -1,28 +1,49 @@
 import os
-import subprocess
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Path to your xacro file
-    xacro_file = os.path.join(
+    flir_camera_arg = DeclareLaunchArgument(
+        'use_flir_camera',
+        default_value='true',
+        description='Whether to include the FLIR camera in the robot description',
+    )
+
+    use_flir_camera = LaunchConfiguration('use_flir_camera')
+
+    xacro_path = os.path.join(
         get_package_share_directory('auv_setup'), 'description', 'orca.urdf.xacro'
     )
 
-    # Run xacro to convert to URDF
-    robot_description = subprocess.check_output(['xacro', xacro_file]).decode()
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {
+                'robot_description': Command(
+                    [
+                        'xacro',
+                        ' ',
+                        str(xacro_path),
+                        ' ',
+                        'use_flir_camera:=',
+                        use_flir_camera,
+                    ]
+                )
+            }
+        ],
+    )
 
     return LaunchDescription(
         [
-            Node(
-                package='robot_state_publisher',
-                executable='robot_state_publisher',
-                name='robot_state_publisher',
-                output='screen',
-                parameters=[{'robot_description': robot_description}],
-            )
+            flir_camera_arg,
+            robot_state_publisher_node,
         ]
     )
