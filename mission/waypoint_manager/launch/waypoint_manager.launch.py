e476b5247ede83e0bcd6ca0b93a881687ecd0fbd
abubakaraliyubadawi <abubakb@stud.ntnu.no>
2025-04-07 22:25:27 +0200

    modified launch file and added config to orca.yaml

--- mission/waypoint_manager/launch/waypoint_manager.launch.py
+++ mission/waypoint_manager/launch/waypoint_manager.launch.py
@@ -3,31 +3,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     waypoint_manager_pkg_dir = get_package_share_directory('waypoint_manager')
-    reference_filter_pkg_dir = get_package_share_directory('reference_filter_dp')
-
-    reference_filter_config = os.path.join(
-        reference_filter_pkg_dir, 'config', 'reference_filter_params.yaml'
-    )
 
     waypoint_manager_config = os.path.join(
-        waypoint_manager_pkg_dir, 'config', 'waypoint_manager_params.yaml'
-    )
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-
-    reference_filter_node = Node(
-        package='reference_filter_dp',
-        executable='reference_filter_dp_node',
-        name='reference_filter_node',
-        namespace='orca',
-        parameters=[reference_filter_config, {'use_sim_time': use_sim_time}],
-        output='screen',
+        waypoint_manager_pkg_dir, 'config', 'orca.yaml'
     )
 
     waypoint_manager_node = Node(
@@ -35,7 +18,7 @@ def generate_launch_description():
         executable='waypoint_manager_node',
         name='waypoint_manager_node',
         namespace='orca',
-        parameters=[waypoint_manager_config, {'use_sim_time': use_sim_time}],
+        parameters=[waypoint_manager_config],
         output='screen',
     )
 
@@ -46,7 +29,6 @@ def generate_launch_description():
                 default_value='false',
                 description='Use simulation (Gazebo) clock if true',
             ),
-            reference_filter_node,
             waypoint_manager_node,
         ]
     )
