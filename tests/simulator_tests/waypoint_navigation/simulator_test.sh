1dd2d5515b4a96b14ebecc521449c0fc81af0a68
Anders Høgden <anders.hogden@zeabuz.com>
2025-09-30 13:49:34 +0200

    temporarily use log directory to store bag

--- tests/simulator_tests/waypoint_navigation/simulator_test.sh
+++ tests/simulator_tests/waypoint_navigation/simulator_test.sh
@@ -19,7 +19,8 @@ cleanup() {
 }
 trap cleanup ERR
 
-setsid ros2 bag record -o ${WORKSPACE}/bags/recording -s mcap -a &
+# setsid ros2 bag record -o ${WORKSPACE}/bags/recording -s mcap -a &
+setsid ros2 bag record -o ${WORKSPACE}/log/recording -s mcap -a &
 BAG_PID=$!
 echo "Started bagging with PID: $BAG_PID"
 
