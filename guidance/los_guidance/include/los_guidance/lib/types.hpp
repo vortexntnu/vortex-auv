3bb892627bd89d847961fe2da87048f7a998d616
Anbit <anbitadhi123'gmail.com>
2025-11-02 12:36:03 +0100

    fix: fix ignore lib

---
+++ guidance/los_guidance/include/los_guidance/lib/types.hpp
@@ -0,0 +1,56 @@
+#ifndef types_hpp
+#define types_hpp
+
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
+#include <cmath>
+
+namespace vortex::guidance::lod::types{
+
+    namespace LOS {
+
+        struct Point {
+            double x{};
+            double y{};
+            double z{};
+
+            Point operator-(const Point& other) const {
+                return Point{x - other.x, y - other.y, z - other.z};
+            }
+
+            Eigen::Vector3d as_vector() const { return Eigen::Vector3d(x, y, z); }
+        };
+
+        struct CrossTrackError {
+            double x_e{};
+            double y_e{};
+            double z_e{};
+
+            inline static CrossTrackError from_vector(const Eigen::Vector3d& vector) {
+                return CrossTrackError{vector.x(), vector.y(), vector.z()};
+            }
+        };
+
+       struct Output {
+            double psi_d{};
+            double theta_d{};
+        };
+
+        struct Inputs{
+            Point prev_point{};
+            Point next_point{};
+            Point current_position{};
+        };
+
+        enum class Active_LOSMethod {
+            PROPORTIONAL,
+            INTEGRAL,
+            ADAPTIVE
+        };
+        
+
+    } // namespace LOS
+
+} // namespace vortex::guidance::los::types
+
+#endif
