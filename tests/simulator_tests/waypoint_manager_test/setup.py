b82db78d8c58f8233500c02e8139c2fc1bc7ebc8
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2025-12-06 14:26:00 +0100

    first waypoint test

--- tests/simulator_tests/waypoint_manager_test/setup.py
+++ tests/simulator_tests/waypoint_manager_test/setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 package_name = 'waypoint_manager_test'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'test', '*.tests', '*.test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
--- tests/simulator_tests/waypoint_manager_test/test/test_single_waypoint.py
+++ tests/simulator_tests/waypoint_manager_test/test/test_single_waypoint.py
@@ -1,19 +1,20 @@
-from waypoint_manager_test.test_common.launch_setup import generate_wm_test_description
 from waypoint_manager_test.test_common.base_test import WaypointManagerTestBase
-from waypoint_manager_test.test_common.utilities import make_waypoint
+from waypoint_manager_test.test_common.launch_description import (
+    generate_wm_test_description,
+)
+from waypoint_manager_test.test_common.utils import make_waypoint
 
-import launch_testing
 
 def generate_test_description():
     return generate_wm_test_description()
 
-class TestSingleWaypoint(WaypointManagerTestBase):
 
+class TestSingleWaypoint(WaypointManagerTestBase):
     def test_single_wp(self):
         wp = make_waypoint(2.0, 0.0, 1.0)
 
         _, result = self.send_goal([wp], persistent=False, conv=0.3)
 
-        self.assertTrue(result.success)
-        self.assertTrue(result.pose_valid)
+        assert result.success
+        assert result.pose_valid
         self.assert_pose_close(result.final_pose, wp.pose, tol=0.6)
