ea4fcbf64038886749590925d3a2c7203dff56b0
eirisak <eirisak@stud.ntnu.no>
2026-01-08 15:34:19 +0100

    deleted operation mode python files

--- mission/operation_mode_manager/operation_mode_manager/mode_manager_utils.py
+++
@@ -1,3 +0,0 @@
-from vortex_msgs.srv import OperationModeSRV
-
-modes = {OperationModeSRV.Request.KILLSWITCH: "KILLSWITCH", OperationModeSRV.Request.AUTONOMOUS: "AUTONOMOUS", OperationModeSRV.Request.MANUAL: "MANUAL"}
