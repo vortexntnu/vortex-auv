7ce5d04bde7a7893a08b3aab1c25d0ae398c2934
eirisak <eirisak@stud.ntnu.no>
2026-02-11 17:51:00 +0100

    Fixed operation mode manager main

--- mission/operation_mode_manager/src/operation_mode_manager.cpp
+++ mission/operation_mode_manager/src/operation_mode_manager.cpp
@@ -165,7 +165,7 @@ void OperationModeManager::publish_mode() {
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<OperationModeManager>(rclcpp::NodeOptions()));
+    rclcpp::spin(std::make_shared<vortex::mission::OperationModeManager>(rclcpp::NodeOptions()));
     rclcpp::shutdown();
     return 0;
 }
