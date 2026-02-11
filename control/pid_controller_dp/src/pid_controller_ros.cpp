397e5b38cd911ee184320f6c44bdae9e1478d2df
eirisak <eirisak@stud.ntnu.no>
2026-02-11 18:16:00 +0100

    Updated pid_controller version

--- control/pid_controller_dp/src/pid_controller_ros.cpp
+++ control/pid_controller_dp/src/pid_controller_ros.cpp
@@ -4,6 +4,7 @@
 #include "pid_controller_dp/pid_controller_conversions.hpp"
 #include "pid_controller_dp/pid_controller_utils.hpp"
 #include "pid_controller_dp/typedefs.hpp"
+#include <vortex/utils/ros/ros_conversions.hpp>
 
 PIDControllerNode::PIDControllerNode() : Node("pid_controller_node") {
     time_step_ = std::chrono::milliseconds(10);
