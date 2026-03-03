6266b7833779707ea6b485b4777a4f09acfdb967
Sina <aanstadsina@gmail.com>
2026-03-03 21:43:20 +0100

    created publisher, subscribed, fixed transform logic

--- mission/tacc/docking_task/pool_exploration/include/pool_exploration/pool_exploration_ros.hpp
+++ mission/tacc/docking_task/pool_exploration/include/pool_exploration/pool_exploration_ros.hpp
@@ -1,36 +1,72 @@
 #ifndef POOL_EXPLORATION_ROS_HPP
 #define POOL_EXPLORATION_ROS_HPP
 
+#include <tf2_ros/buffer.hpp>
+#include <message_filters/subscriber.h>
+#include <tf2_ros/message_filter.h>
+
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <pool_exploration/pool_exploration.hpp>
+#include <rclcpp/timer.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <tf2_ros/static_transform_broadcaster.hpp>
 #include <tf2_eigen/tf2_eigen.hpp>
+#include <vortex_msgs/msg/line_segment3_d_array.hpp>
 
-
+#include <rmw/types.h>
+#include <tf2_ros/create_timer_ros.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
 namespace vortex::pool_exploration{
 
 class PoolExplorationNode : public rclcpp::Node {
 public:
-    PoolExplorationNode(const rclcpp::NodeOptions& options);
-    ~PoolExplorationNode() {};
-
-    geometry_msgs::msg::TransformStamped compute_map_odom_transform();
+    explicit PoolExplorationNode(const rclcpp::NodeOptions& options);
+    ~PoolExplorationNode() = default;
 
 private:
-    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster_;
-    
+
+    // setup
+    void setup_publishers_and_subscribers();
+
+    // TF map->odom
+    geometry_msgs::msg::TransformStamped compute_map_odom_transform();
+
+    // callbacks
+    void line_callback(const vortex_msgs::msg::LineSegment3DArray::ConstSharedPtr& msg);
+    void timer_callback();
+
+    // helpers
+    static std::vector<LineSegment> toMapSegments2D( //static?
+        const vortex_msgs::msg::LineSegment3DArray& msg,
+        const Eigen::Matrix4f& T_map_src); 
+
     void publish_grid();
 
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
-
-    PoolExplorationMap map_;
-
+    // frames
     std::string odom_frame_;
     std::string map_frame_;
+    std::chrono::milliseconds pub_dt_{200};
 
-    Eigen::Matrix4f map_to_odom_tf_; //hva brukes denne til 
+    // TF publishing
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster_;
+
+    // TF listening
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer_; //Hvorfor bruke buffer
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
+    // subscriber (HVEM AV DISSE?? Bruke filter??)
+    //rclcpp::Subscription<vortex_msgs::msg::LineSegment3DArray>::SharedPtr line_sub_;
+    message_filters::Subscriber<vortex_msgs::msg::LineSegment3DArray> line_sub_;
+    std::shared_ptr<tf2_ros::MessageFilter<vortex_msgs::msg::LineSegment3DArray>> line_filter_;
+
+    // publisher
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // map
+    PoolExplorationMap map_;
 
 };
 
--- mission/tacc/docking_task/pool_exploration/src/pool_exploration_ros.cpp
+++ mission/tacc/docking_task/pool_exploration/src/pool_exploration_ros.cpp
@@ -1,70 +1,98 @@
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+// #include <nav_msgs/msg/detail/occupancy_grid__struct.hpp>
+#include <vortex_msgs/msg/line_segment3_d_array.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/create_timer_ros.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <spdlog/spdlog.h>
+
+// #include <rmw/types.h>
+#include <chrono>
+#include <memory>
 #include <pool_exploration/pool_exploration_ros.hpp>
+
+#include <rclcpp/qos.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
+#include <tf2/exceptions.hpp>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_ros/buffer.hpp>
+
 
 namespace vortex::pool_exploration{
 
 PoolExplorationNode::PoolExplorationNode(const rclcpp::NodeOptions& options)
-    : Node("pool_exploration_node", options), map_(
+    : Node("pool_exploration_node", options), 
+    map_(
           this->declare_parameter<double>("size_x", 10.0),
           this->declare_parameter<double>("size_y", 10.0),
           this->declare_parameter<double>("resolution", 0.1),
-          this->declare_parameter<std::string>("frame_id", "map")
-      ) 
-    {
+          this->declare_parameter<std::string>("frame_id", "map")) {
+            setup_publishers_and_subscribers();
+} 
 
-    this->declare_parameter<bool>("enu_to_ned", false); //når bestemmes denne verdien?? skal være false??
+void PoolExplorationNode::setup_publishers_and_subscribers() {
+    map_frame_ = this->declare_parameter<std::string>("map_frame", "map");
+    odom_frame_ = this->declare_parameter<std::string>("odom_frame", "odom");
 
+    pub_dt_ = std::chrono::milliseconds(
+        this->declare_parameter<int>("publish_rate_ms", 200));
 
-    odom_frame_ =
-        this->declare_parameter<std::string>("odom_frame", "odom");
+    this->declare_parameter<bool>("enu_to_ned", false); // skal ha med??
 
-    map_frame_ =
-        this->declare_parameter<std::string>("map_frame", "map");
-    //forward_camera_frame
-    //downward_camera_frame
-    //sonar_frame
-    tf_broadcaster_ =
-        std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    const std::string line_sub_topic =
+        this->declare_parameter<std::string>("line_sub_topic", "/filtered_lines");
+    const std::string map_pub_topic =
+        this->declare_parameter<std::string>("map_pub_topic", "/map");
 
-    auto map_to_odom_tf = compute_map_odom_transform();
-
+    // TF broadcaster
+    tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    const auto map_to_odom_tf = compute_map_odom_transform(); //const?
     tf_broadcaster_->sendTransform(map_to_odom_tf);
 
-    Eigen::Affine3f eigen_transform = tf2::transformToEigen(map_to_odom_tf.transform).cast<float>();
+    //legge til spdlog??
 
-    map_to_odom_tf_ = eigen_transform.matrix();
+    // TF buffer/listener (HVA GJØR DENNE??)
+    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+    auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
+        this->get_node_base_interface(), this->get_node_timers_interface());
+    tf_buffer_->setCreateTimerInterface(timer_interface);
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-    double size_x = this->declare_parameter<double>("size_x", 10.0);
-    double size_y = this->declare_parameter<double>("size_y", 10.0);
-    double resolution = this->declare_parameter<double>("resolution", 0.1);
-    std::string frame_id = this->declare_parameter<std::string>("frame_id", "map");
-    
-    map_ = PoolExplorationMap(size_x, size_y, resolution, frame_id);
+    const auto qos_sub = rclcpp::SensorDataQoS();
+    // BYTTA UT MED
+    //const auto qos_pub = rclcpp::QoS(1).best_effort().durability_volatile();
+    auto map_qos = rclcpp::QoS(rclcpp::KeepLast(1))
+        .reliable()
+        .transient_local();
 
-    map_pub_ = create_publisher<nav_msgs::msg::OccupancyGrid>("map", 10);
+    // subscriber (HAR BYTTA UT?)
+    // line_sub_ = this->create_subscription<vortex_msgs::msg::LineSegment3DArray>(
+    //     line_sub_topic, qos_sub,
+    //    std::bind(&PoolExplorationNode::line_callback, this, std::placeholders::_1));
+    auto sub_options = rclcpp::SubscriptionOptions();
 
-    //må subscribe på linjene
-    //line_sub_
-
-    /*
-    odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        odom_sub_topic, 
-        qos, 
-        std::bind(&MappingInterfaceNode::odometry_callback, this, _1),
+    line_sub_.subscribe(
+        this,
+        line_sub_topic,
+        qos_sub.get_rmw_qos_profile(),
         sub_options);
-    */
 
-    publish_grid(); //må endre hvor ofte publiseres, evt hele funksjonen
-}
+    // MessageFilter: slipper bare gjennom meldinger når TF til map_frame_ finnes
+    line_filter_ = std::make_shared<tf2_ros::MessageFilter<vortex_msgs::msg::LineSegment3DArray>>(
+        line_sub_, *tf_buffer_, map_frame_, 10,  // queue size
+        this->get_node_logging_interface(),
+        this->get_node_clock_interface());
 
+    line_filter_->registerCallback(
+        std::bind(&PoolExplorationNode::line_callback, this, std::placeholders::_1));
 
-void PoolExplorationNode::publish_grid()
-{
-    auto grid_msg = map_.grid(); //const reference
-    nav_msgs::msg::OccupancyGrid msg_to_publish = grid_msg;  // make copy because const reference 
-    msg_to_publish.header.stamp = this->get_clock()->now();
-    map_pub_->publish(msg_to_publish);
+    // publisher
+    //map_pub_ = create_publisher<nav_msgs::msg::OccupancyGrid>(map_pub_topic, qos_pub); 
+    map_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(map_pub_topic, map_qos);
 
+    //timer 
+    timer_ = this->create_wall_timer(pub_dt_, [this]() { this->timer_callback(); });
 }
 
 geometry_msgs::msg::TransformStamped PoolExplorationNode::compute_map_odom_transform() {
@@ -73,6 +101,7 @@ geometry_msgs::msg::TransformStamped PoolExplorationNode::compute_map_odom_trans
     map_to_odom.header.frame_id = map_frame_;
     map_to_odom.child_frame_id = odom_frame_;
 
+    // dette blir i midten av kartet
     map_to_odom.transform.translation.x = 0.0;
     map_to_odom.transform.translation.y = 0.0;
     map_to_odom.transform.translation.z = 0.0;
@@ -96,6 +125,79 @@ geometry_msgs::msg::TransformStamped PoolExplorationNode::compute_map_odom_trans
 
     return map_to_odom;
 }
+
+
+//FUNSKJON SOM TRANSFORMERER msg TIL LineSegmentene (Må dobbeltsjekke) 
+std::vector<LineSegment> PoolExplorationNode::toMapSegments2D(
+    const vortex_msgs::msg::LineSegment3DArray& msg,
+    const Eigen::Matrix4f& T_map_src)
+{
+    //const Eigen::Matrix4f odom_to_map = map_to_odom_tf.inverse(); //Skal ikke inverse likevel, håndteres av lookupTransform
+    
+    //src er odom i dette tilfellet
+    std::vector<LineSegment> segVector;
+    segVector.reserve(msg.lines.size());
+
+    for (const auto& line : msg.lines) {
+        Eigen::Vector4f p0_src(line.p0.x, line.p0.y, line.p0.z, 1.0f);
+        Eigen::Vector4f p1_src(line.p1.x, line.p1.y, line.p1.z, 1.0f);
+
+        Eigen::Vector4f p0_map = T_map_src * p0_src;
+        Eigen::Vector4f p1_map = T_map_src * p1_src;
+
+        LineSegment seg;
+        seg.p0 = {p0_map.x(), p0_map.y()};
+        seg.p1 = {p1_map.x(), p1_map.y()};
+        segVector.push_back(seg);
+    }
+    return segVector;
+}
+
+
+void PoolExplorationNode::line_callback(
+    const vortex_msgs::msg::LineSegment3DArray::ConstSharedPtr& msg) {
+
+    geometry_msgs::msg::TransformStamped tf_stamped;
+    try {
+        tf_stamped = tf_buffer_->lookupTransform(
+            map_frame_, msg->header.frame_id, msg->header.stamp);
+    } catch (const tf2::TransformException& ex) {
+        spdlog::warn("[PoolExploration] TF failed {} -> {}: {}",
+                    msg->header.frame_id, map_frame_, ex.what());
+        return;
+    }
+
+    const Eigen::Affine3d T = tf2::transformToEigen(tf_stamped.transform);
+    const Eigen::Matrix4f T_map_src = T.matrix().cast<float>();
+
+    //se nærmere på denne transform logikken
+    auto segs = toMapSegments2D(*msg, T_map_src);
+    // oppdaterer griddet direkte
+    map_.insertSegmentsMapFrame(segs);
+
+   // publish_grid();
+}
+
+void PoolExplorationNode::timer_callback() {
+    publish_grid();
+}
+
+void PoolExplorationNode::publish_grid() {
+    nav_msgs::msg::OccupancyGrid msg = map_.grid(); 
+
+    msg.header.frame_id = map_frame_;
+    msg.header.stamp = this->get_clock()->now();
+
+    map_pub_->publish(msg);
+}
+
+/*
+
+    map_to_odom_tf_ = eigen_transform.matrix();
+    map_ = PoolExplorationMap(size_x, size_y, resolution, frame_id);
+
+ */
+
 
 RCLCPP_COMPONENTS_REGISTER_NODE(PoolExplorationNode)
 
