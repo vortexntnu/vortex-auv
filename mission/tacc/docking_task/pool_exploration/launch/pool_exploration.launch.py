520d8f24671cad9644c000dce0b377dd04d87575
Sina <aanstadsina@gmail.com>
2026-03-05 12:45:40 +0100

    added launch and config files

--- mission/tacc/docking_task/pool_exploration/launch/pool_exploration.launch.py
+++ mission/tacc/docking_task/pool_exploration/launch/pool_exploration.launch.py
@@ -1,8 +1,17 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("pool_exploration"),
+        "config",
+        "pool_exploration.yaml",
+    )
+
     return LaunchDescription(
         [
             Node(
@@ -10,7 +19,10 @@ def generate_launch_description():
                 executable='pool_exploration_node',
                 name='pool_exploration',
                 output='screen',
-                parameters=[{'use_sim_time': False}],
+                parameters=[
+                    config,
+                    {'use_sim_time': True},
+                ],
             ),
         ]
     )
