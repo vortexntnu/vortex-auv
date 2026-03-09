234d3c7dd297edc238e1cd6631b63482653ba5d4
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2026-03-09 19:41:10 +0100

    made launch files drone agnostic

--- auv_setup/launch/drone_description.launch.py
+++ auv_setup/launch/drone_description.launch.py
@@ -2,87 +2,50 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
-def generate_launch_description():
-    drone_name_arg = DeclareLaunchArgument(
-        'drone',
-        default_value='drone',
-        description='Name of the drone (used as frame prefix)',
-    )
-
-    flir_camera_arg = DeclareLaunchArgument(
-        'include_flir',
-        default_value='true',
-        description='Whether to include the FLIR camera in the robot description',
-    )
-
-    gripper_arg = DeclareLaunchArgument(
-        'include_gripper',
-        default_value='false',
-        description='Whether to include the gripper in the robot description',
-    )
-
-    static_shoulder_arg = DeclareLaunchArgument(
-        'static_shoulder',
-        default_value='true',
-        description='Whether the gripper shoulder joint is fixed (true) or revolute (false)',
-    )
-
-    drone_name = LaunchConfiguration('drone')
-
-    include_flir = LaunchConfiguration('include_flir')
-
-    include_gripper = LaunchConfiguration('include_gripper')
-
-    static_shoulder = LaunchConfiguration('static_shoulder')
+def launch_setup(context, *args, **kwargs):
+    drone_name = LaunchConfiguration("drone").perform(context)
 
     xacro_path = os.path.join(
-        get_package_share_directory('auv_setup'), 'description', 'drone.urdf.xacro'
+        get_package_share_directory("auv_setup"),
+        "description",
+        f"{drone_name}.urdf.xacro",
     )
 
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
         parameters=[
             {
-                'robot_description': ParameterValue(
-                    Command(
-                        [
-                            'xacro',
-                            ' ',
-                            str(xacro_path),
-                            ' ',
-                            'include_flir:=',
-                            include_flir,
-                            ' ',
-                            'include_gripper:=',
-                            include_gripper,
-                            ' ',
-                            'static_shoulder:=',
-                            static_shoulder,
-                            ' ',
-                        ]
-                    ),
+                "robot_description": ParameterValue(
+                    Command(["xacro", " ", xacro_path]),
                     value_type=str,
                 ),
-                'frame_prefix': [drone_name, '/'],
+                "frame_prefix": f"{drone_name}/",
             }
         ],
+    )
+
+    return [robot_state_publisher_node]
+
+
+def generate_launch_description():
+    drone_name_arg = DeclareLaunchArgument(
+        "drone",
+        default_value="orca",
+        description="Name of the drone (used as frame prefix)",
     )
 
     return LaunchDescription(
         [
             drone_name_arg,
-            flir_camera_arg,
-            gripper_arg,
-            static_shoulder_arg,
-            robot_state_publisher_node,
+            OpaqueFunction(function=launch_setup),
         ]
     )
