bfcc41356753359582c7a37c8e0f7b86c86eca75
CurryMonsters <hinthu5@hotmail.no>
2026-03-11 17:27:48 +0100

    fix

--- mission/landmark_server/launch/landmark_server.launch.py
+++ mission/landmark_server/launch/landmark_server.launch.py
@@ -2,35 +2,50 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import OpaqueFunction
 from launch_ros.actions import Node
 
-landmark_config = os.path.join(
-    get_package_share_directory('landmark_server'),
-    'config',
-    'landmark_server_config.yaml',
+from auv_setup.launch_arg_common import (
+    declare_drone_and_namespace_args,
+    resolve_drone_and_namespace,
 )
 
-orca_config = os.path.join(
-    get_package_share_directory('auv_setup'),
-    'config',
-    'robots',
-    'orca.yaml',
-)
+
+def launch_setup(context, *args, **kwargs):
+    drone, namespace = resolve_drone_and_namespace(context)
+
+    landmark_config = os.path.join(
+        get_package_share_directory("landmark_server"),
+        "config",
+        "landmark_server_config.yaml",
+    )
+
+    drone_params = os.path.join(
+        get_package_share_directory("auv_setup"),
+        "config",
+        "robots",
+        f"{drone}.yaml",
+    )
+
+    return [
+        Node(
+            package="landmark_server",
+            executable="landmark_server_node",
+            name="landmark_server_node",
+            namespace=namespace,
+            parameters=[
+                landmark_config,
+                drone_params,
+                {
+                    "use_sim_time": False,
+                },  # If testing with rosbags sim_time might be preferred if bag is looped
+            ],
+            output="screen",
+        )
+    ]
 
 
 def generate_launch_description():
-    landmark_node = Node(
-        package='landmark_server',
-        executable='landmark_server_node',
-        name='landmark_server_node',
-        namespace='orca',
-        parameters=[
-            landmark_config,
-            orca_config,
-            {
-                'use_sim_time': False
-            },  # If testing with rosbags sim_time might be preferred if bag is looped
-        ],
-        output='screen',
+    return LaunchDescription(
+        declare_drone_and_namespace_args() + [OpaqueFunction(function=launch_setup)]
     )
-    return LaunchDescription([landmark_node])
