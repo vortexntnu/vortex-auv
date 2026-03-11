fdc314de3637ca07cdc7a35cfc1bab8809c9cb4d
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2026-03-11 18:45:40 +0100

    use drone, namespace helper in description launch

--- auv_setup/launch/drone_description.launch.py
+++ auv_setup/launch/drone_description.launch.py
@@ -2,50 +2,50 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import OpaqueFunction
+from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
+from auv_setup.launch_arg_common import (
+    declare_drone_and_namespace_args,
+    resolve_drone_and_namespace,
+)
+
 
 def launch_setup(context, *args, **kwargs):
-    drone_name = LaunchConfiguration("drone").perform(context)
+    drone, namespace = resolve_drone_and_namespace(context)
 
     xacro_path = os.path.join(
         get_package_share_directory("auv_setup"),
         "description",
-        f"{drone_name}.urdf.xacro",
+        f"{drone}.urdf.xacro",
     )
 
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="screen",
-        parameters=[
-            {
-                "robot_description": ParameterValue(
-                    Command(["xacro", " ", xacro_path]),
-                    value_type=str,
-                ),
-                "frame_prefix": f"{drone_name}/",
-            }
-        ],
-    )
-
-    return [robot_state_publisher_node]
+    return [
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            namespace=namespace,
+            output="screen",
+            parameters=[
+                {
+                    "robot_description": ParameterValue(
+                        Command(["xacro", " ", xacro_path]),
+                        value_type=str,
+                    ),
+                    "frame_prefix": f"{namespace}/",
+                }
+            ],
+        )
+    ]
 
 
 def generate_launch_description():
-    drone_name_arg = DeclareLaunchArgument(
-        "drone",
-        default_value="orca",
-        description="Name of the drone (used as frame prefix)",
-    )
-
     return LaunchDescription(
-        [
-            drone_name_arg,
+        declare_drone_and_namespace_args()
+        + [
             OpaqueFunction(function=launch_setup),
         ]
     )
