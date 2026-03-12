7a39ece3d4571a45643e6505dbe83a65d64400ec
Anbit <anbitadhi123@gmail.com>
2026-03-12 16:09:42 +0100

    Fix: merge issiue

--- guidance/los_guidance/launch/los_guidance.launch.py
+++ guidance/los_guidance/launch/los_guidance.launch.py
@@ -5,29 +5,47 @@ from launch import LaunchDescription
 from launch.actions import OpaqueFunction
 from launch_ros.actions import Node
 
-adapt_params = path.join(
-    get_package_share_directory("los_guidance"),
-    "config",
-    "guidance_params.yaml",
+from auv_setup.launch_arg_common import (
+    declare_drone_and_namespace_args,
+    resolve_drone_and_namespace,
 )
-orca_params = path.join(
-    get_package_share_directory("auv_setup"),
-    "config",
-    "robots",
-    "orca.yaml",
-)
+
+
+def launch_setup(context, *args, **kwargs):
+    drone, namespace = resolve_drone_and_namespace(context)
+
+    guidance_config = os.path.join(
+        get_package_share_directory("los_guidance"),
+        "config",
+        "guidance_params.yaml",
+    )
+
+    drone_params = os.path.join(
+        get_package_share_directory("auv_setup"),
+        "config",
+        "robots",
+        f"{drone}.yaml",
+    )
+
+    return [
+        Node(
+            package="los_guidance",
+            executable="los_guidance_node",
+            name="los_guidance_node",
+            namespace=namespace,
+            parameters=[
+                drone_params,
+                {
+                    "los_config_file": guidance_config,
+                    "time_step": 0.1,
+                },
+            ],
+            output="screen",
+        )
+    ]
 
 
 def generate_launch_description():
-    los_guidance_node = Node(
-        package="los_guidance",
-        executable="los_guidance_node",
-        name="los_guidance_node",
-        namespace="orca",
-        parameters=[
-            orca_params,
-            adapt_params,
-        ],
-        output="screen",
+    return LaunchDescription(
+        declare_drone_and_namespace_args() + [OpaqueFunction(function=launch_setup)]
     )
-    return LaunchDescription([los_guidance_node])
