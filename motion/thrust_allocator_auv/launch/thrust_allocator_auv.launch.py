4e9d2e996cb76a683ff673bb2efb57fbcd074a24
Cyprian Osinski <cyprian.github@gmail.com>
2026-03-18 20:05:02 +0100

    fixed configs and added default launch argument to allocator launch file such that we can test both solver types in CI

--- motion/thrust_allocator_auv/launch/thrust_allocator_auv.launch.py
+++ motion/thrust_allocator_auv/launch/thrust_allocator_auv.launch.py
@@ -2,9 +2,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import OpaqueFunction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
 from auv_setup.launch_arg_common import (
     declare_drone_and_namespace_args,
     resolve_drone_and_namespace,
@@ -13,6 +13,7 @@ from auv_setup.launch_arg_common import (
 
 def launch_setup(context, *args, **kwargs):
     drone, namespace = resolve_drone_and_namespace(context)
+    solver_type = LaunchConfiguration("solver_type").perform(context)
 
     config_file = os.path.join(
         get_package_share_directory("auv_setup"),
@@ -27,7 +28,10 @@ def launch_setup(context, *args, **kwargs):
             executable="thrust_allocator_auv_node",
             name="thrust_allocator_auv_node",
             namespace=namespace,
-            parameters=[config_file],
+            parameters=[
+                config_file,
+                {"propulsion.solver_type": solver_type},
+            ],
             output="screen",
         )
     ]
@@ -35,5 +39,13 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description() -> LaunchDescription:
     return LaunchDescription(
-        declare_drone_and_namespace_args() + [OpaqueFunction(function=launch_setup)]
+        declare_drone_and_namespace_args()
+        + [
+            DeclareLaunchArgument(
+                "solver_type",
+                default_value="qp",
+                description="Thrust allocator solver type (available: pseudoinverse, qp)",
+            ),
+            OpaqueFunction(function=launch_setup),
+        ]
     )
