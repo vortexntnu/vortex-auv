dbc1be115c4298a9f68fce6a9284a48c335e15bb
Anbit <anbitadhi123@gmail.com>
2026-03-28 12:22:58 +0100

    Fix: Pre -commit issues

--- auv_setup/launch/autopilot.launch.py
+++ auv_setup/launch/autopilot.launch.py
@@ -8,6 +8,7 @@ from launch.actions import (
 from launch_ros.actions import Node
 
 from auv_setup.launch_arg_common import (
+    declare_drone_and_namespace_args,
     resolve_drone_and_namespace,
 )
 
@@ -15,7 +16,7 @@ from auv_setup.launch_arg_common import (
 def launch_setup(context, *args, **kwargs):
     drone, namespace = resolve_drone_and_namespace(context)
 
-    velocity_lqr_config = os.path.join(
+    velocity_config = os.path.join(
         get_package_share_directory("velocity_controller"),
         "config",
         f"{drone}_params.yaml",
@@ -39,30 +40,29 @@ def launch_setup(context, *args, **kwargs):
         executable="los_guidance_node",
         name="los_guidance_node",
         namespace=namespace,
-        parameters=[drone_params, los_config],
+        parameters=[
+            drone_params,
+            {
+                "los_config_file": los_config,
+                "time_step": 0.1,
+            },
+        ],
         output="screen",
     )
 
-    lqr_node = Node(
+    velocity_node = Node(
         package="velocity_controller",
         executable="velocity_controller_node.py",
         name="velocity_controller_node",
         namespace=namespace,
         output="screen",
-        parameters=[drone_params, velocity_lqr_config],
+        parameters=[drone_params, velocity_config],
     )
 
-    return [los_node, lqr_node]
+    return [los_node, velocity_node]
 
 
 def generate_launch_description():
     return LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "drone",
-                default_value="orca",
-                description="Drone name / namespace",
-            ),
-            OpaqueFunction(function=launch_setup),
-        ]
+        declare_drone_and_namespace_args() + [OpaqueFunction(function=launch_setup)]
     )
--- control/velocity_controller/launch/VCnTest.launch.py
+++ control/velocity_controller/launch/VCnTest.launch.py
@@ -22,10 +22,6 @@ def launch_setup(context, *args, **kwargs):
     drone, namespace = resolve_drone_and_namespace(context)
     global_share = get_package_share_directory('auv_setup')
     config_path_global = os.path.join(global_share, 'config', 'robots', f'{drone}.yaml')
-    common_launch_args = {
-        "drone": drone,
-        "namespace": namespace,
-    }.items()
 
     stonefish_dir = get_package_share_directory('stonefish_sim')
 
@@ -45,33 +41,22 @@ def launch_setup(context, *args, **kwargs):
             )
         ],
     )
-    operation_mode_manager_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory("operation_mode_manager"),
-                "launch",
-                "operation_mode_manager.launch.py",
-            )
-        ),
-        launch_arguments=common_launch_args,
-    )
 
     node_name_arg = DeclareLaunchArgument(
         'node_name_1',
-        default_value='test_VC_node',
+        default_value='test_vc_node',
         description='Name of the test VC node',
     )
-    test_VC_name = LaunchConfiguration('node_name_1')
+    test_vc_name = LaunchConfiguration('node_name_1')
 
     return [
         stonefish_sim,
         orca_sim,
         node_name_arg,
-        # operation_mode_manager_launch,
         Node(
             package='velocity_controller',
-            executable='test_VC_node',
-            name=test_VC_name,
+            executable='test_vc_node',
+            name=test_vc_name,
             namespace=namespace,
             output='screen',
             parameters=[config_path_global],
