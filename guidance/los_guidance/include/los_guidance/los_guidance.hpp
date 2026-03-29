c106bbbce852d40f18db697299337ae298da5086
Henrik <henrimha@stud.ntnu.no>
2026-03-29 18:03:46 +0200

    new try at reverting guidance to version from main

--- guidance/los_guidance/include/los_guidance/los_guidance.hpp
+++ guidance/los_guidance/include/los_guidance/los_guidance.hpp
@@ -1,13 +1,16 @@
-#ifndef LOS_GUIDANCE_HPP
-#define LOS_GUIDANCE_HPP
+#ifndef LOS_GUIDANCE__LOS_GUIDANCE_HPP_
+#define LOS_GUIDANCE__LOS_GUIDANCE_HPP_
 
 #include <eigen3/Eigen/Dense>
 
+namespace vortex::guidance {
+
 namespace LOS {
+
 struct Point {
-    double x;
-    double y;
-    double z;
+    double x{};
+    double y{};
+    double z{};
 
     Point operator-(const Point& other) const {
         return Point{x - other.x, y - other.y, z - other.z};
@@ -17,9 +20,9 @@ struct Point {
 };
 
 struct CrossTrackError {
-    double x_e;
-    double y_e;
-    double z_e;
+    double x_e{};
+    double y_e{};
+    double z_e{};
 
     inline static CrossTrackError from_vector(const Eigen::Vector3d& vector) {
         return CrossTrackError{vector.x(), vector.y(), vector.z()};
@@ -27,13 +30,14 @@ struct CrossTrackError {
 };
 
 struct Params {
-    double lookahead_distance_h;
-    double lookahead_distance_v;
-    double gamma_h;
-    double gamma_v;
-    double time_step;
+    double lookahead_distance_h{};
+    double lookahead_distance_v{};
+    double gamma_h{};
+    double gamma_v{};
+    double time_step{};
 };
-};  // namespace LOS
+
+}  // namespace LOS
 
 /**
  * @brief Adaptive Line-of-Sight (LOS) guidance algorithm based on slide 113
@@ -41,7 +45,7 @@ struct Params {
  */
 class AdaptiveLOSGuidance {
    public:
-    AdaptiveLOSGuidance(const LOS::Params& params);
+    explicit AdaptiveLOSGuidance(const LOS::Params& params);
     ~AdaptiveLOSGuidance() = default;
 
     void update_angles(const LOS::Point& prev_point,
@@ -49,27 +53,25 @@ class AdaptiveLOSGuidance {
 
     LOS::CrossTrackError calculate_crosstrack_error(
         const LOS::Point& prev_point,
-        const LOS::Point& current_position);
+        const LOS::Point& current_position) const;
 
-    double calculate_psi_d(const double& y_e);
+    double calculate_psi_d(const double& y_e) const;
 
-    double calculate_theta_d(const double& z_e);
+    double calculate_theta_d(const double& z_e) const;
 
     void update_adaptive_estimates(
         const LOS::CrossTrackError& crosstrack_error);
 
    private:
-    double time_step_;
-    double lookahead_distance_h_;
-    double lookahead_distance_v_;
-    double gamma_h_;
-    double gamma_v_;
-    Eigen::Matrix3d rotation_y_;
-    Eigen::Matrix3d rotation_z_;
-    double pi_h_;
-    double pi_v_;
-    double beta_c_hat_ = 0.0;
-    double alpha_c_hat_ = 0.0;
+    LOS::Params params_;
+    Eigen::Matrix3d rotation_y_ = Eigen::Matrix3d::Zero();
+    Eigen::Matrix3d rotation_z_ = Eigen::Matrix3d::Zero();
+    double pi_h_{};
+    double pi_v_{};
+    double beta_c_hat_{};
+    double alpha_c_hat_{};
 };
 
-#endif  // LOS_GUIDANCE_HPP
+}  // namespace vortex::guidance
+
+#endif  // LOS_GUIDANCE__LOS_GUIDANCE_HPP_
