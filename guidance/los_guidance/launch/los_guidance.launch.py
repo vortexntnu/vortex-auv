e198b168387073143dec58e4c805ba445d51d3bd
Henrik <henrimha@stud.ntnu.no>
2026-03-29 15:32:58 +0200

    updated guidance repositry from main

--- guidance/los_guidance/launch/los_guidance.launch.py
+++ guidance/los_guidance/launch/los_guidance.launch.py
@@ -1,42 +1,32 @@
+from os import path
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from auv_setup.launch_arg_common import (
-    declare_drone_and_namespace_args,
-    resolve_drone_and_namespace,
+adapt_params = path.join(
+    get_package_share_directory("los_guidance"),
+    "config",
+    "guidance_params.yaml",
+)
+orca_params = path.join(
+    get_package_share_directory("auv_setup"),
+    "config",
+    "robots",
+    "orca.yaml",
 )
 
 
-def launch_setup(context, *args, **kwargs):
-    drone, namespace = resolve_drone_and_namespace(context)
-
-    los_config = os.path.join(
-        get_package_share_directory("los_guidance"),
-        "config",
-        "guidance_params.yaml",
-    )
-
-    drone_params = os.path.join(
-        get_package_share_directory("auv_setup"),
-        "config",
-        "robots",
-        f"{drone}.yaml",
-    )
-
-    return [
-        Node(
-            package="los_guidance",
-            executable="los_guidance_node",
-            name="los_guidance_node",
-            namespace=namespace,
-            parameters=[drone_params, los_config],
-            output="screen",
-        )
-    ]
-
-
 def generate_launch_description():
-    return LaunchDescription(
-        declare_drone_and_namespace_args() + [OpaqueFunction(function=launch_setup)]
+    los_guidance_node = Node(
+        package="los_guidance",
+        executable="los_guidance_node",
+        name="los_guidance_node",
+        namespace="orca",
+        parameters=[
+            orca_params,
+            adapt_params,
+        ],
+        output="screen",
     )
+    return LaunchDescription([los_guidance_node])
--- guidance/los_guidance/src/los_guidance_ros.cpp
+++ guidance/los_guidance/src/los_guidance_ros.cpp
@@ -170,6 +170,8 @@ void LOSGuidanceNode::execute(
 
     adaptive_los_guidance_->update_angles(last_point_, next_point_);
 
+    auto feedback =
+        std::make_shared<vortex_msgs::action::LOSGuidance::Feedback>();
     auto result = std::make_shared<vortex_msgs::action::LOSGuidance::Result>();
 
     rclcpp::Rate loop_rate(1000.0 / time_step_.count());
@@ -199,21 +201,21 @@ void LOSGuidanceNode::execute(
 
         adaptive_los_guidance_->update_adaptive_estimates(errors);
 
-        if ((eta_ - next_point_).as_vector().norm() < goal_reached_tol_) {
+        vortex_msgs::msg::LOSGuidance reference_msg = fill_los_reference();
+
+        feedback->feedback = reference_msg;
+
+        goal_handle->publish_feedback(feedback);
+        reference_pub_->publish(reference_msg);
+
+        if ((eta_ - next_point_).as_vector().norm() < 0.5) {
             result->success = true;
             goal_handle->succeed(result);
-            u_desired_ = 0.0;
-            auto final_reference_msg =
-                std::make_unique<vortex_msgs::msg::LOSGuidance>(
-                    fill_los_reference());
-            reference_pub_->publish(std::move(final_reference_msg));
+            vortex_msgs::msg::LOSGuidance reference_msg = fill_los_reference();
+            reference_pub_->publish(reference_msg);
             spdlog::info("Goal reached");
             return;
         }
-
-        auto reference_msg = std::make_unique<vortex_msgs::msg::LOSGuidance>(
-            fill_los_reference());
-        reference_pub_->publish(std::move(reference_msg));
 
         loop_rate.sleep();
     }
