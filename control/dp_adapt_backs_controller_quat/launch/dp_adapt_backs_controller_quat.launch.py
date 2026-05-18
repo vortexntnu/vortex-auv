053097410abb7b3678bdde6c6da0769c1a517bc5
Cyprian Osinski <102925573+Q3rkses@users.noreply.github.com>
2026-05-18 22:20:40 +0200

    removed default yaml file, added launch args for launching drone  (#717)
    * removed default yaml file, added launch args for launching drone either for simulator or physical drone
    
    * ran pre commit hooks
    
    * change default config type are to physical
    
    * remove duplicate launch arg
    
    * pre-commit changes
    
    ---------
    
    Co-authored-by: Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>

--- control/dp_adapt_backs_controller_quat/launch/dp_adapt_backs_controller_quat.launch.py
+++ control/dp_adapt_backs_controller_quat/launch/dp_adapt_backs_controller_quat.launch.py
@@ -6,18 +6,21 @@ from launch.actions import OpaqueFunction
 from launch_ros.actions import Node
 
 from auv_setup.launch_arg_common import (
+    declare_config_type_arg,
     declare_drone_and_namespace_args,
+    resolve_config_type,
     resolve_drone_and_namespace,
 )
 
 
 def launch_setup(context, *args, **kwargs):
     drone, namespace = resolve_drone_and_namespace(context)
+    config_type = resolve_config_type(context)
 
     adapt_params = os.path.join(
         get_package_share_directory("dp_adapt_backs_controller_quat"),
         "config",
-        f"adapt_params_{drone}.yaml",
+        f"adapt_params_{drone}_{config_type}.yaml",
     )
 
     drone_params = os.path.join(
@@ -41,5 +44,6 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description():
     return LaunchDescription(
-        declare_drone_and_namespace_args() + [OpaqueFunction(function=launch_setup)]
+        declare_drone_and_namespace_args()
+        + [declare_config_type_arg(), OpaqueFunction(function=launch_setup)]
     )
